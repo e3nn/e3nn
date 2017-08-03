fix some normalization + add batch norm

experiments/toy_shapenet/m7.py
@@ -3,6 +3,7 @@
 Based on m3
 
 + scalar repr
++ batch norm
 '''
 import torch
 import torch.nn as nn
@@ -32,22 +33,35 @@ class CNN(nn.Module):
 
         for i in range(len(representations) - 1):
             non_lin = i < len(representations) - 2
-            conv = SE3Convolution(4, representations[i + 1], representations[i], bias_relu=non_lin, norm_relu=non_lin)
+            conv = SE3Convolution(4, representations[i + 1], representations[i], bias_relu=non_lin, norm_relu=non_lin, scalar_batch_norm=True)
             setattr(self, 'conv{}'.format(i), conv)
             self.convolutions.append(conv)
 
-        self.bias = torch.nn.Parameter(torch.FloatTensor(number_of_classes))
-        self.bias.data[:] = 0
+        self.bn_in = nn.BatchNorm3d(1, affine=True)
+        self.bn_out = nn.BatchNorm3d(number_of_classes, affine=True)
+
+        # self.bias = torch.nn.Parameter(torch.FloatTensor(number_of_classes))
+        # self.bias.data[:] = 0
 
     def forward(self, x):
         '''
         :param x: [batch, features, x, y, z]
         '''
+        x = self.bn_in(x.contiguous())
+        logger.info("Mean = %f Std = %f", x.data.mean(), x.data.std())
         for conv in self.convolutions:
+            if x.size(1) == 35:
+                logger.info("R1 Mean = %f Std = %f", x.data[:, :6].mean(), x.data[:, :6].std())
+                logger.info("R3 Mean = %f Std = %f", x.data[:, 6:18].mean(), x.data[:, 6:18].std())
+                logger.info("R5 Mean = %f Std = %f", x.data[:, 18:28].mean(), x.data[:, 18:28].std())
+                logger.info("R7 Mean = %f Std = %f", x.data[:, 28:].mean(), x.data[:, 28:].std())
             x = conv(x)
 
+        logger.info("Mean = %f Std = %f", x.data.mean(), x.data.std())
         x = x.mean(-1).squeeze(-1).mean(-1).squeeze(-1).mean(-1).squeeze(-1) # [batch, features]
-        x = x + self.bias.view(1, -1).expand_as(x)
+        x = self.bn_out(x.contiguous())
+        logger.info("Mean = %f Std = %f", x.data.mean(), x.data.std())
+        # x = x + self.bias.view(1, -1).expand_as(x)
         return x
 
 class MyModel(Model):

se3_cnn/convolution.py
@@ -7,10 +7,9 @@ from se3_cnn import SO3
 from se3_cnn.non_linearities.scalar_activation import BiasRelu
 from se3_cnn.non_linearities.norm_activation import NormRelu
 from se3_cnn.utils import time_logging
-# import logging
 
 class SE3Convolution(torch.nn.Module):
-    def __init__(self, size, Rs_out, Rs_in, bias_relu=False, norm_relu=False, M=15, central_base=True):
+    def __init__(self, size, Rs_out, Rs_in, bias_relu=False, norm_relu=False, scalar_batch_norm=False, M=15, central_base=True):
         '''
         :param Rs_out: list of couple (multiplicity, representation)
         multiplicity is a positive integer
@@ -21,7 +20,7 @@ class SE3Convolution(torch.nn.Module):
         super(SE3Convolution, self).__init__()
         self.combination = SE3KernelCombination(size, Rs_out, Rs_in, M=M, central_base=central_base)
         self.weight = Parameter(torch.FloatTensor(self.combination.nweights))
-        self.bias_relu = BiasRelu([(m * SO3.dim(R), SO3.dim(R) == 1) for m, R in Rs_out]) if bias_relu else None
+        self.bias_relu = BiasRelu([(m * SO3.dim(R), SO3.dim(R) == 1) for m, R in Rs_out], batch_norm=scalar_batch_norm) if bias_relu else None
         self.norm_relu = NormRelu([(SO3.dim(R), SO3.dim(R) > 1) for m, R in Rs_out for _ in range(m)]) if norm_relu else None
         self.reset_parameters()
 
@@ -34,13 +33,12 @@ class SE3Convolution(torch.nn.Module):
 
     def forward(self, input): # pylint: disable=W
         time = time_logging.start()
+
         kernel = self.combination(self.weight)
         time = time_logging.end("kernel combination", time)
+
         output = torch.nn.functional.conv3d(input, kernel)
         time = time_logging.end("3d convolutions", time)
-
-        # logger = logging.getLogger("trainer")
-        # logger.info("pre activation %f +- %f", output.data.mean(), output.data.std())
 
         if self.bias_relu is not None:
             output = self.bias_relu(output)
@@ -49,7 +47,6 @@ class SE3Convolution(torch.nn.Module):
             output = self.norm_relu(output)
             time = time_logging.end("norm relu", time)
 
-        # logger.info("post activation %f +- %f", output.data.mean(), output.data.std())
         return output
 
 
@@ -62,10 +59,10 @@ class SE3KernelCombination(torch.autograd.Function):
         Rs_in = [(m, R) for m, R in Rs_in if m >= 1]
         self.multiplicites_out = [m for m, _ in Rs_out]
         self.multiplicites_in = [m for m, _ in Rs_in]
-        self.dims_out = [basis_kernels.dim(R) for _, R in Rs_out]
-        self.dims_in = [basis_kernels.dim(R) for _, R in Rs_in]
+        self.dims_out = [SO3.dim(R) for _, R in Rs_out]
+        self.dims_in = [SO3.dim(R) for _, R in Rs_in]
 
-        def generate_basis(R_out, R_in):
+        def generate_basis(R_out, R_in, m_out, m_in):
             basis = basis_kernels.cube_basis_kernels_subsampled_cosine(size, R_out, R_in, M)
 
             if central_base and size % 2 == 1:
@@ -79,12 +76,13 @@ class SE3KernelCombination(torch.autograd.Function):
             for k in range(len(basis)):
                 basis[k] /= np.linalg.norm(basis[k])
                 basis[k] *= np.sqrt(SO3.dim(R_out) / len(basis))
+                basis[k] /= np.sqrt(m_in)
                 # such that the weight can be initialized with Normal(0,1)
             return basis
 
-        self.kernels = [[torch.FloatTensor(generate_basis(R_out, R_in))
-            for _, R_in in Rs_in]
-            for _, R_out in Rs_out]
+        self.kernels = [[torch.FloatTensor(generate_basis(R_out, R_in, m_out, m_in))
+            for m_in, R_in in Rs_in]
+            for m_out, R_out in Rs_out]
 
         self.nweights = 0
         for i in range(len(Rs_out)):
@@ -150,6 +148,27 @@ class SE3KernelCombination(torch.autograd.Function):
             begin_i += self.multiplicites_out[i] * self.dims_out[i]
 
         return grad_weight
+
+
+def test_normalization(batch, size, Rs_out=None, Rs_in=None):
+    if Rs_out is None:
+        Rs_out = [(1, SO3.repr1), (1, SO3.repr3)]
+    if Rs_in is None:
+        Rs_in = [(1, SO3.repr1), (1, SO3.repr3)]
+    conv = SE3Convolution(4, Rs_out, Rs_in, bias_relu=False, norm_relu=False)
+    print("Weights Amount = {} Mean = {} Std = {}".format(conv.weight.numel(), conv.weight.data.mean(), conv.weight.data.std()))
+
+    n_out = sum([m * SO3.dim(r) for m, r in Rs_out])
+    n_in = sum([m * SO3.dim(r) for m, r in Rs_in])
+
+    x = torch.autograd.Variable(torch.randn(batch, n_in, size, size, size))
+    print("x Amount = {} Mean = {} Std = {}".format(x.numel(), x.data.mean(), x.data.std()))
+    y = conv(x)
+
+    assert y.size(1) == n_out
+
+    print("y Amount = {} Mean = {} Std = {}".format(y.numel(), y.data.mean(), y.data.std()))
+    return y.data
 
 
 def test_convolution_gradient():

se3_cnn/non_linearities/scalar_activation.py
@@ -3,7 +3,7 @@ import torch
 from torch.nn.parameter import Parameter
 
 class BiasRelu(torch.nn.Module):
-    def __init__(self, enable):
+    def __init__(self, enable, batch_norm=False):
         '''
         :param enable: list of tuple (dimension, boolean)
 
@@ -20,6 +20,15 @@ class BiasRelu(torch.nn.Module):
                 self.enable[-1] = (self.enable[-1][0] + d, on)
             else:
                 self.enable.append((d, on))
+
+        self.batch_norms = None
+        if batch_norm:
+            self.batch_norms = []
+            for d, on in self.enable:
+                if on:
+                    bn = torch.nn.BatchNorm3d(d, affine=False)
+                    setattr(self, "bn{}".format(len(self.batch_norms)), bn)
+                    self.batch_norms.append(bn)
 
         nbias = sum([d for d, on in self.enable if on])
         self.bias = Parameter(torch.FloatTensor(nbias)) if nbias > 0 else None
@@ -39,13 +48,19 @@ class BiasRelu(torch.nn.Module):
         xs = []
         begin1 = 0
         begin2 = 0
+        bn_counter = 0
 
         for d, on in self.enable:
             x = input[:, begin1:begin1 + d]
 
             if on:
+                if self.batch_norms is not None:
+                    x = self.batch_norms[bn_counter](x.contiguous())
+                    bn_counter += 1
                 x = x + self.bias[begin2:begin2 + d].view(1, -1, 1, 1, 1).expand_as(x)
                 x = torch.nn.functional.relu(x)
+                x.sub_(0.3989422804014327)
+                x.mul_(1.712858550449663)
                 begin2 += d
 
             xs.append(x)