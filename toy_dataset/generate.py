3cf3da9b43830664f8d317f9acd88a59ac53bd83
Mario Geiger <geiger.mario@gmail.com>
2017-08-23 11:54:35 +0200

    generate
    Former-commit-id: 5fd3eb4e899f9e2f9a4dcf1b393f3034f0c130e2

--- toy_dataset/generate.py
+++ toy_dataset/generate.py
@@ -7,7 +7,6 @@ i_csv = "../../datasets/shapenet/val.csv"
 i_zip = "../../datasets/shapenet/val_perturbed.zip"
 i_dir =                         "val_perturbed"
 
-o_csv = "../../datasets/shapenet/voxels/val.csv"
 o_dir = "../../datasets/shapenet/voxels/val_perturbed"
 
 os.mkdir(o_dir)
@@ -20,29 +19,10 @@ with open(i_csv, "rt") as file:
 id_ = rows[0].index("id")
 cl_ = rows[0].index("synsetId")
 
-classes = ["02691156", "02958343", "04256520", "04090263", "03636649", "04530566"]
-
-"""
-04379243 = tables, desks, dresser, chair, ... (5876 objects)
-03001627 = chair (modern style) (4612 objects)
-02691156 = flying objects (airplanes, spaceships?) (2832 objects)
-02958343 = vehicules (cars, autocars) (2502 objects)
-04256520 = chair (simple style) (2198 objects)
-04090263 = gun (1655 objcets)
-03636649 = lamps (1620 objects)
-04530566 = boat (1356 objects)
-"""
 
 
-subset = rows[:1] + [row for row in rows[1:] if row[cl_] in [cl for cl in classes]]
-
-# save subset into csv file
-with open(o_csv, "wt") as file:
-    writer = csv.writer(file)
-    writer.writerows(subset)
-
-for i, row in enumerate(subset[1:]):
-    print("{}/{}    ".format(i, len(subset) - 1), end="\r")
+for i, row in enumerate(rows[1:]):
+    print("{}/{}    ".format(i, len(rows) - 1), end="\r")
 
     os.system("unzip -p {} {}/{}.obj > tmp.obj".format(i_zip, i_dir, row[id_]))
 
@@ -52,4 +32,4 @@ for i, row in enumerate(subset[1:]):
     x = x.astype(np.int8)
     x = x.reshape((64, 64, 64))
 
-    np.save(os.path.join(o_dir, row[id_] + ".npy"), x)
+    np.savez_compressed(os.path.join(o_dir, row[id_] + ".npz"), x)
--- toy_dataset/mini_datasets.py
+++ toy_dataset/mini_datasets.py
@@ -4,6 +4,18 @@ import random
 
 in_csv = "../../datasets/shapenet/voxels/val.csv"
 classes = ["02691156", "02958343", "04256520", "04090263", "03636649", "04530566"]
+
+"""
+04379243 = tables, desks, dresser, chair, ... (5876 objects)
+03001627 = chair (modern style) (4612 objects)
+02691156 = flying objects (airplanes, spaceships?) (2832 objects)
+02958343 = vehicules (cars, autocars) (2502 objects)
+04256520 = chair (simple style) (2198 objects)
+04090263 = gun (1655 objcets)
+03636649 = lamps (1620 objects)
+04530566 = boat (1356 objects)
+"""
+
 how_much = 194
 out_csv = "../../datasets/shapenet/voxels/val_{}x{}.csv".format(len(classes), how_much)
 
