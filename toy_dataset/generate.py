94d199c41786fb8da282e068ca98606df7133e69
Mario Geiger <geiger.mario@gmail.com>
2017-08-24 21:50:34 +0200

    generate
    Former-commit-id: 6ad16729eede861749250897a6f08666df48c062

--- toy_dataset/generate.py
+++ toy_dataset/generate.py
@@ -4,12 +4,15 @@ import os
 import numpy as np
 
 i_csv = "../../datasets/shapenet/val.csv"
-i_zip = "../../datasets/shapenet/val_perturbed.zip"
-i_dir =                         "val_perturbed"
+i_zip = "../../datasets/shapenet/val_normal.zip"
+i_dir =                         "val_normal"
 
-o_dir = "../../datasets/shapenet/voxels/val_perturbed"
+o_dir = "../../datasets/shapenet/voxels/val_normal"
 
-os.mkdir(o_dir)
+try:
+    os.mkdir(o_dir)
+except:
+    pass
 
 with open(i_csv, "rt") as file:
     reader = csv.reader(file)
@@ -24,12 +27,15 @@ cl_ = rows[0].index("synsetId")
 for i, row in enumerate(rows[1:]):
     print("{}/{}    ".format(i, len(rows) - 1), end="\r")
 
-    os.system("unzip -p {} {}/{}.obj > tmp.obj".format(i_zip, i_dir, row[id_]))
+    output = os.path.join(o_dir, row[id_] + ".npz")
 
-    os.system("obj2voxel --size 64 tmp.obj tmp.npy")
+    if not os.path.isfile(output):
+        os.system("unzip -p {} {}/{}.obj > tmp.obj".format(i_zip, i_dir, row[id_]))
 
-    x = np.load("tmp.npy")
-    x = x.astype(np.int8)
-    x = x.reshape((64, 64, 64))
+        os.system("obj2voxel --size 64 tmp.obj tmp.npy")
 
-    np.savez_compressed(os.path.join(o_dir, row[id_] + ".npz"), x)
+        x = np.load("tmp.npy")
+        x = x.astype(np.int8)
+        x = x.reshape((64, 64, 64))
+
+        np.savez_compressed(output, x)
