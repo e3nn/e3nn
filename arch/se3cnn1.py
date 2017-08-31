7f29401a5782d0809f08a911014451769bf5f5c5
Mario Geiger <geiger.mario@gmail.com>
2017-08-31 10:58:04 +0200

    add repr5
    Former-commit-id: 800527a2c49bae66714a24be7c43969cb9d15165

--- arch/se3cnn1.py
+++ arch/se3cnn1.py
@@ -3,7 +3,7 @@
 Best SE3 equivariant architecture found for shapenet
 classification of 6 classes
 
-c18
+c21
 '''
 import torch
 import torch.nn as nn
@@ -22,28 +22,28 @@ logger = logging.getLogger("trainer")
 
 
 class Block(nn.Module):
-    def __init__(self, scalar_out, vector_out, scalar_in, vector_in, relu, stride):
+    def __init__(self, repr_in, repr_out, relu, stride):
         super().__init__()
-        self.tensor = TensorProduct([(scalar_in, 1, False), (vector_in, 3, True)]) if vector_in > 0 else None
+        self.tensor = TensorProduct([(repr_in[0], 1, False), (repr_in[1], 3, True), (repr_in[2], 5, False)]) if repr_in[1] > 0 else None
         self.conv = SE3Convolution(7, 3,
-            [(scalar_out, SO3.repr1), (vector_out, SO3.repr3)],
-            [(scalar_in, SO3.repr1), (vector_in, SO3.repr3), (vector_in, SO3.repr3x3)],
+            [(repr_out[0], SO3.repr1), (repr_out[1], SO3.repr3), (repr_out[2], SO3.repr5)],
+            [(repr_in[0], SO3.repr1), (repr_in[1], SO3.repr3), (repr_in[2], SO3.repr5), (repr_in[1], SO3.repr3x3)],
             stride=stride,
             padding=3)
-        self.bn = SE3BatchNorm([(scalar_out, 1), (vector_out, 3)])
-        self.relu = BiasRelu([(scalar_out, True), (vector_out * 3, False)]) if relu else None
+        self.bn = SE3BatchNorm([(repr_out[0], 1), (repr_out[1], 3), (repr_out[2], 5)])
+        self.relu = BiasRelu([(repr_out[0], True), (repr_out[1] * 3, False), (repr_out[2] * 5, False)]) if relu else None
 
-    def forward(self, sv): # pylint: disable=W
+    def forward(self, sv5): # pylint: disable=W
         if self.tensor is not None:
-            t = self.tensor(sv)
-            svt = torch.cat([sv, t], dim=1)
+            t = self.tensor(sv5)
+            sv5t = torch.cat([sv5, t], dim=1)
         else:
-            svt = sv
-        sv = self.conv(svt) # only convolution
-        sv = self.bn(sv)
+            sv5t = sv5
+        sv5 = self.conv(sv5t) # only convolution
+        sv5 = self.bn(sv5)
         if self.relu is not None:
-            sv = self.relu(sv)
-        return sv
+            sv5 = self.relu(sv5)
+        return sv5
 
 class CNN(nn.Module):
 
@@ -52,20 +52,20 @@ class CNN(nn.Module):
 
         logger.info("Create CNN for classify %d classes", number_of_classes)
 
-        features = [(1, 0), # 64
-            (4, 2), # 32
-            (10, 5), # 32
-            (10, 5), # 16
-            (12, 6), # 16
-            (12, 6), # 8
-            (14, 7), # 8
-            (number_of_classes, 0)]  # 4
+        features = [(1, 0, 0), # 64
+            (4, 2, 0), # 32
+            (10, 5, 1), # 32
+            (10, 5, 1), # 16
+            (12, 6, 1), # 16
+            (12, 6, 2), # 8
+            (14, 7, 2), # 8
+            (number_of_classes, 0, 0)]  # 4
 
         self.convolutions = []
 
         for i in range(len(features) - 1):
             relu = i < len(features) - 2
-            conv = Block(features[i + 1][0], features[i + 1][1], features[i][0], features[i][1], relu, 2 if i % 2 == 0 else 1)
+            conv = Block(features[i], features[i + 1], relu, 2 if i % 2 == 0 else 1)
             setattr(self, 'conv{}'.format(i), conv)
             self.convolutions.append(conv)
 
