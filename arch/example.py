394ca4817805587cd305ca1a66ea45410bdc9522
Mario Geiger <geiger.mario@gmail.com>
2018-01-03 17:21:16 +0100

    Add comments into example
    Former-commit-id: 68c0063444ea101763936acae0fecad2a6e9e553

--- arch/example.py
+++ arch/example.py
@@ -1,10 +1,19 @@
 # pylint: disable=C,R,E1101
 '''
 Minimalist example of usage of SE(3) CNN
+
+This example train a neural network to classify two geometrical objects
+A ball : x^2 + y^2 + z^2 < radius^2
+An octahedron : |x| + |y| + |z| < radius
+
+The input data is generated on the fly, the radius is random and noise is added
 '''
 import torch
 import numpy as np
 
+# The Highway Block is a way to introduce non linearity into the neural network
+# The class HighwayBlock inherit from the class torch.nn.Module.
+# It contains one convolution, some ReLU and multiplications
 from se3_cnn.blocks.highway import HighwayBlock
 
 
@@ -13,6 +22,10 @@ class CNN(torch.nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
 
+        # The parameters of a HighwayBlock are:
+        # - The representation multiplicities (scalar, vector and dim. 5 repr.) for the input and the output
+        # - The stride, same as 2D convolution
+        # - A parameter to tell if the non linearity is enabled or not (ReLU or nothing)
         features = [
             (1, 0, 0),
             (10, 10, 2),
@@ -32,7 +45,11 @@ class CNN(torch.nn.Module):
         '''
         :param inp: [batch, features, x, y, z]
         '''
+
+        # Perform the Highway blocks
         x = self.blocks(inp)  # [batch, features, x, y, z]
+
+        # Take the average over the spacial indicies
         x = x.view(x.size(0), x.size(1), -1)  # [batch, features, x*y*z]
         x = x.mean(-1)  # [batch, features]
 
@@ -49,15 +66,18 @@ def main():
     optimizer = torch.optim.Adam(cnn.parameters(), lr=1e-2)
 
     batch_size = 64
-    sample_size = 24
+    sample_size = 24  # Size of the input cube
 
     mesh = np.linspace(-1, 1, sample_size)
     mx, my, mz = np.meshgrid(mesh, mesh, mesh)
 
     def step(i):
+        # only noise
         x = 0.1 * np.random.randn(batch_size, 1, sample_size, sample_size, sample_size)
+        # random labels: zero for the ball and one for the octahedron
         y = np.random.randint(0, 2, size=(batch_size,))
 
+        # add the balls and octahedrons on top of the noise
         for j, label in enumerate(y):
             radius = 0.6 + np.random.rand() * (0.9 - 0.6)
 
@@ -65,14 +85,16 @@ def main():
                 # ball
                 mask = mx ** 2 + my ** 2 + mz ** 2 < radius ** 2
             if label == 1:
-                # cube
+                # octahedron
                 mask = abs(mx) + abs(my) + abs(mz) < radius
 
             x[j, 0, mask] += np.random.randint(2) * 2 - 1
 
+        # convert the input and labels into Pytorch tensor
         x = torch.FloatTensor(x)
         y = torch.LongTensor(y)
 
+        # upload on the GPU if available
         if torch.cuda.is_available():
             x = x.cuda()
             y = y.cuda()
@@ -80,16 +102,19 @@ def main():
         x = torch.autograd.Variable(x)
         y = torch.autograd.Variable(y)
 
+        # forward and backward propagation
         optimizer.zero_grad()
         out = cnn(x)
         loss = criterion(out, y)
         loss.backward()
         optimizer.step()
 
+        # download results on the CPU
         loss = loss.data.cpu().numpy()
         out = out.data.cpu().numpy()
         y = y.data.cpu().numpy()
 
+        # compute the accuracy
         acc = np.sum(out.argmax(-1) == y) / batch_size
 
         print("{}: acc={}% loss={}".format(i, 100 * acc, float(loss)))
