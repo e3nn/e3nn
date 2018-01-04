edea34566770b429a743bee47b21b120ef99a725
Mario Geiger <geiger.mario@gmail.com>
2018-01-04 11:53:29 +0100

    TensorProductBlock
    Former-commit-id: c72522a3a8be37c71fe379abe9eeb17da072b4e1

--- arch/qm7.py
+++ arch/qm7.py
@@ -7,10 +7,7 @@ RMSE test = 5.7
 import torch
 import torch.nn as nn
 from se3_cnn.batchnorm import SE3BatchNorm
-from se3_cnn.bn_conv import SE3BNConvolution
-from se3_cnn.non_linearities.scalar_activation import BiasRelu
-from se3_cnn.non_linearities.tensor_product import TensorProduct
-from se3_cnn import SO3
+from se3_cnn.blocks.tensor_product import TensorProductBlock
 from util_cnn.model_backup import ModelBackup
 
 from util_cnn import time_logging
@@ -21,33 +18,6 @@ import pickle
 logger = logging.getLogger("trainer")
 
 
-class Block(nn.Module):
-    def __init__(self, repr_in, repr_out, relu, stride):
-        super().__init__()
-        self.tensor = TensorProduct([(repr_in[0], 1, False), (repr_in[1], 3, True), (repr_in[2], 5, False)]) if repr_in[1] > 0 else None
-        self.bn_conv = SE3BNConvolution(
-            size=7,
-            radial_amount=3,
-            Rs_in=[(repr_in[0], SO3.repr1), (repr_in[1], SO3.repr3), (repr_in[2], SO3.repr5), (repr_in[1], SO3.repr3x3)],
-            Rs_out=[(repr_out[0], SO3.repr1), (repr_out[1], SO3.repr3), (repr_out[2], SO3.repr5)],
-            stride=stride,
-            padding=3,
-            momentum=0.01,
-            mode='maximum')
-        self.relu = BiasRelu([(repr_out[0], True), (repr_out[1] * 3, False), (repr_out[2] * 5, False)], normalize=False) if relu else None
-
-    def forward(self, sv5): # pylint: disable=W
-        if self.tensor is not None:
-            t = self.tensor(sv5)
-            sv5t = torch.cat([sv5, t], dim=1)
-        else:
-            sv5t = sv5
-
-        sv5 = self.bn_conv(sv5t)
-
-        if self.relu is not None:
-            sv5 = self.relu(sv5)
-        return sv5
 
 class CNN(nn.Module):
 
@@ -75,7 +45,7 @@ class CNN(nn.Module):
         assert len(self.block_params) + 1 == len(self.features)
 
         for i in range(len(self.block_params)):
-            block = Block(self.features[i], self.features[i + 1], self.block_params[i]['relu'], self.block_params[i]['stride'])
+            block = TensorProductBlock(self.features[i], self.features[i + 1], self.block_params[i]['relu'], self.block_params[i]['stride'])
             setattr(self, 'block{}'.format(i), block)
 
         self.lin = torch.nn.Linear(5, 1)
--- se3_cnn/blocks/tensor_product.py
+++ se3_cnn/blocks/tensor_product.py
@@ -1,7 +1,6 @@
 # pylint: disable=C,R,E1101
 import torch
-from se3_cnn.convolution import SE3Convolution
-from se3_cnn.batchnorm import SE3BatchNorm
+from se3_cnn.bn_conv import SE3BNConvolution
 from se3_cnn.non_linearities.scalar_activation import BiasRelu
 from se3_cnn.non_linearities.tensor_product import TensorProduct
 from se3_cnn import SO3
@@ -11,13 +10,16 @@ class TensorProductBlock(torch.nn.Module):
     def __init__(self, repr_in, repr_out, relu, stride):
         super().__init__()
         self.tensor = TensorProduct([(repr_in[0], 1, False), (repr_in[1], 3, True), (repr_in[2], 5, False)]) if repr_in[1] > 0 else None
-        self.conv = SE3Convolution(7, 3,
-            [(repr_out[0], SO3.repr1), (repr_out[1], SO3.repr3), (repr_out[2], SO3.repr5)],
-            [(repr_in[0], SO3.repr1), (repr_in[1], SO3.repr3), (repr_in[2], SO3.repr5), (repr_in[1], SO3.repr3x3)],
+        self.bn_conv = SE3BNConvolution(
+            size=7,
+            radial_amount=3,
+            Rs_in=[(repr_in[0], SO3.repr1), (repr_in[1], SO3.repr3), (repr_in[2], SO3.repr5), (repr_in[1], SO3.repr3x3)],
+            Rs_out=[(repr_out[0], SO3.repr1), (repr_out[1], SO3.repr3), (repr_out[2], SO3.repr5)],
             stride=stride,
-            padding=3)
-        self.bn = SE3BatchNorm([(repr_out[0], 1), (repr_out[1], 3), (repr_out[2], 5)])
-        self.relu = BiasRelu([(repr_out[0], True), (repr_out[1] * 3, False), (repr_out[2] * 5, False)]) if relu else None
+            padding=3,
+            momentum=0.01,
+            mode='maximum')
+        self.relu = BiasRelu([(repr_out[0], True), (repr_out[1] * 3, False), (repr_out[2] * 5, False)], normalize=False) if relu else None
 
     def forward(self, sv5): # pylint: disable=W
         if self.tensor is not None:
@@ -25,8 +27,9 @@ class TensorProductBlock(torch.nn.Module):
             sv5t = torch.cat([sv5, t], dim=1)
         else:
             sv5t = sv5
-        sv5 = self.conv(sv5t) # only convolution
-        sv5 = self.bn(sv5)
+
+        sv5 = self.bn_conv(sv5t)
+
         if self.relu is not None:
             sv5 = self.relu(sv5)
         return sv5
