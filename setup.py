ed56d568fb90290a5c1114759fede3823dd2c1f4
Mario Geiger <geiger.mario@gmail.com>
2018-05-19 09:30:21 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
-#pylint: disable=C
-import os
 from setuptools import setup, find_packages
-
-this_dir = os.path.dirname(__file__)
 
 setup(
     name='se3_cnn',
-    packages=find_packages(exclude=["build"])
+    packages=find_packages()
 )
