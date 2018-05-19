618f9dbba35d62b32288821a6ca488463fa83ad1
Mario Geiger <geiger.mario@gmail.com>
2018-05-20 00:30:47 +0200

    plot kernels
    Former-commit-id: 938279f3b5dcde7279edf8da1792b94d22a4df73

--- experiments/plots/kernels.py
+++ experiments/plots/kernels.py
@@ -5,24 +5,43 @@ from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D  # pylint: disable=W
 from lie_learn.representations.SO3.spherical_harmonics import sh  # real valued by default
 from se3_cnn.basis_kernels import _basis_transformation_Q_J
+from se3_cnn.util.cache_file import cached_dirpklgz
 
 
-def _sample_sh_sphere(beta, alpha, order_in, order_out, order_irreps):
+def beta_alpha(n):
+    beta = np.linspace(0, np.pi / 2, n)
+    alpha = np.arange(2 * n) * 2 * np.pi / (2 * n)
+    beta, alpha = np.meshgrid(beta, alpha, indexing='ij')
+    return beta, alpha
+
+
+@cached_dirpklgz("Y_sphere_cache")
+def _sample_Y(n, J):
+    beta, alpha = beta_alpha(n)
+    
+    Y_J = np.zeros((2 * J + 1, len(beta.flatten())))
+    for idx_m in range(2 * J + 1):
+        m = idx_m - J
+        for idx, (b, a) in enumerate(zip(beta.flatten(), alpha.flatten())):
+            Y_J[idx_m, idx] = sh(J, m, b, a)
+            
+    return Y_J
+
+
+def _sample_sh_sphere(n, order_in, order_out):
+    order_irreps = range(abs(order_in - order_out), order_in + order_out + 1)
+    
     sh_spheres = []
     for J in order_irreps:
-        Y_J = np.zeros((2 * J + 1, len(beta.flatten())))
-        for idx_m in range(2 * J + 1):
-            m = idx_m - J
-            for idx, (b, a) in enumerate(zip(beta.flatten(), alpha.flatten())):
-                Y_J[idx_m, idx] = sh(J, m, b, a)
+        Y_J = _sample_Y(n, J)
 
         # compute basis transformation matrix Q_J
         Q_J = _basis_transformation_Q_J(J, order_in, order_out)
         K_J = np.einsum('mn,n...->m...', Q_J, Y_J)
-        K_J = K_J.reshape(2 * order_out + 1, 2 * order_in + 1, *beta.shape)
+        K_J = K_J.reshape(2 * order_out + 1, 2 * order_in + 1, n, 2 * n)
         sh_spheres.append(K_J)
 
-    return sh_spheres
+    return np.array(sh_spheres)
 
 
 def plot_sphere(beta, alpha, f):
@@ -52,16 +71,13 @@ def plot_sphere(beta, alpha, f):
 
 def main():
     scale = 1.5
+    n = 50
 
-    beta = np.linspace(0, np.pi / 2, int(scale * 40))
-    n = int(scale * 100)
-    alpha = np.arange(n) * 2 * np.pi / n
-    beta, alpha = np.meshgrid(beta, alpha, indexing='ij')
-    f = _sample_sh_sphere(beta, alpha + np.pi / n, 1, 1, [0, 1, 2])
-    f = np.array(f)
+    f = _sample_sh_sphere(n, 1, 1)
     f = (f - np.min(f)) / (np.max(f) - np.min(f))
 
-    f = f.reshape(*f.shape[:3], *beta.shape)
+    beta, alpha = beta_alpha(n)
+    alpha = alpha - np.pi / (2 * n)
 
     nbase = f.shape[0]
     dim_out = f.shape[1]
