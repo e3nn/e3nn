c79ec6bfc7227740258dc1a8334b3207b571851e
Mario Geiger <geiger.mario@gmail.com>
2018-06-06 16:58:27 +0200

    new data cath plot

--- experiments/plots/cath.py
+++ experiments/plots/cath.py
@@ -1,49 +1,43 @@
+#pylint: disable=C
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-steerable = np.array([
-    [4, 0.55],
-    [2, 0.62],
-    [3, 0.59],
-    [4, 0.54],
-    [0, 0.66],
-    [2, 0.62],
-    [3, 0.59],
-    [1, 0.64],
-    [1, 0.64],
-    [0, 0.67],
-])
+def main():
+    steerable = np.array([
+        [0, 1.0, 0.657388, 0.008196],
+        [1, 2.0, 0.641843, 0.006038],
+        [2, 4.0, 0.618149, 0.008430],
+        [3, 8.0, 0.597872, 0.011057],
+        [4, 16.0, 0.543758, 0.020094],
+    ])
 
-cnn = np.array([
-    [4, 0.48],
-    [0, 0.61],
-    [1, 0.60],
-    [2, 0.58],
-    [3, 0.53],
-    [4, 0.48],
-    [3, 0.55],
-    [0, 0.62],
-    [1, 0.61],
-    [2, 0.56],
-])
+    cnn = np.array([
+        [0, 1.0, 0.608836, 0.013043],
+        [1, 2.0, 0.603203, 0.011121],
+        [2, 4.0, 0.572544, 0.011519],
+        [3, 8.0, 0.545515, 0.015933],
+        [4, 16.0, 0.489087, 0.010964],
+    ])
 
-plt.figure(figsize=(4, 2.8))
+    plt.figure(figsize=(4, 2.8))
 
-plt.plot(steerable[:, 0], steerable[:, 1], ".", label="3D Steerable CNN", color="#190c9f")
-plt.plot(cnn[:, 0], cnn[:, 1], "x", label="3D CNN", color="#b02124")
+    plt.errorbar(steerable[:, 0], steerable[:, 2], yerr=steerable[:, 3], fmt=".", label="3D Steerable CNN", color="#190c9f")
+    plt.errorbar(cnn[:, 0], cnn[:, 2], yerr=cnn[:, 3], fmt="x", label="3D CNN", color="#b02124")
+
+    plt.xticks(range(5), [r"$2^{}$".format(i) for i in range(5)])
+    y = np.arange(0.5, 0.65, 0.05)
+    plt.yticks(y, [r"${:.2f}$".format(i) for i in y])
+
+    plt.xlabel("training set size reduction factor")
+    plt.ylabel("test accuracy")
+    plt.legend()
+    plt.grid(axis="y")
+
+    plt.tight_layout()
+
+    # plt.savefig("cath.pdf")
+    plt.savefig("cath.pgf")
 
 
-plt.xticks(range(5), [r"$2^{}$".format(i) for i in range(5)])
-y = np.arange(0.5, 0.65, 0.05)
-plt.yticks(y, [r"${:.2f}$".format(i) for i in y])
-
-plt.xlabel("training set size reduction factor")
-plt.ylabel("test accuracy")
-plt.legend()
-plt.grid(axis="y")
-
-plt.tight_layout()
-
-# plt.savefig("cath.pdf")
-plt.savefig("cath.pgf")
+main()
