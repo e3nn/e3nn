9837297fd165691f71d0987e4c8051e5f6742873
Mario Geiger <geiger.mario@gmail.com>
2018-06-15 21:34:13 +0200

    field[x, y, z] = [vx, vy, vz]
    Former-commit-id: f686ca3ed46c0ad014ef248ed7faa3c249ad4063

--- se3_cnn/SE3.py
+++ se3_cnn/SE3.py
@@ -4,12 +4,14 @@ from scipy.ndimage import affine_transform
 
 
 def rotate_scalar(x, rot):
+    # this function works in the convention field[x, y, z]
     invrot = np.linalg.inv(rot)
     center = (np.array(x.shape) - 1) / 2
     return affine_transform(x, matrix=invrot, offset=center - np.dot(invrot, center))
 
 
 def rotate_field(x, rot, R):
+    # this function works in the convention field[x, y, z] = [vx, vy, vz]
     invrot = np.linalg.inv(rot)
     y = np.empty_like(x)
     for k in range(y.shape[0]):
