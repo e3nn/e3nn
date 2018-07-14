edd78c34753c6618a0884093fddfd77131a425e4
Mario Geiger <geiger.mario@gmail.com>
2018-07-14 17:58:03 +0200

    movie: vector + more comple rotation + side view
    Former-commit-id: 3c63390247af84eef17260886128204d5dbc21f9 [formerly 2c0c564c3969c3201b364c070c014da14cb7eb5c]
    Former-commit-id: 05a69f0514d193e62400ef8e5967114cc57ce74f

--- examples/plots/equivariance_video.py
+++ examples/plots/equivariance_video.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from se3cnn.util import time_logging
 import os
+from se3cnn.SO3 import irr_repr
 
 
 class Model(nn.Module):
@@ -20,7 +21,7 @@ class Model(nn.Module):
             (2, 2, 2, 2),
             (0, 2, 2, 2),  # no scalar fields here
             (2, 2, 2, 2),
-            (1,),
+            (1, 1),
         ]
 
         common_block_params = {
@@ -53,25 +54,51 @@ class Model(nn.Module):
         return x
 
 
-def rotate(x, alpha):
+def rotate(x, alpha, beta, gamma):
     t = time_logging.start()
     y = x.cpu().detach().numpy()
-    R = rot(alpha, 0, 0)
-    y = rotate_scalar(y, R)
+    R = rot(alpha, beta, gamma)
+    if x.ndimension() == 4:
+        for i in range(y.shape[0]):
+            y[i] = rotate_scalar(y[i], R)
+    else:
+        y = rotate_scalar(y, R)
     x = x.new_tensor(y)
+    rep = irr_repr(1, alpha, beta, gamma, x.dtype).to(x.device)
+    if x.ndimension() == 4 and x.size(0) == 1 + 3:
+        x[1:] = torch.einsum("ij,jxyz->ixyz", (rep, x[1:]))
     time_logging.end("rotate", t)
     return x
 
 
-def project(x):
-    return x.mean(2).detach().cpu().numpy()
+def project(x, dim):
+    n = x.size(0)
+    assert x.size(1) == n
+    assert x.size(2) == n
+    n = int(0.15 * n)
+    x = x.mean(dim)
+    x = x[n:-n, n:-n]
+    if dim == 0:
+        x = x.t().contiguous()
+    return x.detach().cpu().numpy()
+
+
+def project_vector(x, dim):
+    A = x.new_tensor([[0, 1, 0], [0, 0, 1], [1, 0, 0]]).t()
+    x = torch.einsum("ij,jxyz->ixyz", (A, x))
+    if dim == 0:
+        return project(x[2], 0), project(x[1], 0)
+    if dim == 1:
+        return project(x[0], 1), project(x[2], 1)
+    if dim == 2:
+        return project(x[0], 2), project(x[1], 2)
 
 
 def main():
     t = time_logging.start()
     device = torch.device("cuda:0")
 
-    torch.manual_seed(16)
+    torch.manual_seed(1)
     f = Model().to(device)
 
     x = np.load("{}/119.npy".format(os.path.dirname(__file__)))
@@ -79,60 +106,89 @@ def main():
     x = x.astype(np.float32)
     x = torch.tensor(x, device=device, dtype=torch.float32)
 
+    n = 181
     # x = x[::2, ::2, ::2]
+    # n = 4
 
-    n = 361
-    angles = np.linspace(0, np.pi, n)
+    alpha = np.linspace(0, 2 * np.pi, n)
+    beta = np.linspace(0, np.pi / 2, n)
 
-    fig = plt.figure(figsize=(13, 13))
+    fig = plt.figure(figsize=(4 * 6, 3 * 6))
 
-    ax1 = fig.add_axes([0, 0, .5, .5])  # x
-    ax2 = fig.add_axes([0, .5, .5, .5])  # R(x)
-    ax3 = fig.add_axes([.5, .5, .5, .5])  # f(R(x))
-    ax4 = fig.add_axes([.5, 0, .5, .5])  # R-1(f(R(x)))
+    ax_input = [fig.add_axes([x, 2/3, 1/4, 1/3]) for x in [0, 1/4, 1/2, 3/4]]
+    im_input = [None] * 4
+    ax_scalar = [fig.add_axes([x, 1/3, 1/4, 1/3]) for x in [0, 1/4, 1/2, 3/4]]
+    im_scalar = [None] * 4
+    ax_vector = [fig.add_axes([x, 0, 1/4, 1/3]) for x in [0, 1/4, 1/2, 3/4]]
+    im_vector = [None] * 4
 
     y = f(x)
-    image1 = ax1.imshow(project(x), interpolation='none')
-    image2 = ax2.imshow(project(x), interpolation='none')
-    image3 = ax3.imshow(project(y), interpolation='none')
-    image4 = ax4.imshow(project(y), interpolation='none')
 
-    for image in [image1, image2, image3, image4]:
-        image.set_cmap("summer")
+    imshow_param = {
+        'interpolation': 'none',
+        'origin': 'lower',
+    }
 
-    ax1.text(0.5, 0.99, 'stabilized input', horizontalalignment='center', verticalalignment='top', transform=ax1.transAxes, color='white', fontsize=30)
-    ax2.text(0.5, 0.99, 'input', horizontalalignment='center', verticalalignment='top', transform=ax2.transAxes, color='white', fontsize=30)
-    ax3.text(0.5, 0.99, 'featuremap', horizontalalignment='center', verticalalignment='top', transform=ax3.transAxes, color='white', fontsize=30)
-    ax4.text(0.5, 0.99, 'stabilized featuremap', horizontalalignment='center', verticalalignment='top', transform=ax4.transAxes, color='white', fontsize=30)
-    text = ax2.text(0.5, 0.8, '', horizontalalignment='center', verticalalignment='center', transform=ax2.transAxes, color='white', fontsize=40)
+    quiver_param = {
+        'units': 'xy',
+        'scale': 0.2,
+        'pivot': 'tail',
+        'headwidth': 2.5,
+        'headlength': 5,
+        'minlength': 0,
+        'minshaft': 1
+    }
 
-    for ax in [ax1, ax2, ax3, ax4]:
-        plt.sca(ax)
-        plt.axis('off')
+    for i in range(4):
+        im_input[i] = ax_input[i].imshow(project(x, 0), **imshow_param)
+        im_scalar[i] = ax_scalar[i].imshow(project(y[0], 0), **imshow_param)
+        im_vector[i] = ax_vector[i].quiver(*project_vector(y[1:], 0), **quiver_param)
+
+    for im in im_input + im_scalar + im_vector:
+        im.set_cmap("summer")
+
+    # ax1.text(0.5, 0.99, 'stabilized input', horizontalalignment='center', verticalalignment='top', transform=ax1.transAxes, color='white', fontsize=30)
+    # ax2.text(0.5, 0.99, 'input', horizontalalignment='center', verticalalignment='top', transform=ax2.transAxes, color='white', fontsize=30)
+    # ax3.text(0.5, 0.99, 'featuremap', horizontalalignment='center', verticalalignment='top', transform=ax3.transAxes, color='white', fontsize=30)
+    # ax4.text(0.5, 0.99, 'stabilized featuremap', horizontalalignment='center', verticalalignment='top', transform=ax4.transAxes, color='white', fontsize=30)
+
+    for ax in ax_input + ax_scalar + ax_vector:
+        ax.set_axis_off()
 
     time_logging.end("init", t)
 
     def init():
-        image2.set_data(project(x))
         y = f(x)
-        image3.set_data(project(y))
-        image4.set_data(project(y))
-        text.set_text("")
-        return image2, image3, image4, text
+
+        for i in range(4):
+            dim = 2 if i % 2 == 0 else 0
+            im_input[i].set_data(project(x, dim))
+            im_scalar[i].set_data(project(y[0], dim))
+            im_vector[i].set_UVC(*project_vector(y[1:], dim))
+
+        return tuple(im_input + im_scalar + im_vector)
 
     def animate(i):
         print("rendering {} / {}".format(i, n))
-        alpha = angles[i]
+        a, b = alpha[i], beta[i]
 
-        rx = rotate(x, alpha)
+        rx = rotate(x, a, b, 0)
         frx = f(rx)
-        rfrx = rotate(frx, -alpha)
+        rfrx = rotate(frx, 0, -b, -a)
 
-        image2.set_data(project(rx))
-        image3.set_data(project(frx))
-        image4.set_data(project(rfrx))
-        text.set_text(r"$\frac{{ {} \pi }}{{ {} }}$".format(i, n - 1))
-        return image2, image3, image4, text
+        for i in range(0, 2):
+            dim = 2 if i % 2 == 0 else 0
+            im_input[i].set_data(project(rx, dim))
+            im_scalar[i].set_data(project(frx[0], dim))
+            im_vector[i].set_UVC(*project_vector(frx[1:], dim))
+
+        for i in range(2, 4):
+            dim = 2 if i % 2 == 0 else 0
+            im_input[i].set_data(project(x, dim))
+            im_scalar[i].set_data(project(rfrx[0], dim))
+            im_vector[i].set_UVC(*project_vector(rfrx[1:], dim))
+
+        return tuple(im_input + im_scalar + im_vector)
 
     ani = animation.FuncAnimation(fig, animate, init_func=init, interval=2, blit=True, save_count=n)
 
