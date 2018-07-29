b3215d03600f7d89ae85993ca7b5f832b988e591
Mario Geiger <geiger.mario@gmail.com>
2018-07-29 10:08:44 +0200

    shrec17 deeper model
    Former-commit-id: e808edcaeaa5477d0988cb575135bb3f041230d0 [formerly 05c7c87e7924cd1b5cecc194b31eb0b02533f0b8]
    Former-commit-id: b917da4d0f58f8dd630311d3d1f9bb2a5318fd33

---
+++ examples/shrec17/model_deep.py
@@ -0,0 +1,65 @@
+# pylint: disable=E1101,R,C,W1202
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from se3cnn.blocks import GatedBlock
+
+
+class AvgSpacial(torch.nn.Module):
+    def forward(self, inp):  # pylint: disable=W
+        # inp [batch, features, x, y, z]
+        return inp.view(inp.size(0), inp.size(1), -1).mean(-1)  # [batch, features]
+
+
+class Model(torch.nn.Module):
+
+    def __init__(self, n_out):
+        super().__init__()
+
+        self.int_repr = None
+
+        features = [
+            (1, ), # 64
+            (8, 4, 2), (8, 4, 2), # 34, 38
+            (16, 8, 4), (16, 8, 4), # 21, 25
+            (32, 16, 8), (32, 16, 8), # 15, 19
+            (32, 16, 8), # 12
+            (512, )
+        ]
+
+        common_block_params = {
+            'size': 5,
+            'padding': 4,
+            'normalization': 'batch',
+        }
+
+        block_params = [
+            {'activation': (F.relu, F.sigmoid), 'stride': 2},
+            {'activation': (F.relu, F.sigmoid)},
+            {'activation': (F.relu, F.sigmoid), 'stride': 2},
+            {'activation': (F.relu, F.sigmoid)},
+            {'activation': (F.relu, F.sigmoid), 'stride': 2},
+            {'activation': (F.relu, F.sigmoid)},
+            {'activation': (F.relu, F.sigmoid), 'stride': 2},
+            {'activation': (F.relu, F.sigmoid)},
+        ]
+
+        assert len(block_params) + 1 == len(features)
+
+        blocks = [
+            GatedBlock(features[i], features[i + 1], **common_block_params, **block_params[i])
+            for i in range(len(block_params))
+        ]
+
+        self.sequence = torch.nn.Sequential(
+            *blocks,
+            AvgSpacial(),
+            nn.Linear(features[-1][0], n_out),
+        )
+
+    def forward(self, x):  # pylint: disable=W
+        '''
+        :param x: [batch, features, x, y, z]
+        '''
+        return self.sequence(x)
