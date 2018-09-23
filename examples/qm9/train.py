b2ed5ae251b23ee114dc7f7b2427ae18e02041db
Mario Geiger <geiger.mario@gmail.com>
2018-09-23 23:59:17 +0200

    qm9 draft
    Former-commit-id: e6004449220c140494c727c453e75165cefd009b [formerly c768d8b1db3fff3b0048c3502eb292659b34fdea]
    Former-commit-id: 4864254d202fdd89ba33f351650e10d4e4f65298

---
+++ examples/qm9/train.py
@@ -0,0 +1,155 @@
+# pylint: disable=E1101,R,C,W1202
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import os
+import shutil
+import time
+import logging
+import copy
+import types
+import importlib.machinery
+import numpy as np
+
+from se3cnn.util.dataset.molecules import QM9, center_positions, random_rotate_translate, VoxelizeBlobs
+
+
+def main(log_dir, model_path, dataset, batch_size, learning_rate, num_workers, restore_dir):
+    arguments = copy.deepcopy(locals())
+
+    os.mkdir(log_dir)
+    shutil.copy2(__file__, os.path.join(log_dir, "script.py"))
+    shutil.copy2(model_path, os.path.join(log_dir, "model.py"))
+
+    logger = logging.getLogger("train")
+    logger.setLevel(logging.DEBUG)
+    logger.handlers = []
+    ch = logging.StreamHandler()
+    logger.addHandler(ch)
+    fh = logging.FileHandler(os.path.join(log_dir, "log.txt"))
+    logger.addHandler(fh)
+
+    logger.info("%s", repr(arguments))
+
+    torch.backends.cudnn.benchmark = True
+
+    device = torch.device("cuda:0")
+
+    # Load the model
+    loader = importlib.machinery.SourceFileLoader('model', os.path.join(log_dir, "model.py"))
+    mod = types.ModuleType(loader.name)
+    loader.exec_module(mod)
+
+    model = mod.Model(5, 1).to(device)
+    perceptron = nn.Linear(5, 1)
+
+    if restore_dir is not None:
+        model.load_state_dict(torch.load(os.path.join(restore_dir, "state.pkl")))
+        perceptron.load_state_dict(torch.load(os.path.join(restore_dir, "perceptron.pkl")))
+
+    logger.info("{} paramerters in total".format(sum(x.numel() for x in model.parameters())))
+
+    # Load the dataset
+    render = VoxelizeBlobs(5, 77, 0.2)
+    def transform(positions, qualias, energy):
+        positions = random_rotate_translate(center_positions(positions))
+        x = render(positions, qualias)
+        return x, energy
+
+    train_set = QM9("qm9_data", transform=transform)
+    torch.manual_seed(5)
+    indices = torch.randperm(len(train_set))
+    train_set = torch.utils.data.Subset(train_set, indices[:len(train_set) // 2])
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True, drop_last=True)
+
+    optimizer = torch.optim.Adam(list(model.parameters()) + list(perceptron.parameters()), lr=0)
+
+    def train_step(data, target):
+        model.train()
+        data, target = data.to(device), target.to(device)
+
+        prediction = model(data) + perceptron(data.view(data.size(0), data.size(1), -1).sum(-1))
+        loss = (prediction - target.view(-1)).pow(2).mean()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        return loss.sqrt().item()
+
+    def get_learning_rate(epoch):
+        limits = [500, 1000]
+        lrs = [1, 0.1, 0.01]
+        assert len(lrs) == len(limits) + 1
+        for lim, lr in zip(limits, lrs):
+            if epoch < lim:
+                return lr * learning_rate
+        return lrs[-1] * learning_rate
+
+    dynamics = []
+    epoch = 0
+
+    if restore_dir is not None:
+        dynamics = torch.load(os.path.join(restore_dir, "dynamics.pkl"))
+        epoch = dynamics[-1]['epoch'] + 1
+
+    for epoch in range(epoch, 2000):
+
+        lr = get_learning_rate(epoch)
+        logger.info("learning rate = {} and batch size = {}".format(lr, train_loader.batch_size))
+        for p in optimizer.param_groups:
+            p['lr'] = lr
+
+        total_loss = 0
+        time_before_load = time.perf_counter()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            time_after_load = time.perf_counter()
+            time_before_step = time.perf_counter()
+            loss = train_step(data, target)
+
+            total_loss += loss
+
+            avg_loss = total_loss / (batch_idx + 1)
+
+            logger.info("[{}:{}/{}] RMSE={:.2} <RMSE>={:.2} time={:.2}+{:.2} perceptron={}+{}".format(
+                epoch, batch_idx, len(train_loader),
+                loss, avg_loss,
+                time_after_load - time_before_load,
+                time.perf_counter() - time_before_step,
+                perceptron.weight, perceptron.bias.item()))
+            time_before_load = time.perf_counter()
+
+            dynamics.append({
+                'epoch': epoch,
+                'batch_idx': batch_idx,
+                'step': epoch * len(train_loader) + batch_idx,
+                'learning_rate': lr,
+                'batch_size': len(data),
+                'loss': loss,
+                'avg_loss': avg_loss,
+            })
+
+        torch.save(model.state_dict(), os.path.join(log_dir, "state.pkl"))
+        torch.save(perceptron.state_dict(), os.path.join(log_dir, "perceptron.pkl"))
+        torch.save(dynamics, os.path.join(log_dir, "dynamics.pkl"))
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--log_dir", type=str, required=True)
+    parser.add_argument("--model_path", type=str, required=True)
+    parser.add_argument("--dataset", choices={"test", "val", "train"}, default="train")
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--num_workers", type=int, default=1)
+    parser.add_argument("--learning_rate", type=float, default=0.5)
+    parser.add_argument("--restore_dir", type=str)
+
+    args = parser.parse_args()
+
+    main(**args.__dict__)
+
---
+++ se3cnn/util/dataset/molecules.py
@@ -0,0 +1,150 @@
+# pylint: disable=C,R,E1101,E1102
+import torch
+import torch.utils.data
+import os
+import scipy.io
+import numpy as np
+from se3cnn.util.bounding_sphere import bounding_sphere
+import glob
+from se3cnn import SO3
+
+
+class VoxelizeBlobs:
+    def __init__(self, n_qualias, size, p):
+        # for QM7 size=72 and p=0.3 works fine
+        self.n_qualias = n_qualias
+        self.size = size
+        self.p = p
+        lim = self.p * 0.5 * (self.size - 1)
+        self.a = torch.linspace(-lim, lim, self.size)
+        self.xx = self.a.view(-1, 1, 1).expand(self.size, self.size, self.size)
+        self.yy = self.a.view(1, -1, 1).expand(self.size, self.size, self.size)
+        self.zz = self.a.view(1, 1, -1).expand(self.size, self.size, self.size)
+
+    def __call__(self, positions, qualias):
+        fields = torch.zeros((self.n_qualias, self.size, self.size, self.size))
+
+        for position, qualia in zip(positions, qualias):
+            x, y, z = position
+            x, y, z = float(x), float(y), float(z)
+            density = torch.exp(-((self.xx - x)**2 + (self.yy - y)**2 + (self.zz - z)**2) / (2 * self.p**2))
+            density.div_(density.sum())
+            fields[qualia] += density
+
+        return fields
+
+
+def center_positions(positions):
+    _radius, center = bounding_sphere([pos.numpy() for pos in positions], 1e-6)
+    center = torch.tensor(center)
+    return [pos - center for pos in positions]
+
+
+def random_rotate_translate(positions, rotation=True, translation=1):
+    trans = torch.randn(3)
+    trans = translation * trans / trans.norm()
+    rot = SO3.rot(*torch.rand(3) * 7)
+    return [rot @ pos + trans for pos in positions]
+
+
+class QM7(torch.utils.data.Dataset):
+    url = 'http://quantum-machine.org/data/qm7.mat'
+    mat_file = 'qm7.mat'
+
+    def __init__(self, root, split, transform=None):
+        self.root = os.path.expanduser(root)
+        self.download()
+        self.transform = transform
+
+        data = scipy.io.loadmat(os.path.join(self.root, self.mat_file))
+        indices = data['P'][split]  # shape = (5, 1433)
+        self.positions = data['R'][indices]  # positions, shape = (7165, 23, 3)
+        self.charges = data['Z'][indices].astype(np.int32)  # charge: 5 atom types: 1, 6, 7, 8, 16, shape = (7165, 23)
+        self.energies = data['T'].flatten()[indices]  # labels: atomization energies in kcal/mol, shape = (7165, )
+
+    def __getitem__(self, index):
+        positions, charges, energy = self.positions[index], self.charges[index], self.energies[index]
+        positions = positions[charges > 0]
+        qualias = [{1: 0, 6: 1, 7: 2, 8: 3, 16: 4}[ch] for ch in charges[charges > 0]]
+        positions = [torch.tensor(pos) for pos in positions]
+        if self.transform is not None:
+            return self.transform(positions, qualias, energy)
+        return positions, qualias, energy
+
+    def __len__(self):
+        return len(self.energies)
+
+    def download(self):
+        if not os.path.isdir(self.root):
+            os.makedirs(self.root)
+
+        file_path = os.path.join(self.root, self.mat_file)
+        if not os.path.isfile(file_path):
+            print('Download...')
+            from six.moves import urllib
+            data = urllib.request.urlopen(self.url)
+            with open(file_path, 'wb') as f:
+                f.write(data.read())
+
+
+class QM9(torch.utils.data.Dataset):
+    url = 'https://ndownloader.figshare.com/files/3195389'
+
+    def __init__(self, root, transform=None):
+        self.root = os.path.expanduser(root)
+        self.files = None
+        self.download()
+        self.transform = transform
+
+    def __getitem__(self, index):
+        path = self.files[index]
+        with open(path, "rt") as f:
+            lines = f.readlines()
+        n = int(lines[0])
+        energy = float(lines[1].split()[12])
+        qualias = []
+        positions = []
+        for i in range(2, 2 + n):
+            atom, x, y, z, _ = lines[i].split()
+            qualias.append("CHONF".index(atom))
+            positions.append([float(a.replace('*^', 'e')) for a in [x, y, z]])
+        positions = [torch.tensor(pos) for pos in positions]
+        if self.transform is not None:
+            return self.transform(positions, qualias, energy)
+        return positions, qualias, energy
+
+    def __len__(self):
+        return len(self.files)
+    
+    def download(self):
+        if not os.path.isdir(self.root):
+            os.makedirs(self.root)
+
+        bz2_path = os.path.join(self.root, "data.xyz.tar.bz2")
+        if not os.path.isfile(bz2_path):
+            print("Download...")
+            from six.moves import urllib
+            data = urllib.request.urlopen(self.url)
+            with open(bz2_path, 'wb') as f:
+                f.write(data.read())
+                
+        tar_path = os.path.join(self.root, "data.xyz.tar")
+        if not os.path.isfile(tar_path):
+            print("Decompress...")
+            import bz2
+            file = bz2.BZ2File(bz2_path)
+            with open(tar_path, 'wb') as f:
+                f.write(file.read())
+            file.close()
+        
+        xyz_path = os.path.join(self.root, "data.xyz")
+        if not os.path.isdir(xyz_path):
+            print("Extract...")
+            import tarfile
+            tar = tarfile.open(tar_path)
+            tar.extractall(xyz_path)
+            tar.close()            
+            
+        self.files = sorted(glob.glob(os.path.join(xyz_path, "*.xyz")))
+        assert len(self.files) == 133885
+
