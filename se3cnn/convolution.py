24bc98ea6e9c8b65fb16fee49a4527d9aea468ef
Mario Geiger <geiger.mario@gmail.com>
2018-10-03 17:07:00 +0200

    add test_equivariance
    Former-commit-id: 0437109769649c32a084f17c1c4682d0d1272742 [formerly a3a8af1541acf289265de80fc9c255b6f17c269b]
    Former-commit-id: bb6edd440815edf306cd5f9d64a46491a2ef24b8

--- se3cnn/convolution.py
+++ se3cnn/convolution.py
@@ -22,6 +22,37 @@ class SE3Convolution(torch.nn.Module):
         return torch.nn.functional.conv3d(input, self.kernel(), **self.kwargs)
 
 
+def test_equivaraince():
+    torch.set_default_dtype(torch.float64)
+
+    f = torch.nn.Sequential(
+        SE3Convolution([(1, 0)], [(2, 0), (2, 1), (1, 2)], size=5),
+        SE3Convolution([(2, 0), (2, 1), (1, 2)], [(1, 0)], size=5),
+    )
+
+    def rotate(t):
+        # rotate 90 degrees in plane of axes 2 and 3
+        return torch.flip(t, (2, )).transpose(2, 3)
+
+    def unrotate(t):
+        # undo the rotation by 3 more rotations
+        return rotate(rotate(rotate(t)))
+
+    inp = torch.randn(2, 1, 16, 16, 16)
+    inp_r = rotate(inp)
+
+    diff_inp = (inp - unrotate(inp_r)).abs().max()
+    if diff_inp > 1e-10:
+        raise AssertionError('Sanity check failed, `unrotate` doesn\'t work')
+
+    out = f(inp)
+    out_r = f(inp_r)
+
+    diff_out = (out - unrotate(out_r)).abs().max().item()
+    if diff_out > 1e-10:
+        raise AssertionError('Operations are not rotation-equivariant. Diff', diff_out)
+
+
 def test_normalization(batch, input_size, Rs_in, Rs_out, size):
     conv = SE3Convolution(Rs_in, Rs_out, size)
 
@@ -49,6 +80,7 @@ def test_combination_gradient(Rs_in, Rs_out, size):
 
 
 def main():
+    test_equivaraince()
     test_normalization(3, 15,
                        [(2, 0), (1, 1), (3, 4)],
                        [(2, 0), (2, 1), (1, 2)],
