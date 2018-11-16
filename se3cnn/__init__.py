930a3bbd27b72e7a5b0efe72835814f6ee19fb33
Mario Geiger <geiger.mario@gmail.com>
2018-11-16 13:22:33 +0100

    SE3ConvolutionTranspose

--- se3cnn/__init__.py
+++ se3cnn/__init__.py
@@ -1,7 +1,7 @@
 from . import SO3
 from . import SE3
 from .kernel import SE3Kernel
-from .convolution import SE3Convolution
+from .convolution import SE3Convolution, SE3ConvolutionTranspose
 from .batchnorm import SE3BatchNorm, SE3BNConvolution
 from .groupnorm import SE3GroupNorm, SE3GNConvolution
 from .dropout import SE3Dropout
--- se3cnn/convolution.py
+++ se3cnn/convolution.py
@@ -20,3 +20,21 @@ class SE3Convolution(torch.nn.Module):
 
     def forward(self, input):  # pylint: disable=W
         return torch.nn.functional.conv3d(input, self.kernel(), **self.kwargs)
+
+
+class SE3ConvolutionTranspose(torch.nn.Module):
+    def __init__(self, Rs_in, Rs_out, size, radial_window=kernel.gaussian_window_wrapper, dyn_iso=False, verbose=False, **kwargs):
+        super().__init__()
+
+        self.kernel = SE3Kernel(Rs_out, Rs_in, size, radial_window=radial_window, dyn_iso=dyn_iso, verbose=verbose)
+        self.kwargs = kwargs
+
+    def __repr__(self):
+        return "{name} ({kernel}, kwargs={kwargs})".format(
+            name=self.__class__.__name__,
+            kernel=self.kernel,
+            kwargs=self.kwargs,
+        )
+
+    def forward(self, input):  # pylint: disable=W
+        return torch.nn.functional.conv_transpose3d(input, self.kernel(), **self.kwargs)
