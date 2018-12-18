f5e6e719ab446ba1dfdec4341040f7e9b9029cf3
Tess <blondegeek@gmail.com>
2018-12-17 17:11:16 -0800

    point convolution
    Former-commit-id: 5d89c22d789bdad51a3a16c6c7985fa7101ab760 [formerly f518d0b4e90e299895ca0292c4003b45e6d30413]
    Former-commit-id: a5a914311117faaf7472b87077855c0bdeb9f78c

--- se3cnn/convolution.py
+++ se3cnn/convolution.py
@@ -1,7 +1,8 @@
 # pylint: disable=C,R,E1101,E1102
 import torch
 from se3cnn import SE3Kernel
-from se3cnn import kernel
+from se3cnn.point_kernel import SE3PointKernel
+from se3cnn import kernel, point_kernel
 
 
 class SE3Convolution(torch.nn.Module):
@@ -38,3 +39,34 @@ class SE3ConvolutionTranspose(torch.nn.Module):
 
     def forward(self, input):  # pylint: disable=W
         return torch.nn.functional.conv_transpose3d(input, self.kernel(), **self.kwargs)
+
+
+class SE3PointConvolution(torch.nn.Module):
+    def __init__(self, Rs_in, Rs_out, radii, radial_function=point_kernel.gaussian_radial_function, J_filter_max=10, **kwargs):
+        super().__init__()
+
+        self.kernel = SE3PointKernel(Rs_in, Rs_out, radii, radial_function=radial_function, J_filter_max=J_filter_max)
+        self.kwargs = kwargs
+
+    def __repr__(self):
+        return "{name} ({kernel}, kwargs={kwargs})".format(
+            name=self.__class__.__name__,
+            kernel=self.kernel,
+            kwargs=self.kwargs,
+        )
+
+    def forward(self, input, difference_mat, relative_mask=None):  # pylint: disable=W
+        kernel = self.kernel(difference_mat)
+
+        if len(input.size()) == 2:
+            # No batch dimension
+            output = torch.einsum('ca,dcba->db', (input, kernel))
+        elif len(input.size()) == 3:
+            # Batch dimension
+            # Apply relative_mask to kernel (if examples are not all size N, M)
+            if relative_mask is not None:
+                kernel = torch.einsum('nba,dcnba->dcnba', (relative_mask, kernel))
+            output = torch.einsum('nca,dcnba->ndb', (input, kernel))
+
+        return output
+
