59ef6418e8d63da77cf90f1775c1fa3a1c7c79cf
Tess <blondegeek@gmail.com>
2019-02-23 15:45:08 -0800

    add relative mask to point_utils

--- se3cnn/point_utils.py
+++ se3cnn/point_utils.py
@@ -5,3 +5,6 @@ def difference_matrix(geometry):
     rj = geometry.unsqueeze(-3)  # [1, N, 3]
     rij = ri - rj  # [N, N, 3]
     return rij
+
+def get_relative_mask(mask):
+    return torch.einsum('ti,tj->tij', (mask, mask))
