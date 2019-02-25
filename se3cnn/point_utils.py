c9eb3efb13921d0f4067b1fadc8543addb8677dc
Tess <blondegeek@users.noreply.github.com>
2019-02-24 16:55:43 -0800

    add point_utils for neighbors
    Former-commit-id: fbc76bdcfb561de629d7fe6a76cd04a7bf3509dc [formerly 0f6df71121c3b7194160085cdad7f3c59773f9ca]
    Former-commit-id: a85c22ec8c6fa7915fc671f6643e43d56be17f0c

--- se3cnn/point_utils.py
+++ se3cnn/point_utils.py
@@ -8,3 +8,21 @@ def difference_matrix(geometry):
 
 def get_relative_mask(mask):
     return torch.einsum('ti,tj->tij', (mask, mask))
+
+def neighbor_difference_matrix(neighbors, geometry):
+    N, K = neighbors.shape[-2:]
+    ri = geometry[neighbors]  # [N, K, 3]
+    rj = geometry[torch.arange(N)].unsqueeze(-2)  # [N, 1, 3]
+    rij = ri - rj  # [N, K, 3]
+    return rij
+    
+def neighbor_feature_matrix(neighbors, features):
+    """
+    Args:
+       neighbors: LongTensor of [batch, points, neighbors]
+       features: FloatTensor of [batch, channel, points]
+    
+    Returns:
+       neighbor_features: FloatTensor of [batch, channel, points, neighbors]
+    """
+    return features[:, neighbors]  # [C, N, K]
