231350503c7360475a72c728a9b8b5b542101221
Mario Geiger <geiger.mario@gmail.com>
2019-07-09 15:10:28 -0700

    generic GatedBlock

--- se3cnn/blocks/point_gated_block.py
+++ se3cnn/blocks/point_gated_block.py
@@ -1,20 +1,19 @@
-# pylint: disable=no-member, missing-docstring, invalid-name, redefined-builtin, arguments-differ
+# pylint: disable=no-member, missing-docstring, invalid-name, redefined-builtin, arguments-differ, line-too-long
 import torch
 
-from se3cnn.convolution import SE3PointConvolution
 from se3cnn.non_linearities import ScalarActivation
 
 
-# Added this for tetris_point.py
-# TODO: Refactor so we don't need separate functions for point and image
-class PointGatedBlock(torch.nn.Module):
-    def __init__(self, repr_in, repr_out, Kernel, activation=(None, None)):
+class GatedBlock(torch.nn.Module):
+    def __init__(self, repr_in, repr_out, scalar_activation, gate_activation, Convolution):
+        """
+        :param repr_in: input multiplicities
+        :param repr_out: output multiplicities
+        :param scalar_activation: nonlinear function applied on l=0 channels
+        :param gate_activation: nonlinear function applied on the gates
+        :param Convolution: class of signature (Rs_in, Rs_out)
+        """
         super().__init__()
-
-        if isinstance(activation, tuple):
-            scalar_activation, gate_activation = activation
-        else:
-            scalar_activation, gate_activation = activation, activation
 
         self.repr_out = repr_out
 
@@ -34,30 +33,30 @@ class PointGatedBlock(torch.nn.Module):
             Rs_out_with_gate = Rs_out
             self.gate_act = None
 
-        self.conv = SE3PointConvolution(Kernel(Rs_in, Rs_out_with_gate))
+        self.conv = Convolution(Rs_in, Rs_out_with_gate)
 
 
-    def forward(self, input, difference_matrix, relative_mask=None):
-        y = self.conv(input, difference_matrix, relative_mask)  # [batch, channel, N]
+    def forward(self, *args, **kwargs):
+        """
+        :return: tensor (batch, channel, ...)
+        """
+        y = self.conv(*args, **kwargs)  # [batch, channel, ...]
 
         if self.scalar_act is None and self.gate_act is None:
             return y
 
-        has_batch = difference_matrix.dim() == 4
-        if not has_batch:
-            difference_matrix = difference_matrix.unsqueeze(0)
-            y = y.unsqueeze(0)
-
-        batch, N, _M, _ = difference_matrix.size()
+        batch = y.size(0)
+        size = y.size()[2:]
 
         size_out = sum(mul * (2 * l + 1) for l, mul in enumerate(self.repr_out))
 
         if self.gate_act is not None:
             g = y[:, size_out:]
+            assert g.size(1) == sum(self.repr_out[1:])
             g = self.gate_act(g)
             begin_g = 0  # index of first scalar gate capsule
 
-        z = y.new_empty(batch, size_out, N)
+        z = y.new_empty(batch, size_out, *size)
         begin_y = 0  # index of first capsule
 
         for l, mul in enumerate(self.repr_out):
@@ -66,7 +65,7 @@ class PointGatedBlock(torch.nn.Module):
             dim = 2 * l + 1
 
             # crop out capsules of order l
-            field_y = y[:, begin_y: begin_y + mul * dim]  # [batch, feature * repr, N]
+            field_y = y[:, begin_y: begin_y + mul * dim]  # [batch, feature * repr, ...]
 
             if l == 0:
                 # Scalar activation
@@ -78,18 +77,18 @@ class PointGatedBlock(torch.nn.Module):
                 if self.gate_act is not None:
                     # reshape channels in capsules and capsule entries
                     field_y = field_y.contiguous()
-                    field_y = field_y.view(batch, mul, dim, N)  # [batch, feature, repr, N]
+                    field_y = field_y.view(batch, mul, dim, *size)  # [batch, feature, repr, ...]
 
                     # crop out corresponding scalar gates
-                    field_g = g[:, begin_g: begin_g + mul]  # [batch, feature, N]
+                    field_g = g[:, begin_g: begin_g + mul]  # [batch, feature, ...]
                     begin_g += mul
                     # reshape channels for broadcasting
                     field_g = field_g.contiguous()
-                    field_g = field_g.view(batch, mul, 1, N)  # [batch, feature, 1, N]
+                    field_g = field_g.view(batch, mul, 1, *size)  # [batch, feature, 1, ...]
 
                     # scale non-scalar capsules by gate values
-                    field = field_y * field_g  # [batch, feature, repr, N]
-                    field = field.view(batch, mul * dim, N)  # [batch, feature * repr, N]
+                    field = field_y * field_g  # [batch, feature, repr, ...]
+                    field = field.view(batch, mul * dim, *size)  # [batch, feature * repr, ...]
                     del field_g
                 else:
                     field = field_y
@@ -98,8 +97,5 @@ class PointGatedBlock(torch.nn.Module):
             z[:, begin_y: begin_y + mul * dim] = field
             begin_y += mul * dim
             del field
-
-        if not has_batch:
-            z = z.squeeze(0)
 
         return z
