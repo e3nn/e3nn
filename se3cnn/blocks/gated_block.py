c0e5012a2ca4cecff22bc9bd09d354e092de5db4
Mario Geiger <geiger.mario@gmail.com>
2019-07-09 20:28:58 -0700

    rename files
    Former-commit-id: d4cf3f71bddd61e41519417df13aeea453f5ad50 [formerly f3e7b16ce89d4e737e7608996df2bd241d594a58]
    Former-commit-id: 615a054310a20ecec6986873c84f1e3fb9310e41

--- se3cnn/blocks/gated_block.py
+++ se3cnn/blocks/gated_block.py
@@ -1,171 +1,101 @@
-# pylint: disable=C,R,E1101
-from functools import partial
+# pylint: disable=no-member, missing-docstring, invalid-name, redefined-builtin, arguments-differ, line-too-long
 import torch
-import torch.utils.checkpoint
-from se3cnn import SE3BNConvolution, SE3Convolution, SE3GNConvolution, SE3ConvolutionTranspose
+
 from se3cnn.non_linearities import ScalarActivation
-from se3cnn.dropout import SE3Dropout
-from se3cnn import kernel
-from se3cnn.filter import low_pass_filter
 
 
 class GatedBlock(torch.nn.Module):
-    def __init__(self,
-                 repr_in, repr_out, size, radial_window=kernel.gaussian_window_wrapper,  # kernel params
-                 activation=(None, None), stride=1, padding=0, dilation=1, capsule_dropout_p=None,  # conv/nonlinearity/dropout params
-                 normalization=None, batch_norm_momentum=0.1,  # batch norm params
-                 bias=True, smooth_stride=True, dyn_iso=False, checkpoint=True, verbose=False, transpose=False):
-        '''
-        :param repr_in: tuple with multiplicities of repr. (1, 3, 5, ..., 15)
-        :param repr_out: same but for the output
-        :param int size: the filters are cubes of dimension = size x size x size
-        :param radial_window: radial window function
-        :param activation: (scalar activation, gate activation) which are functions like torch.nn.functional.relu or None
-        :param int stride: stride of the convolution (for torch.nn.functional.conv3d)
-        :param int padding: padding of the convolution (for torch.nn.functional.conv3d)
-        :param int dilation: dilation of the convolution (for torch.nn.functional.conv3d)
-        :param float capsule_dropout_p: dropout probability
-        :param str normalization: "batch", "group", "instance" or None
-        :param float batch_norm_momentum: batch normalization momentum (ignored if no batch normalization)
-        :param bool bias: bias for the gates and scalar fields
-        :param bool smooth_stride: apply a low pass filter before the stride
-        :param bool dyn_iso: initialize with some sort of Dynamical Isometry (inspired by Algo. 2 in https://arxiv.org/abs/1806.05393)
-        '''
+    def __init__(self, repr_in, repr_out, scalar_activation, gate_activation, Convolution):
+        """
+        :param repr_in: input multiplicities
+        :param repr_out: output multiplicities
+        :param scalar_activation: nonlinear function applied on l=0 channels
+        :param gate_activation: nonlinear function applied on the gates
+        :param Convolution: class of signature (Rs_in, Rs_out)
+        """
         super().__init__()
-
-        if type(activation) is tuple:
-            scalar_activation, gate_activation = activation
-        else:
-            scalar_activation, gate_activation = activation, activation
 
         self.repr_out = repr_out
 
         Rs_in = [(m, l) for l, m in enumerate(repr_in)]
-        Rs_out_with_gate = [(m, l) for l, m in enumerate(repr_out)]
+        Rs_out = [(m, l) for l, m in enumerate(repr_out)]
 
-        if (scalar_activation is not None and repr_out[0] > 0):
-            self.scalar_act = ScalarActivation([(repr_out[0], scalar_activation)], bias=bias)
+        if scalar_activation is not None and repr_out[0] > 0:
+            self.scalar_act = ScalarActivation([(repr_out[0], scalar_activation)], bias=False)
         else:
             self.scalar_act = None
 
-        n_non_scalar = sum(repr_out[1:])
-        if gate_activation is not None and n_non_scalar > 0:
-            Rs_out_with_gate.append((n_non_scalar, 0))  # concatenate scalar gate capsules after normal capsules
-            self.gate_act = ScalarActivation([(n_non_scalar, gate_activation)], bias=bias)
+        num_non_scalar = sum(repr_out[1:])
+        if gate_activation is not None and num_non_scalar > 0:
+            Rs_out_with_gate = Rs_out + [(num_non_scalar, 0)]
+            self.gate_act = ScalarActivation([(num_non_scalar, gate_activation)], bias=False)
         else:
+            Rs_out_with_gate = Rs_out
             self.gate_act = None
 
-        if normalization == None:
-            Convolution = SE3Convolution
-        elif normalization == "batch":
-            Convolution = partial(SE3BNConvolution, momentum=batch_norm_momentum)
-        elif normalization == "batch_max":
-            Convolution = partial(SE3BNConvolution, reduce='max', momentum=batch_norm_momentum)
-        elif normalization == "group":
-            Convolution = SE3GNConvolution
-        elif normalization == "instance":
-            Convolution = partial(SE3GNConvolution, Rs_gn=[(1, 2 * n + 1) for n, mul in enumerate(repr_in) for _ in range(mul)])
-        else:
-            raise NotImplementedError('normalization mode unknown')
-
-        if transpose == True:
-           Convolution = SE3ConvolutionTranspose
-
-        self.conv = Convolution(
-            Rs_in=Rs_in,
-            Rs_out=Rs_out_with_gate,
-            size=size,
-            radial_window=radial_window,
-            stride=1 if smooth_stride else stride,
-            padding=padding,
-            dilation=dilation,
-            dyn_iso=dyn_iso,
-            verbose=verbose,
-        )
-
-        self.stride = stride if smooth_stride else 1
-
-        self.dropout = None
-        if capsule_dropout_p is not None:
-            Rs_out_without_gate = [(mul, 2 * n + 1) for n, mul in enumerate(repr_out)]  # Rs_out without gates
-            self.dropout = SE3Dropout(Rs_out_without_gate, capsule_dropout_p)
-
-        self.checkpoint = checkpoint
+        self.conv = Convolution(Rs_in, Rs_out_with_gate)
 
 
-    def forward(self, x):  # pylint: disable=W
+    def forward(self, *args, **kwargs):
+        """
+        :return: tensor (batch, channel, ...)
+        """
+        y = self.conv(*args, **kwargs)  # [batch, channel, ...]
 
-        def gate(y):
-            nbatch = y.size(0)
-            nx = y.size(2)
-            ny = y.size(3)
-            nz = y.size(4)
+        if self.scalar_act is None and self.gate_act is None:
+            return y
 
-            size_out = sum(mul * (2 * n + 1) for n, mul in enumerate(self.repr_out))
+        batch = y.size(0)
+        size = y.size()[2:]
 
-            if self.gate_act is not None:
-                g = y[:, size_out:]
-                g = self.gate_act(g)
-                begin_g = 0  # index of first scalar gate capsule
+        size_out = sum(mul * (2 * l + 1) for l, mul in enumerate(self.repr_out))
 
-            z = y.new_empty((y.size(0), size_out, y.size(2), y.size(3), y.size(4)))
-            begin_y = 0  # index of first capsule
+        if self.gate_act is not None:
+            g = y[:, size_out:]
+            assert g.size(1) == sum(self.repr_out[1:])
+            g = self.gate_act(g)
+            begin_g = 0  # index of first scalar gate capsule
 
-            for n, mul in enumerate(self.repr_out):
-                if mul == 0:
-                    continue
-                dim = 2 * n + 1
+        z = y.new_empty(batch, size_out, *size)
+        begin_y = 0  # index of first capsule
 
-                # crop out capsules of order n
-                field_y = y[:, begin_y: begin_y + mul * dim]  # [batch, feature * repr, x, y, z]
+        for l, mul in enumerate(self.repr_out):
+            if mul == 0:
+                continue
+            dim = 2 * l + 1
 
-                if n == 0:
-                    # Scalar activation
-                    if self.scalar_act is not None:
-                        field = self.scalar_act(field_y)
-                    else:
-                        field = field_y
+            # crop out capsules of order l
+            field_y = y[:, begin_y: begin_y + mul * dim]  # [batch, feature * repr, ...]
+
+            if l == 0:
+                # Scalar activation
+                if self.scalar_act is not None:
+                    field = self.scalar_act(field_y)
                 else:
-                    if self.gate_act is not None:
-                        # reshape channels in capsules and capsule entries
-                        field_y = field_y.contiguous()
-                        field_y = field_y.view(nbatch, mul, dim, nx, ny, nz)  # [batch, feature, repr, x, y, z]
+                    field = field_y
+            else:
+                if self.gate_act is not None:
+                    # reshape channels in capsules and capsule entries
+                    field_y = field_y.contiguous()
+                    field_y = field_y.view(batch, mul, dim, *size)  # [batch, feature, repr, ...]
 
-                        # crop out corresponding scalar gates
-                        field_g = g[:, begin_g: begin_g + mul]  # [batch, feature, x, y, z]
-                        begin_g += mul
-                        # reshape channels for broadcasting
-                        field_g = field_g.contiguous()
-                        field_g = field_g.view(nbatch, mul, 1, nx, ny, nz)  # [batch, feature, repr, x, y, z]
+                    # crop out corresponding scalar gates
+                    field_g = g[:, begin_g: begin_g + mul]  # [batch, feature, ...]
+                    begin_g += mul
+                    # reshape channels for broadcasting
+                    field_g = field_g.contiguous()
+                    field_g = field_g.view(batch, mul, 1, *size)  # [batch, feature, 1, ...]
 
-                        # scale non-scalar capsules by gate values
-                        field = field_y * field_g  # [batch, feature, repr, x, y, z]
-                        field = field.view(nbatch, mul * dim, nx, ny, nz)  # [batch, feature * repr, x, y, z]
-                        del field_g
-                    else:
-                        field = field_y
-                del field_y
+                    # scale non-scalar capsules by gate values
+                    field = field_y * field_g  # [batch, feature, repr, ...]
+                    field = field.view(batch, mul * dim, *size)  # [batch, feature * repr, ...]
+                    del field_g
+                else:
+                    field = field_y
+            del field_y
 
-                z[:, begin_y: begin_y + mul * dim] = field
-                begin_y += mul * dim
-                del field
-
-            return z
-
-
-        # convolution
-        z = self.conv(x)
-
-        # gate
-        if self.scalar_act is not None or self.gate_act is not None:
-            z = torch.utils.checkpoint.checkpoint(gate, z) if self.checkpoint else gate(z)
-
-        # stride
-        if self.stride > 1:
-            z = low_pass_filter(z, self.stride, self.stride)
-
-        # dropout
-        if self.dropout is not None:
-            z = self.dropout(z)
+            z[:, begin_y: begin_y + mul * dim] = field
+            begin_y += mul * dim
+            del field
 
         return z
