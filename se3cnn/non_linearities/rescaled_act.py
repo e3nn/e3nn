0cbc564b3e125a2b64681445d80da09c8fc37418
Mario Geiger <geiger.mario@gmail.com>
2019-07-10 11:32:30 -0700

    activation functions

---
+++ se3cnn/non_linearities/rescaled_act.py
@@ -0,0 +1,24 @@
+# pylint: disable=no-member, missing-docstring, invalid-name
+import torch
+
+
+class Softplus:
+    def __init__(self, beta):
+        x = torch.randn(100000, dtype=torch.float64)
+        self.factor = torch.nn.functional.softplus(x, beta).pow(2).mean().rsqrt().item()
+        self.beta = beta
+
+    def __call__(self, x):
+        return torch.nn.functional.softplus(x, self.beta).mul(self.factor)
+
+
+def sigmoid(x):
+    return x.sigmoid().mul(1.84623)
+
+
+def tanh(x):
+    return x.tanh().mul(1.59254)
+
+
+def relu(x):
+    return x.relu().mul(2 ** 0.5)
