5f364e345926939b1a8fa6eba73956a240d5a0bb
Mario Geiger <geiger.mario@gmail.com>
2019-07-23 15:35:27 -0700

    add install_requires

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+# pylint: disable=not-callable, no-member, invalid-name, line-too-long, wildcard-import, unused-wildcard-import, missing-docstring
 from setuptools import setup, find_packages
 
 setup(
     name='se3cnn',
-    packages=find_packages(exclude=['experiments*'])
+    packages=find_packages(),
+    install_requires=[
+        'scipy',
+        'pytorch',
+        'git+https://github.com/AMLab-Amsterdam/lie_learn',
+    ],
 )
