f9b8235fea2f88fd57036abed30068298a1028d0
Mario Geiger <geiger.mario@gmail.com>
2019-07-23 15:55:19 -0700

    setup.py
    Former-commit-id: 28870a746d84ce700e21d51c9867b75486c718d5 [formerly a9307f6bcefebdb6662a2dcd0996d9f5e34f2c0e]
    Former-commit-id: 0b0088c5567c0823977a65ff0565c0be266871ae

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='se3cnn',
+    url='https://github.com/mariogeiger/se3cnn',
     packages=find_packages(),
     install_requires=[
         'scipy',
         'pytorch',
         'git+https://github.com/AMLab-Amsterdam/lie_learn',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
