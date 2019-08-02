4d7efd0554c32d2443cd529ba08d00e9762b8300
Mario Geiger <geiger.mario@gmail.com>
2019-08-02 12:23:47 -0700

    ShiftedSoftplus

--- se3cnn/non_linearities/rescaled_act.py
+++ se3cnn/non_linearities/rescaled_act.py
@@ -12,6 +12,17 @@ class Softplus:
         return torch.nn.functional.softplus(x, self.beta).mul(self.factor)
 
 
+class ShiftedSoftplus:
+    def __init__(self, beta):
+        x = torch.randn(100000, dtype=torch.float64)
+        self.factor = torch.nn.functional.softplus(x, beta).pow(2).mean().rsqrt().item()
+        self.shift = torch.nn.functional.softplus(torch.zeros(()), beta).item()
+        self.beta = beta
+
+    def __call__(self, x):
+        return torch.nn.functional.softplus(x, self.beta).sub(self.shift).mul(self.factor)
+
+
 def sigmoid(x):
     return x.sigmoid().mul(1.84623)
 
