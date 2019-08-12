51b995ca3dd6236586d4d80d4855078fc9c8d32b
machism0 <benmiller1315@gmail.com>
2019-08-12 16:14:40 -0700

    added arbitrary parity operator.
    Former-commit-id: 33fc34f3193785d8d5a2b4abe6bf0be3172bcca2 [formerly 6b5982cddcb6fc2bfedaa485a8b1d383464a63cb]
    Former-commit-id: 88b1af1575856b8e9bde1703ce865ccad082e388

--- se3cnn/parity.py
+++ se3cnn/parity.py
@@ -3,6 +3,27 @@ import torch
 from se3cnn.util.default_dtype import torch_default_dtype
 
 
+def parity_operator(Rs, dtype=None):
+    """Returns a sum([2*l+1 for _, l, _ in Rs]) length mask. Even parity (1) masks as 1, Odd parity (-1) masks as -1."""
+    assert all([len(i) == 3 for i in Rs])
+
+    group_element = []
+
+    if dtype is None:
+        dtype = torch.get_default_dtype()
+
+    with torch_default_dtype(dtype):
+        for m, l, p in Rs:
+            if p == 1:
+                parity = torch.tensor(1).repeat(2 * l + 1).repeat(m)
+            elif p == -1:
+                parity = torch.tensor(-1).repeat(2 * l + 1).repeat(m)
+            else:
+                raise ValueError('Parity must be defined as -1 or 1 for each p in Rs.')
+            group_element.append(parity)
+        return torch.cat(group_element)
+
+
 def spherical_harmonic_parity_operator(Rs, dtype=None):
     """The parity group element for the coefficients of a spherical harmonic signal."""
     assert all([len(i) == 2 for i in Rs])
