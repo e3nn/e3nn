baf9c36822545dbbfe3e57cc3053580b892e97b5
Mario Geiger <geiger.mario@gmail.com>
2019-08-15 08:07:10 -0700

    Norm module

---
+++ se3cnn/non_linearities/norm.py
@@ -0,0 +1,37 @@
+# pylint: disable=invalid-name, arguments-differ, missing-docstring, line-too-long, no-member
+import torch
+
+from se3cnn import SO3
+
+
+class Norm(torch.nn.Module):
+    def __init__(self, Rs, normalization='norm'):
+        super().__init__()
+
+        Rs = SO3.normalizeRs(Rs)
+        n = sum(mul for mul, _, _ in Rs)
+        self.Rs_in = Rs
+        self.Rs_out = [(n, 0, +1)]
+        self.normalization = normalization
+
+
+    def forward(self, features):
+        '''
+        :param features: [..., channels]
+        '''
+        *size, n = features.size()
+        output = []
+        index = 0
+        for mul, l, _ in self.Rs_in:
+            sub = features.narrow(-1, index, mul * (2 * l + 1)).reshape(*size, mul, 2 * l + 1)  # [..., u, i]
+            index += mul * (2 * l + 1)
+
+            sub = sub.norm(2, dim=-1)  # [..., u]
+
+            if self.normalization == 'component':
+                sub.div_((2 * l + 1) ** 0.5)
+
+            output.append(sub)
+        assert index == n
+
+        return torch.cat(output, dim=-1)
