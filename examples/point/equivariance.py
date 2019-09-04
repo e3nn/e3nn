892002897097d40e8b5dc84359734df5cb118eda
machism0 <benmiller1315@gmail.com>
2019-09-04 12:39:37 -0700

    rotation equivariance check, polymorphic checking of cg calculations
    Former-commit-id: 306c4686faa6f382c147a772caa4d3408396f0fd [formerly bef19cd1615e4b1eaa73fbf17d1cac889c1ca527]
    Former-commit-id: 262c4cdd1af2cb3f7b9554912d74db50fa98b7ed

--- examples/point/equivariance.py
+++ examples/point/equivariance.py
@@ -11,73 +11,70 @@ from se3cnn.point.radial import ConstantRadialModel
 from se3cnn.SO3 import normalizeRs, rep, rot
 
 
-def check_rotation():
-    pass
+class RandomDataRotation(object):
+    def __init__(self, batch, n_atoms, Rs_in, Rs_out):
+        self.abc = torch.randn(3)  # Rotation seed of euler angles.
+        self.rot_geo = rot(*self.abc)
+        self.D_in = rep(Rs_in, *self.abc)
+        self.D_out = rep(Rs_out, *self.abc)
+
+        c = sum([mul * (2 * l + 1) for mul, l, _ in Rs_in])
+        self.feat = torch.randn(batch, n_atoms, c)  # Transforms with wigner D matrix
+        self.geo = torch.randn(batch, n_atoms, 3)  # Transforms with rotation matrix.
 
 
-def check_rotation_parity():
+def check_rotation(batch: int = 10, n_atoms: int = 25):
+    # Setup the network.
     K = partial(Kernel, RadialModel=ConstantRadialModel)
     C = partial(Convolution, K)
-
-    Rs_in = [(1, 0, 1)]
-    m = GatedBlockParity(
-        C,
+    Rs_in = [(1, 0), (1, 1)]
+    Rs_out = [(1, 0), (1, 1), (1, 2)]
+    m = GatedBlock(
         Rs_in,
-        [(4, 0, 1)],
-        [(-1, relu)],
-        [(8, 0, 1)],
-        [(-1, sigmoid)],
-        [(4, 1, -1), (4, 2, 1)]
+        Rs_out,
+        scalar_activation=sigmoid,
+        gate_activation=sigmoid,
+        Operation=C
     )
-    # Rs_in and m.Rs_out
 
-
-def check_network_equivariance(network, Rs_in, Rs_out, vector_output: bool = False, batch: int = 2,
-                               n_atoms: int = 6) -> bool:
-    """
-    from user func(input_data, alpha, beta, gamma, parity)
-    from user func(output_data,  alpha, beta, gamma, parity)
-
-    Checks rotation equivariance for a function. Network output transforms with the wigner d matrices.
-
-    :param network: Often the forward pass of an object inheriting from torch.nn.Module
-    :param Rs_in: Rs for the input of the network.
-    :param Rs_out: Rs for the output of the network.
-    :param vector_output: When True, the network outputs vectors which have already been converted to xyz basis.
-    :param batch: Batch size of test case.
-    :param n_atoms: Number of atoms of test case.
-    :return: None
-    """
-
-    def check_parity(Rs):
-        """Does Rs require a test of parity equivariance."""
-        if any(p != 0 for _, _, p in normalizeRs(Rs)):
-            return True
-        else:
-            return False
-
-    # Make sure that all parameters are on the same device. Set that to calculation device.
-    devices = [i.device for i in network.parameters()]
-    device = devices[0]
-    assert all([device == i for i in devices])
-
-    Rs_in = normalizeRs(Rs_in)
-    Rs_out = normalizeRs(Rs_out)
-    parity = check_parity(Rs_in) or check_parity(Rs_out)
-
-    abc = torch.randn(3, device=device)  # Rotation seed of euler angles.
+    # Setup the data. The geometry, input features, and output features must all rotate.
+    abc = torch.randn(3)  # Rotation seed of euler angles.
+    rot_geo = rot(*abc)
     D_in = rep(Rs_in, *abc)
-    geo_rotation_matrix = -rot(*abc) if parity else rot(*abc)  # Geometry is odd parity.
     D_out = rep(Rs_out, *abc)
 
-    c = sum([mul * (2 * l + 1) for mul, l, _ in Rs_in])
-    feat = torch.randn(batch, n_atoms, c, device=device)  # Transforms with wigner D matrix
-    geo = torch.randn(batch, n_atoms, 3, device=device)  # Transforms with rotation matrix.
+    c = sum([mul * (2 * l + 1) for mul, l in Rs_in])
+    feat = torch.randn(batch, n_atoms, c)  # Transforms with wigner D matrix
+    geo = torch.randn(batch, n_atoms, 3)  # Transforms with rotation matrix.
 
-    F = network(feat, geo)
-    if vector_output:
-        RF = torch.einsum("ij,zkj->zki", geo_rotation_matrix, F)
-    else:
-        RF = torch.einsum("ij,zkj->zki", D_out, F)
-    FR = network(feat @ D_in.t(), geo @ geo_rotation_matrix.t())  # [batch, feat, N]
+    # Test equivariance.
+    F = m(feat, geo)
+    RF = torch.einsum("ij,zkj->zki", D_out, F)
+    FR = m(feat @ D_in.t(), geo @ rot_geo.t())
     return (RF - FR).norm() < 10e-5 * RF.norm()
+
+
+def check_rotation_parity(batch: int = 10, n_atoms: int = 25):
+    # Setup the network.
+    K = partial(Kernel, RadialModel=ConstantRadialModel)
+    C = partial(Convolution, K)
+    Rs_in = [(1, 0, 1)]
+    m = GatedBlockParity(
+        Operation=C,
+        Rs_in=Rs_in,
+        Rs_scalars=[(4, 0, 1)],
+        act_scalars=[(-1, relu)],
+        Rs_gates=[(8, 0, 1)],
+        act_gates=[(-1, sigmoid)],
+        Rs_nonscalars=[(4, 1, -1), (4, 2, 1)]
+    )
+    Rs_out = m.Rs_out
+
+    # Setup the data. The geometry, input features, and output features must all rotate.
+
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    check_rotation()
+    # check_rotation_parity()
