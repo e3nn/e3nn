62910f64bd117dc4ca6f6d01b6f068792b2b71d0
machism0 <benmiller1315@gmail.com>
2019-09-06 15:31:34 -0700

    removed a bad class
    Former-commit-id: 8831ace4a87856f74b4a2b5578f76ad78df91b6b [formerly 7eaf14f6147160898ed6afd046a553f5ebd857ed]
    Former-commit-id: 384c5feeff0125c1f5e60f72c418cd3acecea8ce

--- examples/point/equivariance.py
+++ examples/point/equivariance.py
@@ -11,18 +11,6 @@ from se3cnn.point.radial import ConstantRadialModel
 from se3cnn.SO3 import normalizeRs, rep, rot
 
 
-class RandomDataRotation(object):
-    def __init__(self, batch, n_atoms, Rs_in, Rs_out):
-        self.abc = torch.randn(3)  # Rotation seed of euler angles.
-        self.rot_geo = rot(*self.abc)
-        self.D_in = rep(Rs_in, *self.abc)
-        self.D_out = rep(Rs_out, *self.abc)
-
-        c = sum([mul * (2 * l + 1) for mul, l, _ in Rs_in])
-        self.feat = torch.randn(batch, n_atoms, c)  # Transforms with wigner D matrix
-        self.geo = torch.randn(batch, n_atoms, 3)  # Transforms with rotation matrix.
-
-
 def check_rotation(batch: int = 10, n_atoms: int = 25):
     # Setup the network.
     K = partial(Kernel, RadialModel=ConstantRadialModel)
