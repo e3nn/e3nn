2bb3918088e8c92f48d6cd9d500a32224870f70c
L-sky <lapchevsky.k@gmail.com>
2019-11-14 14:35:56 +0200

    rsh: extended to handle float32
    Former-commit-id: 6b82c819d9bc064d981b13f904c1584359c3a3ab [formerly 54628d1f95641391f07634632165ab47ed97ef81]
    Former-commit-id: b434b9240cc44b02b6dd3f814b532010465757f5

--- src/real_spherical_harmonics/rsh_bind.cpp
+++ src/real_spherical_harmonics/rsh_bind.cpp
@@ -1,22 +1,24 @@
 #include <torch/extension.h>
 
 void real_spherical_harmonics_cuda(
-        torch::Tensor output_placeholder,
+        torch::Tensor Ys,
         torch::Tensor radii);
 
 // C++ interface
 
 #define CHECK_CUDA(x) AT_ASSERTM(x.type().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) AT_ASSERTM(x.is_contiguous(), #x " must be contiguous")
-#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
+#define CHECK_DTYPE(x) AT_ASSERTM(x.dtype() == torch::kFloat64 || x.dtype() == torch::kFloat32, #x " must be either float32 or float64")
+#define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x); CHECK_DTYPE(x);
 
 void real_spherical_harmonics(
-        torch::Tensor output_placeholder,
+        torch::Tensor Ys,
         torch::Tensor radii){
-    CHECK_INPUT(output_placeholder);
+    CHECK_INPUT(Ys);
     CHECK_INPUT(radii);
+    AT_ASSERTM(Ys.dtype() == radii.dtype(), "output and input in rsh should have the same data type");
 
-    real_spherical_harmonics_cuda(output_placeholder, radii);
+    real_spherical_harmonics_cuda(Ys, radii);
 }
 
 
