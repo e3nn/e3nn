7e91d5604af454b96865cc2aa17b3c7a1cae11c3
Mario Geiger <geiger.mario@gmail.com>
2019-12-12 18:03:07 +0100

    S2Activation
    Former-commit-id: e32f3ceab43ad410a95ec92fb02b61eddfa39d46 [formerly 9f8dd17dcc7dd039cc3c9e792a5a0b547f28e1f6]
    Former-commit-id: 339baaa94f3cce8e4ad38f054b917ba2cd91e602

---
+++ tests/non_linearities/s2_tests.py
@@ -0,0 +1,47 @@
+# pylint: disable=not-callable, no-member, invalid-name, line-too-long, wildcard-import, unused-wildcard-import, missing-docstring
+import itertools
+import unittest
+from functools import partial
+
+import torch
+
+from e3nn import SO3
+from e3nn.non_linearities.s2 import S2Activation
+
+
+class Tests(unittest.TestCase):
+
+    def test_equivariance(self):
+        torch.set_default_dtype(torch.float64)
+        def test(Rs, act):
+            x = torch.randn(55, sum(2 * l + 1 for _, l, _ in Rs))
+            ac = S2Activation(Rs, act, 1000)
+
+            y1 = ac(x, dim=-1) @ SO3.rep(ac.Rs_out, 0, 0, 0, -1).T
+            y2 = ac(x @ SO3.rep(Rs, 0, 0, 0, -1).T, dim=-1)
+            self.assertLess((y1 - y2).abs().max(), 1e-10)
+
+        L = 5
+        Rss = [
+            [(1, l, -(-1) ** l) for l in range(L)],
+            [(1, l, (-1) ** l) for l in range(L)],
+            [(1, l, -1) for l in range(L)],
+            [(1, l, 1) for l in range(L)],
+        ]
+
+        acts = [torch.tanh, torch.abs]
+
+        for Rs, act in itertools.product(Rss, acts):
+            test(Rs, act)
+
+        Rss = [
+            [(1, l, (-1) ** l) for l in range(L)],
+            [(1, l, 1) for l in range(L)],
+        ]
+
+        acts = [torch.relu, torch.sigmoid]
+
+        for Rs, act in itertools.product(Rss, acts):
+            test(Rs, act)
+
+unittest.main()
