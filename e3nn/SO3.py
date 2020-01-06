cbf09186df98705ed99f7f9f988f0531646d9d0c
machsim0 <benmiller1315@gmail.com>
2020-01-06 23:52:58 +0100

    remove unecessary lines
    Former-commit-id: 052917d4e956a2f179f3e68f7208a6155673d92e [formerly 4ae436e52cdc9bda238a54bc58fd4c17c5f4f31f]
    Former-commit-id: f377c18111affd20bdf04c40e23f16f8daa3e058

--- e3nn/SO3.py
+++ e3nn/SO3.py
@@ -314,8 +314,6 @@ def spherical_harmonics_xyz(order, xyz, sph_last=False, dtype=None, device=None)
 
     if not torch.is_tensor(xyz):
         xyz = torch.tensor(xyz, dtype=torch.float64)
-    elif xyz.dtype is not torch.float64:
-        xyz = xyz.to(dtype=torch.float64)
 
     with torch_default_dtype(torch.float64):
         if device.type == 'cuda' and max(order) <= 10:
