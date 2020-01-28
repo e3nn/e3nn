9b5542b374e8281bd26c099dd62118a2a45ef561
Mario Geiger <geiger.mario@gmail.com>
2020-01-28 18:54:02 +0100

    TensorProduct
    Former-commit-id: 56bda93422115a3200275da6ea4b776c061e0010 [formerly fd6a1bcbaeceb20eb6f9ec8f461138ac5f4b1589]
    Former-commit-id: 6de91989b32f720d28cfc10c1f756885c2d9e98b

--- e3nn/util/manipulations.py
+++ e3nn/util/manipulations.py
@@ -1,135 +1,57 @@
+# pylint: disable=arguments-differ, no-member, missing-docstring, invalid-name, line-too-long
+from functools import reduce
+
 import torch
-from collections import defaultdict
-import e3nn
-import e3nn.SO3 as SO3
+
+from e3nn import SO3
 
 
-def split_Rs(Rs):
+class SortSphericalSignals(torch.nn.Module):
     """
-    Given Rs with all same mulitplicity, split into tensor where multiplicity
-    is separate channel.
-    Returns:
+    Sort the representation order of a tensor
     """
-    muls, Ls = zip(*Rs)
-    total = sum([m * (2 * L + 1) for m, L in Rs])
-    single = sum([(2 * L + 1) for m, L in Rs])
-    M = list(set(muls))[0]
-    rearrange = torch.zeros(M, single, total)
-    if len(set(muls)) != 1:
-        raise ValueError("All L's must have same multiplicity.")
-    # Assumes all L's have same multiplicity
-    # Take [batch, N, C] to [batch, N, M, D]
-    start = 0
-    indices = []
-    
-    for L in Ls:
-        rep = 2 * L + 1
-        index = (start, start + rep)
-        indices.append(index)
-        start += rep * M
-    
-    for m in range(M):
-        new = 0
-        for L, index in zip(Ls, indices):
-            rep = 2 * L + 1
-            rearrange[m, new : new + rep, m * rep + index[0] : m * rep + index[1]] = torch.eye(rep)
-            new += rep
-            
-    return rearrange, indices
+    def __init__(self, Rs):
+        super().__init__()
+
+        self.Rs_in = SO3.normalizeRs(Rs)
+        xs = []
+
+        j = 0  # input offset
+        for mul, l, p in self.Rs_in:
+            d = mul * (2 * l + 1)
+            xs.append((l, p, mul, j, d))
+            j += d
+
+        mixing_matrix = torch.zeros(j, j)
+
+        Rs_out = []
+        i = 0  # output offset
+        for l, p, mul, j, d in sorted(xs):
+            Rs_out.append((mul, l, p))
+            mixing_matrix[i:i+d, j:j+d] = torch.eye(d)
+            i += d
+
+        self.Rs_out = SO3.normalizeRs(Rs_out)
+        self.register_buffer('mixing_matrix', mixing_matrix)
+
+    def forward(self, x):
+        """
+        :param x: tensor [batch, feature, ...]
+        """
+        output = torch.einsum('ij,zja->zia', (self.mixing_matrix, x.flatten(2))).contiguous()
+        return output.view(*x.size())
 
 
-def simplify_Rs(Rs):
+class ConcatenateSphericalSignals(torch.nn.Module):
     """
-    Return simplifed Rs and transformation matrix.
-    Currently ignores parity!
+    Concatenate tensors
     """
-    try:
-        mults, Ls, ps = zip(*Rs)
-    except:
-        mults, Ls = zip(*Rs)
-    totals = [mult * (2 * L + 1) for mult, L, p in Rs]
-    shuffle = torch.zeros(sum(totals), sum(totals))
+    def __init__(self, *Rs):
+        super().__init__()
+        Rs = reduce(list.__add__, Rs, [])
+        self.sort = SortSphericalSignals(Rs)
+        self.Rs_out = self.sort.Rs_out
 
-    # Get total number of multiplicities by L
-    d = defaultdict(int)
-    for mult, L, p in Rs:
-        d[L] += mult
-
-    # Rs_new grouped and sorted by L
-    Rs_new = sorted([x[::-1] for x in d.items()], key=lambda x: x[1])
-    new_mults, new_Ls = zip(*Rs_new)
-    new_totals = [mult * (2 * L + 1) for mult, L in Rs_new]
-
-    # indices for different mults
-    tot_indices = [[sum(totals[0:i]), sum(totals[0:i + 1])] for i in range(len(totals))]
-    new_tot_indices = [[sum(new_totals[0:i]), sum(new_totals[0:i + 1])] for i in range(len(new_totals))]
-
-    # group indices by L
-    d_t_i = defaultdict(list)
-    for L, index in zip(Ls, tot_indices):
-        d_t_i[L].append(index)
-
-    #
-    total_bounds = sorted(list(d_t_i.items()), key=lambda x: x[0])
-    new_total_bounds = list(zip(new_Ls, new_tot_indices))
-
-    for old_indices, (L, new_index) in zip(total_bounds, new_total_bounds):
-        old_indices_list = [torch.arange(i[0], i[1]) for i in old_indices[1]]
-        new_index_list = torch.arange(new_index[0], new_index[1])
-        shuffle[new_index_list, torch.cat(old_indices_list)] = 1
-
-    return Rs_new, shuffle
-
-
-def get_truncated_shuffled_Q(Rs):
-    L_max = max(L for mul, L in Rs)
-    Rs_out, Q = SO3.reduce_tensor_product(Rs, Rs)
-
-    Rs_new, shuffle = simplify_Rs(Rs_out)
-    Rs_new_trunc = [(mul, L) for mul, L in Rs_new if L <= L_max]
-
-    new_Q = torch.einsum('lk,ijk->ijl', (shuffle, Q))
-    shape = new_Q.shape
-    total = sum([mul * (2 * L + 1) for mul, L in Rs_new_trunc])
-
-    new_Q_trunc = new_Q[:, :, :total]
-    return Rs_new_trunc, new_Q_trunc
-
-
-def test_split_Rs():
-    Rs = [(2, 0)]
-    rearrange, indices = split_Rs(Rs)
-    assert (rearrange == torch.eye(2).unsqueeze(-2)).all()
-    
-    Rs = [(3, 0), (2, 1)]
-    try:
-        rearrange, indices = split_Rs(Rs)
-        assert False
-    except:
-        assert True
-    
-    Rs = [(3, 0), (3, 1)]
-    rearrange, indices = split_Rs(Rs)
-    assert (rearrange[range(3), 0, range(3)] == 1.).all()
-    assert (rearrange[0, 1:, 3:6] == torch.eye(3)).all() == True
-    assert (rearrange[1, 1:, 6:9] == torch.eye(3)).all() == True
-    assert (rearrange[2, 1:, 9:12] == torch.eye(3)).all() == True
-
-
-def test_simplify_Rs():
-    L_max = 5
-    mul = 7
-    Rs_single = [(1, L, 0) for L in range(L_max + 1)]
-    Rs = [(mul, L) for L in range(L_max + 1)]
-    Rs_out, shuffle = simplify_Rs(Rs_single * mul)
-    assert Rs_out == Rs
-    
-    Rs_orig = [(1, 0, 0), (3, 2, 0), (2, 1, 0)]
-    Rs_check = [(1, 0), (2, 1), (3, 2)]
-    Rs_out, shuffle = simplify_Rs(Rs_orig)
-    assert Rs_out == Rs_check
-    
-    Rs_orig = [(1, 0, 0), (3, 2, 0), (2, 1, 0), (2, 0, 0)]
-    Rs_check = [(3, 0), (2, 1), (3, 2)]
-    Rs_out, shuffle = simplify_Rs(Rs_orig)
-    assert Rs_out == Rs_check
+    def forward(self, *signals):
+        combined = torch.cat(signals, dim=1)
+        return self.sort(combined)
