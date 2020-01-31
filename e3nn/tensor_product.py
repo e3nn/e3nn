2d931f5eb4d41fc15764013e0796edd6cc86a785
Mario Geiger <geiger.mario@gmail.com>
2020-01-31 13:58:38 +0100

    tensor_product
    Former-commit-id: cbd34a12d09d4808e01b34509f65001cdf3d69d7 [formerly ceb039f1fe0923f5d04cdab01009bd5ff7e06e62]
    Former-commit-id: bb825d8c68d51570528e51678b42a488671b7b28

--- e3nn/tensor_product.py
+++ e3nn/tensor_product.py
@@ -1,25 +1,65 @@
 # pylint: disable=invalid-name, arguments-differ, missing-docstring, line-too-long, no-member
 import torch
-
+from e3nn.linear import Linear
 from e3nn import SO3
 
 
-class Multiplication(torch.nn.Module):
+def tensor_product(Rs_1, Rs_2, get_l_output=SO3.selection_rule):
     """
-    Multiplication
+    :return: Rs_out, matrix
+
+    m_kij A_i B_j
+    """
+    Rs_1 = SO3.simplifyRs(Rs_1)
+    Rs_2 = SO3.simplifyRs(Rs_2)
+
+    Rs_out = []
+    for mul_1, l_1, p_1 in Rs_1:
+        for mul_2, l_2, p_2 in Rs_2:
+            for l in get_l_output(l_1, l_2):
+                Rs_out.append((mul_1 * mul_2, l, p_1 * p_2))
+
+    Rs_out = SO3.simplifyRs(Rs_out)
+
+    mixing_matrix = torch.zeros(SO3.dimRs(Rs_out), SO3.dimRs(Rs_1), SO3.dimRs(Rs_2))
+
+    index_out = 0
+
+    index_1 = 0
+    for mul_1, l_1, _p_1 in Rs_1:
+        dim_1 = mul_1 * (2 * l_1 + 1)
+
+        index_2 = 0
+        for mul_2, l_2, _p_2 in Rs_2:
+            dim_2 = mul_2 * (2 * l_2 + 1)
+            for l in get_l_output(l_1, l_2):
+                dim_out = mul_1 * mul_2 * (2 * l + 1)
+                C = SO3.clebsch_gordan(l, l_1, l_2, cached=True) * (2 * l + 1) ** 0.5
+                I = torch.eye(mul_1 * mul_2).view(mul_1 * mul_2, mul_1, mul_2)
+                m = torch.einsum("wuv,kij->wkuivj", I, C).view(dim_out, dim_1, dim_2)
+                mixing_matrix[index_out:index_out+dim_out, index_1:index_1+dim_1, index_2:index_2+dim_2] = m
+                index_out += dim_out
+
+            index_2 += dim_2
+        index_1 += dim_1
+
+    return Rs_out, mixing_matrix
+
+
+# TODO test performance
+class TensorProduct(torch.nn.Module):
+    """
+    (A x B)_k =  C_ijk A_i B_j
 
     [(2, 0), (1, 1)] x [(1, 1), (2, 0)] = [(2, 1), (5, 0), (1, 1), (1, 2), (2, 1)]
     """
-    def __init__(self, Rs_1, Rs_2, get_l_output=None):
+    def __init__(self, Rs_1, Rs_2, get_l_output=SO3.selection_rule):
         super().__init__()
 
-        def get_ls(l_1, l_2):
-            return list(range(abs(l_1 - l_2), l_1 + l_2 + 1))
+        self.get_ls = get_l_output
 
-        self.get_ls = get_l_output if get_l_output is not None else get_ls
-
-        self.Rs_1 = SO3.normalizeRs(Rs_1)
-        self.Rs_2 = SO3.normalizeRs(Rs_2)
+        self.Rs_1 = SO3.simplifyRs(Rs_1)
+        self.Rs_2 = SO3.simplifyRs(Rs_2)
 
         Rs_out = []
         for mul_1, l_1, p_1 in self.Rs_1:
@@ -31,7 +71,7 @@ class Multiplication(torch.nn.Module):
                         C = SO3.clebsch_gordan(l, l_1, l_2, dtype=torch.float64).view(2 * l + 1, 2 * l + 1) * (2 * l + 1) ** 0.5
                         assert (C - torch.eye(2 * l + 1, dtype=torch.float64)).abs().max() < 1e-10, C.numpy().round(3)
 
-        self.Rs_out = SO3.normalizeRs(Rs_out)
+        self.Rs_out = SO3.simplifyRs(Rs_out)
 
 
     def forward(self, features_1, features_2):
@@ -59,7 +99,7 @@ class Multiplication(torch.nn.Module):
 
                 for l in self.get_ls(l_1, l_2):
                     C = SO3.clebsch_gordan(l, l_1, l_2, cached=True, like=f_1) * (2 * l + 1) ** 0.5
-                    out = torch.einsum("ijk,zuj,zvk->zuvi", C, f_1, f_2).view(batch, mul_1 * mul_2 * (2 * l + 1))  # [z, w, i]
+                    out = torch.einsum("ijk,zuj,zvk->zuvi", C, f_1, f_2).reshape(batch, mul_1 * mul_2 * (2 * l + 1))  # [z, w, i]
                     output.append(out)
         assert index_1 == features_1.size(1)
         assert index_2 == features_2.size(1)
@@ -68,17 +108,72 @@ class Multiplication(torch.nn.Module):
         return output.view(*size_1, -1)
 
 
-class ElementwiseMultiplication(torch.nn.Module):
+def elementwise_tensor_product(Rs_1, Rs_2, get_l_output=SO3.selection_rule):
     """
-    Elementwise multiplication
+    :return: Rs_out, matrix
 
+    m_kij A_i B_j
+    """
+    Rs_1 = SO3.simplifyRs(Rs_1)
+    Rs_2 = SO3.simplifyRs(Rs_2)
+
+    assert sum(mul for mul, _, _ in Rs_1) == sum(mul for mul, _, _ in Rs_2)
+
+    i = 0
+    while i < len(Rs_1):
+        mul_1, l_1, p_1 = Rs_1[i]
+        mul_2, l_2, p_2 = Rs_2[i]
+
+        if mul_1 < mul_2:
+            Rs_2[i] = (mul_1, l_2, p_2)
+            Rs_2.insert(i + 1, (mul_2 - mul_1, l_2, p_2))
+
+        if mul_2 < mul_1:
+            Rs_1[i] = (mul_2, l_1, p_1)
+            Rs_1.insert(i + 1, (mul_1 - mul_2, l_1, p_1))
+        i += 1
+
+    Rs_out = []
+    for (mul, l_1, p_1), (mul_2, l_2, p_2) in zip(Rs_1, Rs_2):
+        assert mul == mul_2
+        for l in get_l_output(l_1, l_2):
+            Rs_out.append((mul, l, p_1 * p_2))
+
+    Rs_out = SO3.simplifyRs(Rs_out)
+
+    mixing_matrix = torch.zeros(SO3.dimRs(Rs_out), SO3.dimRs(Rs_1), SO3.dimRs(Rs_2))
+
+    index_out = 0
+    index_1 = 0
+    index_2 = 0
+    for (mul, l_1, p_1), (mul_2, l_2, p_2) in zip(Rs_1, Rs_2):
+        assert mul == mul_2
+        dim_1 = mul * (2 * l_1 + 1)
+        dim_2 = mul * (2 * l_2 + 1)
+
+        for l in get_l_output(l_1, l_2):
+            dim_out = mul * (2 * l + 1)
+            C = SO3.clebsch_gordan(l, l_1, l_2, cached=True) * (2 * l + 1) ** 0.5
+            I = torch.einsum("uv,wu->wuv", torch.eye(mul), torch.eye(mul))
+            m = torch.einsum("wuv,kij->wkuivj", I, C).view(dim_out, dim_1, dim_2)
+            mixing_matrix[index_out:index_out+dim_out, index_1:index_1+dim_1, index_2:index_2+dim_2] = m
+            index_out += dim_out
+
+        index_1 += dim_1
+        index_2 += dim_2
+
+    return Rs_out, mixing_matrix
+
+
+class ElementwiseTensorProduct(torch.nn.Module):
+    """
     [(2, 0), (1, 1)] x [(1, 1), (2, 0)] = [(1, 1), (1, 0), (1, 1)]
     """
-    def __init__(self, Rs_1, Rs_2):
+    def __init__(self, Rs_1, Rs_2, get_l_output=SO3.selection_rule):
         super().__init__()
 
-        Rs_1 = SO3.normalizeRs(Rs_1)
-        Rs_2 = SO3.normalizeRs(Rs_2)
+        Rs_1 = SO3.simplifyRs(Rs_1)
+        Rs_2 = SO3.simplifyRs(Rs_2)
         assert sum(mul for mul, _, _ in Rs_1) == sum(mul for mul, _, _ in Rs_2)
 
         i = 0
@@ -97,11 +192,12 @@ class ElementwiseMultiplication(torch.nn.Module):
 
         self.Rs_1 = Rs_1
         self.Rs_2 = Rs_2
+        self.get_l_output = get_l_output
 
         Rs_out = []
         for (mul, l_1, p_1), (mul_2, l_2, p_2) in zip(Rs_1, Rs_2):
             assert mul == mul_2
-            for l in range(abs(l_1 - l_2), l_1 + l_2 + 1):
+            for l in get_l_output(l_1, l_2):
                 Rs_out.append((mul, l, p_1 * p_2))
 
                 if l_1 == 0 or l_2 == 0:
@@ -137,7 +233,7 @@ class ElementwiseMultiplication(torch.nn.Module):
                 out = f_1 * f_2
                 output.append(out.view(batch, mul * (2 * max(l_1, l_2) + 1)))
             else:
-                for l in range(abs(l_1 - l_2), l_1 + l_2 + 1):
+                for l in self.get_l_output(l_1, l_2):
                     C = SO3.clebsch_gordan(l, l_1, l_2, cached=True, like=f_1) * (2 * l + 1) ** 0.5
                     out = torch.einsum("ijk,zj,zk->zi", C, f_1, f_2).view(batch, mul * (2 * l + 1))  # [z, i]
                     output.append(out)
@@ -146,3 +242,24 @@ class ElementwiseMultiplication(torch.nn.Module):
 
         output = torch.cat(output, dim=1)
         return output.view(*size_1, -1)
+
+
+class LearnableTensorProduct(torch.nn.Module):
+    def __init__(self, Rs_mid_1, Rs_mid_2, mul_mid, Rs_out, get_l_mul=SO3.selection_rule):
+        super().__init__()
+        self.mul_mid = mul_mid
+        self.m = TensorProduct(Rs_mid_1, Rs_mid_2, get_l_mul)
+        self.si = Linear(mul_mid * self.m.Rs_out, Rs_out)
+
+    def forward(self, x1, x2):
+        """
+        :return:         tensor [..., channel]
+        """
+        # split into mul x Rs
+        *size, _ = x1.shape
+        x1 = x1.view(*size, self.mul_mid, -1)
+        x2 = x2.view(*size, self.mul_mid, -1)
+
+        x = self.m(x1, x2).view(*size, -1)
+        x = self.si(x)
+        return x
