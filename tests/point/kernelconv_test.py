2fa89b15bea45bcaf463f0904f159c18f746e530
Ben <benmiller1315@gmail.com>
2020-02-28 14:22:12 +0100

    got rid of redundent code and added precomputation options for KernelConv
    Former-commit-id: 60820facbe36c244389a6426f057dd1a039c62ad [formerly 9828a0364a212c6af2132da9dcd8f1e3161ea3f1]
    Former-commit-id: fe643238bc0838a09637188b23988c02b0543084

--- tests/point/kernelconv_test.py
+++ tests/point/kernelconv_test.py
@@ -7,7 +7,7 @@ import torch
 from e3nn.kernel import Kernel
 from e3nn.radial import ConstantRadialModel
 from e3nn.point.operations import Convolution
-from e3nn.point.kernelconv import KernelConv, KernelQM9
+from e3nn.point.kernelconv import KernelConv
 from e3nn.rs import dim
 
 
@@ -73,33 +73,6 @@ class TestKernelConv(unittest.TestCase):
             check_loss.backward()
 
             self.assertTrue(torch.allclose(self.features.grad, check_features.grad))
-
-
-class TestQM9Kernel(unittest.TestCase):
-    def setUp(self):
-        super(TestQM9Kernel, self).setUp()
-        torch.set_default_dtype(torch.float64)
-        self.Rs_in = [(1, 0), (1, 1), (2, 0), (1, 2)]
-        self.Rs_out = [(2, 0), (1, 1), (1, 2), (3, 0)]
-
-        self.geometry = torch.rand(100, 40, 3)
-        self.features = torch.rand(100, 40, dim(self.Rs_in), requires_grad=True)
-        torch.backends.cudnn.deterministic = True
-
-    def test_compare_forward_norm(self):
-        for normalization in ["norm", "component"]:
-            torch.manual_seed(0)
-            K = partial(Kernel, RadialModel=ConstantRadialModel, normalization=normalization)
-            C = Convolution(K, self.Rs_in, self.Rs_out)
-            new_features = C(self.features, self.geometry)
-
-            torch.manual_seed(0)
-            K9 = partial(KernelQM9, RadialModel=ConstantRadialModel, normalization=normalization)
-            C9 = Convolution(K9, self.Rs_in, self.Rs_out)
-            check_new_features = C9(self.features, self.geometry)
-
-            assert all(torch.all(a == b) for a, b in zip(C.kernel.parameters(), C9.parameters()))
-            self.assertTrue(torch.allclose(new_features, check_new_features))
 
 
 if __name__ == '__main__':
