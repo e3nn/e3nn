6cb24756cfcb98aba7e9a3177bee35a736a9be7b
Mario Geiger <geiger.mario@gmail.com>
2020-03-31 13:21:24 +0200

    check_input_output

--- e3nn/kernel_mod.py
+++ e3nn/kernel_mod.py
@@ -73,21 +73,11 @@ class Kernel(torch.nn.Module):
 
     def check_input_output(self):
         for _, l_out, p_out in self.Rs_out:
-            has_path = False
-            for _, l_in, p_in in self.Rs_in:
-                if self.get_l_filters(l_in, p_in, l_out, p_out):
-                    has_path = True
-                    break
-            if not has_path:
+            if not any(self.get_l_filters(l_in, p_in, l_out, p_out) for _, l_in, p_in in self.Rs_in):
                 raise ValueError("warning! the output (l={}, p={}) cannot be generated".format(l_out, p_out))
 
         for _, l_in, p_in in self.Rs_in:
-            has_path = False
-            for _, l_out, p_out in self.Rs_out:
-                if self.get_l_filters(l_in, p_in, l_out, p_out):
-                    has_path = True
-                    break
-            if not has_path:
+            if not any(self.get_l_filters(l_in, p_in, l_out, p_out) for _, l_out, p_out in self.Rs_out):
                 raise ValueError("warning! the input (l={}, p={}) cannot be used".format(l_in, p_in))
 
     def forward(self, r, **_kwargs):
