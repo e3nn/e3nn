9f1ddbc0c2f74fcd247982aaceac9db560e7faec
Mario Geiger <geiger.mario@gmail.com>
2020-03-31 16:04:54 +0200

    tests faster

--- tests/o3_test.py
+++ tests/o3_test.py
@@ -101,8 +101,8 @@ class Tests(unittest.TestCase):
 
     def test_clebsch_gordan_sh_norm(self):
         with o3.torch_default_dtype(torch.float64):
-            for l_out in range(6):
-                for l_in in range(6):
+            for l_out in range(3 + 1):
+                for l_in in range(l_out, 4 + 1):
                     for l_f in range(abs(l_out - l_in), l_out + l_in + 1):
                         Q = o3.clebsch_gordan(l_out, l_in, l_f)
                         Y = o3.spherical_harmonics_xyz(l_f, torch.randn(1, 3)).view(2 * l_f + 1)
