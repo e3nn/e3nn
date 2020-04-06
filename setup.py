54fa0cf0036d0d7498dcd18006a237b659311a2b
Ben <benmiller1315@gmail.com>
2020-04-06 17:54:58 +0200

    working on packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 # pylint: disable=not-callable, no-member, invalid-name, line-too-long, wildcard-import, unused-wildcard-import, missing-docstring, bare-except
 import os
+import re
 import tarfile
-
 import requests
-import setuptools.command.install
+
 from setuptools import find_packages, setup
+import setuptools.command.install
+
+from appdirs import user_cache_dir
+
 import torch
 from torch.utils.cpp_extension import CUDA_HOME, BuildExtension, CUDAExtension
-from appdirs import user_cache_dir
 
 # python setup.py develop    - if you wont to be able to execute from PyCharm (or similar IDE) - places .so file into e3nn folder from which real_spherical_harmonics imports
 
@@ -56,19 +59,54 @@ class PostInstallCommand(setuptools.command.install.install):
             pass
 
 
+# Recommendations from https://packaging.python.org/
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def read(*parts):
+    with open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name='e3nn',
+    version=find_version("e3nn", "__init__.py"),
+    description='Equivariant convolutional neural networks '
+                'for the group E(3) of 3 dimensional rotations, translations, and mirrors.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/e3nn/e3nn',
     install_requires=[
         'scipy',
         'lie_learn',
-        'appdirs'
+        'appdirs',
+        'torch'
     ],
+    license="MIT",
+    license_files="LICENSE.txt",
+    python_requires='>=3.7',
     dependency_links=['https://github.com/AMLab-Amsterdam/lie_learn'],
     classifiers=[
+        "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: POSIX",
+        # "Operating System :: MacOS",
     ],
     ext_modules=ext_modules,
     cmdclass={'build_ext': BuildExtension, 'install': PostInstallCommand},
