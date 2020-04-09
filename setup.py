62824f34fc18bc792d83494af271d9bd9cfba304
Mario Geiger <geiger.mario@gmail.com>
2020-04-09 09:39:06 +0200

    comment cuda stuff

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import tarfile
 import requests
 import setuptools.command.install
 from setuptools import find_packages, setup
-import torch
-from torch.utils.cpp_extension import CUDA_HOME, BuildExtension, CUDAExtension
+# import torch
+# from torch.utils.cpp_extension import CUDA_HOME, BuildExtension, CUDAExtension
 from appdirs import user_cache_dir
 
 # python setup.py develop    - if you wont to be able to execute from PyCharm (or similar IDE) - places .so file into e3nn folder from which real_spherical_harmonics imports
@@ -15,20 +15,20 @@ from appdirs import user_cache_dir
 # python setup.py build_ext
 # python setup.py install    - PyCharm won't work, because it can't resolve import, but executable from terminal
 
-if not torch.cuda.is_available():
-    ext_modules = None
-    print("GPU is not available. Skip building CUDA extensions.")
-elif torch.cuda.is_available() and CUDA_HOME is not None:
-    ext_modules = [
-        CUDAExtension('e3nn.real_spherical_harmonics',
-                      sources=['src/real_spherical_harmonics/rsh_bind.cpp',
-                               'src/real_spherical_harmonics/rsh_cuda.cu'],
-                      extra_compile_args={'cxx': ['-std=c++14'],
-                                          'nvcc': ['-std=c++14']})
-    ]
-else:
-    # GPU is available, but CUDA_HOME is None
-    raise AssertionError("CUDA_HOME is undefined. Make sure nvcc compiler is available (cuda toolkit installed?)")
+# if not torch.cuda.is_available():
+#     ext_modules = None
+#     print("GPU is not available. Skip building CUDA extensions.")
+# elif torch.cuda.is_available() and CUDA_HOME is not None:
+#     ext_modules = [
+#         CUDAExtension('e3nn.real_spherical_harmonics',
+#                       sources=['src/real_spherical_harmonics/rsh_bind.cpp',
+#                                'src/real_spherical_harmonics/rsh_cuda.cu'],
+#                       extra_compile_args={'cxx': ['-std=c++14'],
+#                                           'nvcc': ['-std=c++14']})
+#     ]
+# else:
+#     # GPU is available, but CUDA_HOME is None
+#     raise AssertionError("CUDA_HOME is undefined. Make sure nvcc compiler is available (cuda toolkit installed?)")
 
 
 class PostInstallCommand(setuptools.command.install.install):
@@ -70,7 +70,10 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    ext_modules=ext_modules,
-    cmdclass={'build_ext': BuildExtension, 'install': PostInstallCommand},
+    # ext_modules=ext_modules,
+    cmdclass={
+        # 'build_ext': BuildExtension,
+        'install': PostInstallCommand
+    },
     packages=find_packages(),
 )
