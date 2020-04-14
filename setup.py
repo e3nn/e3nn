c73599882b45a7d1a0bf8c1d50344c00fd9af174
Mario Geiger <geiger.mario@gmail.com>
2020-04-14 14:02:45 +0200

    prepare setup.py
    Former-commit-id: 016e3dd2c929d78788a6b6626b605db8a77e5479 [formerly ec20148186dfef829b1237088c6852ee05226d56]
    Former-commit-id: c047a9c01991ca7de67ae3cd0154e93f5dcb33a6

--- setup.py
+++ setup.py
@@ -1,79 +1,20 @@
 # pylint: disable=not-callable, no-member, invalid-name, line-too-long, wildcard-import, unused-wildcard-import, missing-docstring, bare-except
 from setuptools import find_packages, setup
 
-# import torch
-# from torch.utils.cpp_extension import CUDA_HOME, BuildExtension, CUDAExtension
-
-# python setup.py develop    - if you wont to be able to execute from PyCharm (or similar IDE) - places .so file into e3nn folder from which real_spherical_harmonics imports
-
-# Or:
-# python setup.py build_ext
-# python setup.py install    - PyCharm won't work, because it can't resolve import, but executable from terminal
-
-# if not torch.cuda.is_available():
-#     ext_modules = None
-#     print("GPU is not available. Skip building CUDA extensions.")
-# elif torch.cuda.is_available() and CUDA_HOME is not None:
-#     ext_modules = [
-#         CUDAExtension('e3nn.real_spherical_harmonics',
-#                       sources=['src/real_spherical_harmonics/rsh_bind.cpp',
-#                                'src/real_spherical_harmonics/rsh_cuda.cu'],
-#                       extra_compile_args={'cxx': ['-std=c++14'],
-#                                           'nvcc': ['-std=c++14']})
-#     ]
-# else:
-#     # GPU is available, but CUDA_HOME is None
-#     raise AssertionError("CUDA_HOME is undefined. Make sure nvcc compiler is available (cuda toolkit installed?)")
-
-
-# import os
-# import tarfile
-# import requests
-# import setuptools.command.install
-# from appdirs import user_cache_dir
-# class PostInstallCommand(setuptools.command.install.install):
-#     """Post-installation for installation mode."""
-
-#     def run(self):
-#         setuptools.command.install.install.run(self)
-#         setuptools.command.install.install.do_egg_install(self)
-
-#         try:
-#             url = 'https://github.com/e3nn/e3nn/releases/download/v0.2-alpha/cache.tar'
-#             root = user_cache_dir("e3nn")
-
-#             if not os.path.isdir(root):
-#                 os.makedirs(root)
-
-#             tar_path = os.path.join(root, "cache.tar")
-#             r = requests.get(url)
-#             open(tar_path, 'wb').write(r.content)
-
-#             tar = tarfile.open(tar_path)
-#             tar.extractall(root)
-#             tar.close()
-#         except:
-#             pass
-
-
 setup(
     name='e3nn',
-    url='https://github.com/e3nn/e3nn',
+    version="0.0.0",
+    url='https://e3nn.org',
     install_requires=[
         'scipy',
         'lie_learn',
-        # 'appdirs'
     ],
-    dependency_links=['https://github.com/AMLab-Amsterdam/lie_learn'],
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    # ext_modules=ext_modules,
-    cmdclass={
-        # 'build_ext': BuildExtension,
-        # 'install': PostInstallCommand
-    },
+    python_requires=">3.7",
     packages=find_packages(),
 )
