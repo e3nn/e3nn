7e6173751376cc581ec085db2d43fc2172aa75ed
Mario Geiger <geiger.mario@gmail.com>
2020-04-14 14:16:14 +0200

    gen_cache lmax

--- gen_cache.py
+++ gen_cache.py
@@ -7,6 +7,6 @@ lmax = 9
 
 for l1 in range(lmax + 1):
     for l2 in range(lmax + 1):
-        for l3 in range(abs(l1 - l2), l1 + l2 + 1):
+        for l3 in range(abs(l1 - l2), min(l1 + l2, lmax) + 1):
             print(l1, l2, l3)
             o3.wigner_3j(l1, l2, l3)
