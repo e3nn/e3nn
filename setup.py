85f7f63431fd115d04f83256d0f7a71c06674d96
Benjamin K. Miller <12955549+bkmi@users.noreply.github.com>
2020-04-15 16:41:44 +0200

    Getting ready for pypi, fixes setup_cuda.py (#70)
    * working on packaging
    
    * setup closer, changed version
    
    * proposed method for kwarg sharing. Not sure if I like it though
    
    * fixed setup_cuda.py path bug
    
    * pep8
    
    * Update pack.py
    
    Co-authored-by: Mario Geiger <geiger.mario@gmail.com>

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
 # pylint: disable=not-callable, no-member, invalid-name, line-too-long, wildcard-import, unused-wildcard-import, missing-docstring, bare-except
-from setuptools import find_packages, setup
+from setuptools import setup
+from pack import KWARGS
 
-setup(
-    name='e3nn',
-    version="0.0.0",
-    url='https://e3nn.org',
-    install_requires=[
-        'scipy',
-        'lie_learn',
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">3.7",
-    packages=find_packages(),
-)
+setup(**KWARGS)
