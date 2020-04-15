5816f1e17f66ab2ecfd3ae9cf9982fe19b7c5e35
Benjamin K. Miller <12955549+bkmi@users.noreply.github.com>
2020-04-15 16:41:44 +0200

    Getting ready for pypi, fixes setup_cuda.py (#70)
    * working on packaging
    
    * setup closer, changed version
    
    * proposed method for kwarg sharing. Not sure if I like it though
    
    * fixed setup_cuda.py path bug
    
    * pep8
    
    * Update pack.py
    
    Co-authored-by: Mario Geiger <geiger.mario@gmail.com>
    Former-commit-id: cc35e5f88fa4627e35a3d5103834d5eb1e328754 [formerly 85f7f63431fd115d04f83256d0f7a71c06674d96]
    Former-commit-id: 679605c4ad69ee7ea934e6f8fcc6bc81cc034ec6

--- setup_cuda.py
+++ setup_cuda.py
@@ -1,31 +1,18 @@
 # pylint: disable=not-callable, no-member, invalid-name, line-too-long, wildcard-import, unused-wildcard-import, missing-docstring, bare-except
-from setuptools import find_packages, setup
+from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from pack import KWARGS
 
 ext_modules = [
     CUDAExtension('e3nn.rsh',
-                  sources=['src/rsh/rsh_bind.cpp',
-                           'src/rsh/rsh_cuda.cu'],
+                  sources=['src/real_spherical_harmonics/rsh_bind.cpp',
+                           'src/real_spherical_harmonics/rsh_cuda.cu'],
                   extra_compile_args={'cxx': ['-std=c++14'],
                                       'nvcc': ['-std=c++14']})
 ]
 
 setup(
-    name='e3nn',
-    version="0.0.0",
-    url='https://e3nn.org',
-    install_requires=[
-        'scipy',
-        'lie_learn',
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">3.7",
     ext_modules=ext_modules,
     cmdclass={'build_ext': BuildExtension},
-    packages=find_packages(),
+    **KWARGS
 )
