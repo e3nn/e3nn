0367c55882fdec733da59c250c5b2631a3b8a72a
Tess Smidt <blondegeek@gmail.com>
2020-04-17 12:57:24 -0700

    extend direct_sum to handle additional indices

--- e3nn/o3.py
+++ e3nn/o3.py
@@ -220,13 +220,15 @@ def direct_sum(*matrices):
     """
     Direct sum of matrices, put them in the diagonal
     """
-    m = sum(x.size(0) for x in matrices)
-    n = sum(x.size(1) for x in matrices)
-    out = matrices[0].new_zeros(m, n)
+    front_indices = matrices[0].shape[:-2]
+    m = sum(x.size(-2) for x in matrices)
+    n = sum(x.size(-1) for x in matrices)
+    total_shape = list(front_indices) + [m, n]
+    out = matrices[0].new_zeros(*total_shape)
     i, j = 0, 0
     for x in matrices:
-        m, n = x.size()
-        out[i: i + m, j: j + n] = x
+        m, n = x.shape[-2:]
+        out[..., i: i + m, j: j + n] = x 
         i += m
         j += n
     return out
