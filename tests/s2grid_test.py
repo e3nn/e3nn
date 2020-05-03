336458b2c57ddc35c2702c2b264972abe7fb604a
Mario Geiger <geiger.mario@gmail.com>
2020-05-03 18:36:06 +0200

    test fft

--- tests/s2grid_test.py
+++ tests/s2grid_test.py
@@ -8,6 +8,44 @@ from e3nn import o3, rs, s2grid
 
 class Tests(unittest.TestCase):
 
+    def test_fft(self):
+        with o3.torch_default_dtype(torch.float64):
+            for lmax in [0, 1, 10, 20]:
+                print(lmax)
+                res = 2 * lmax + 1
+
+                _alphas, _betas, sha, _shb = s2grid.spherical_harmonics_s2_grid(lmax, res, res)
+
+                # orthogonal
+                self.assertLess((sha @ sha.T / res - torch.eye(res)).abs().max(), 1e-10)
+                self.assertLess((sha.T @ sha / res - torch.eye(res)).abs().max(), 1e-10)
+
+                # forward
+                x = torch.randn(res)
+
+                y1 = x @ sha
+
+                y2 = torch.fft(torch.stack([x, torch.zeros_like(x)], dim=-1), 1)
+                y2 = torch.cat([2**0.5 * y2[lmax + 1:, 1], y2[:1, 0], 2**0.5 * y2[1:lmax + 1, 0]])
+
+                y3 = torch.rfft(x, 1)
+                y3 = torch.cat([-2**0.5 * y3[1:, 1].flip(0), y3[:1, 0], 2**0.5 * y3[1:, 0]])
+
+                self.assertLess((y3 - y2).abs().max(), 1e-10 * y3.abs().max())
+                self.assertLess((y1 - y2).abs().max(), 1e-10 * y1.abs().max())
+
+                # backward
+                y = torch.randn(res)
+                x1 = sha @ y / res
+
+                x2 = torch.stack([
+                    torch.cat([y[lmax:lmax + 1], y[lmax + 1:] / 2**0.5]),
+                    torch.cat([torch.zeros(1), -y[:lmax].flip(0) / 2**0.5]),
+                ], dim=-1)
+                x2 = torch.irfft(x2, 1)
+
+                self.assertLess((x1 - x2).abs().max(), 1e-10 * x1.abs().max())
+
     def test_inverse(self):
         with o3.torch_default_dtype(torch.float64):
             lmax = 5
