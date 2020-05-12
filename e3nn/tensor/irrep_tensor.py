b0bf4248ae1fdcec9aad04dff88c151f0b1057d2
Mario Geiger <geiger.mario@gmail.com>
2020-05-12 14:14:59 +0200

    fix mistakes in SphericalTensor

--- e3nn/tensor/irrep_tensor.py
+++ e3nn/tensor/irrep_tensor.py
@@ -4,6 +4,7 @@ from e3nn import rs
 
 class IrrepTensor():
     def __init__(self, tensor, Rs):
+        Rs = rs.convention(Rs)
         if tensor.shape[-1] != rs.dim(Rs):
             raise ValueError("Last tensor dimension and Rs do not have same dimension.")
         self.tensor = tensor
