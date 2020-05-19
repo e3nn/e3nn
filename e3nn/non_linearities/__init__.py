1aef541ac7bd053dcbd1e6c664deddb6490b0817
Mario Geiger <geiger.mario@gmail.com>
2020-05-19 22:30:34 +0200

    fix

--- e3nn/non_linearities/__init__.py
+++ e3nn/non_linearities/__init__.py
@@ -1,6 +1,5 @@
 # pylint: disable=missing-docstring
 from .gated_block import GatedBlock
 from .gated_block_parity import GatedBlockParity
-from .scalar_activation import ScalarActivation
 
-__all__ = ['GatedBlock', 'GatedBlockParity', 'ScalarActivation']
+__all__ = ['GatedBlock', 'GatedBlockParity']
