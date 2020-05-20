998335ead5aded4c4dc83c6ec1dae51c90b3c78c
Mario Geiger <geiger.mario@gmail.com>
2020-05-20 19:20:02 +0200

    remove PairConvolution and NeighborsConvolution

--- e3nn/point/operations.py
+++ e3nn/point/operations.py
@@ -51,54 +51,6 @@ class ConvolutionEinsumFn(torch.autograd.Function):
         return grad_k, grad_features
 
 
-class PairConvolution(torch.nn.Module):
-    def __init__(self, Kernel, Rs_in, Rs_out):
-        super().__init__()
-        self.kernel = Kernel(Rs_in, Rs_out * 6)
-
-    def forward(self, features, geometry, n_norm=1):
-        """
-        :param features: tensor [batch, c, d, channel]
-        :param geometry: tensor [batch, a, xyz]
-        :return:         tensor [batch, a, b, channel]
-        """
-        assert features.size()[:2] == geometry.size()[:2], "features size ({}) and geometry size ({}) should match".format(features.size(), geometry.size())
-        rb = geometry.unsqueeze(1)  # [batch, 1, b, xyz]
-        ra = geometry.unsqueeze(2)  # [batch, a, 1, xyz]
-        k = self.kernel(rb - ra)  # [batch, a, b, 6 * i, j]
-        k.div_((6 * n_norm ** 2) ** 0.5)
-        k1, k2, k3, k4, k5, k6 = k.split(k.size(3) // 6, 3)
-        out = 0
-        out += torch.einsum("zabij,zcdj->zabi", (k1, features))  # [batch, a, b, channel]
-        out += torch.einsum("zacij,zcdj->zai", (k2, features)).unsqueeze(2)  # [batch, a, b, channel]
-        out += torch.einsum("zadij,zcdj->zai", (k3, features)).unsqueeze(2)  # [batch, a, b, channel]
-        out += torch.einsum("zbcij,zcdj->zbi", (k4, features)).unsqueeze(1)  # [batch, a, b, channel]
-        out += torch.einsum("zbdij,zcdj->zbi", (k5, features)).unsqueeze(1)  # [batch, a, b, channel]
-        out += torch.einsum("zcdij,zcdj->zi", (k6, features)).unsqueeze(1).unsqueeze(2)  # [batch, a, b, channel]
-        return out
-
-
-class PairConvolution2(torch.nn.Module):
-    def __init__(self, Kernel, Rs_in, Rs_out):
-        super().__init__()
-        self.kernel1 = Kernel(Rs_in, Rs_out)
-        self.kernel2 = Kernel(Rs_out, Rs_out)
-
-    def forward(self, features, geometry, n_norm=1):
-        """
-        :param features: tensor [batch, c, d, channel]
-        :param geometry: tensor [batch, a, xyz]
-        :return:         tensor [batch, a, b, channel]
-        """
-        assert features.size()[:2] == geometry.size()[:2], "features size ({}) and geometry size ({}) should match".format(features.size(), geometry.size())
-        rb = geometry.unsqueeze(1)  # [batch, 1, b, xyz]
-        ra = geometry.unsqueeze(2)  # [batch, a, 1, xyz]
-        k1 = self.kernel1(rb - ra)  # [batch, a, b, i, j]
-        k2 = self.kernel2(rb - ra)  # [batch, a, b, i, j]
-        k1.div_(n_norm)
-        return torch.einsum("zacij,zbdjk,zcdk->zabi", (k2, k1, features))  # [batch, a, b, channel]
-
-
 class ApplyKernel(torch.nn.Module):
     def __init__(self, Kernel, Rs_in, Rs_out):
         super().__init__()
@@ -115,53 +67,3 @@ class ApplyKernel(torch.nn.Module):
         ra = geometry.unsqueeze(2)  # [batch, a, 1, xyz]
         k = self.kernel(rb - ra)  # [batch, a, b, i, j]
         return torch.einsum("zabij,zbj->zabi", (k, features))  # [batch, point, point, channel]
-
-
-class NeighborsConvolution(torch.nn.Module):
-    def __init__(self, Kernel, Rs_in, Rs_out, radius):
-        super().__init__()
-        self.kernel = Kernel(Rs_in, Rs_out)
-        self.radius = radius
-
-    def forward(self, features, geometry, n_norm=1):
-        """
-        :param features: tensor [batch, point, channel]
-        :param geometry: tensor [batch, point, xyz]
-        :return:         tensor [batch, point, channel]
-        """
-        assert features.size()[:2] == geometry.size()[:2], "features size ({}) and geometry size ({}) should match".format(features.size(), geometry.size())
-        batch, n, _ = geometry.size()
-
-        rb = geometry.unsqueeze(1)  # [batch, 1, b, xyz]
-        ra = geometry.unsqueeze(2)  # [batch, a, 1, xyz]
-        diff = rb - ra  # [batch, a, b, xyz]
-        norm = diff.norm(2, dim=-1).reshape(batch * n, n)  # [batch * a, b]
-
-        neighbors = [
-            (norm[i] < self.radius).nonzero().flatten()
-            for i in range(batch * n)
-        ]
-
-        k = max(len(nei) for nei in neighbors)
-        rel_mask = features.new_zeros(batch * n, k)
-        for i, nei in enumerate(neighbors):
-            rel_mask[i, :len(nei)] = 1
-        rel_mask = rel_mask.reshape(batch, n, k)  # [batch, a, b]
-
-        neighbors = torch.stack([
-            torch.cat([nei, nei.new_zeros(k - len(nei))])
-            for nei in neighbors
-        ])
-        neighbors = neighbors.reshape(batch, n, k)  # [batch, a, b]
-
-        rb = geometry[torch.arange(batch).reshape(-1, 1, 1), neighbors, :]  # [batch, a, b, xyz]
-        ra = geometry[torch.arange(batch).reshape(-1, 1, 1), torch.arange(n).reshape(1, -1, 1), :]  # [batch, a, 1, xyz]
-        diff = rb - ra  # [batch, a, b, xyz]
-
-        neighbor_features = features[torch.arange(batch).reshape(-1, 1, 1), neighbors, :]  # [batch, a, b, j]
-
-        k = self.kernel(diff)  # [batch, a, b, i, j]
-        k.div_(n_norm ** 0.5)
-        output = torch.einsum('zab,zabij,zabj->zai', (rel_mask, k, neighbor_features))  # [batch, a, i]
-
-        return output
