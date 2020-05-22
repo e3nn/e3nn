e52476f6fcc5bdc6e0dddb1be540fb87be39a46c
Mario Geiger <geiger.mario@gmail.com>
2020-05-22 18:54:06 +0200

    rs test

--- tests/rs_test.py
+++ tests/rs_test.py
@@ -1,116 +1,126 @@
 # pylint: disable=not-callable, no-member, invalid-name, line-too-long, wildcard-import, unused-wildcard-import, missing-docstring, protected-access
-import unittest
-
 import pytest
 import torch
 
 from e3nn import o3, rs
+from e3nn.non_linearities.norm import Norm
 
 
-class Tests(unittest.TestCase):
+def test_convention():
+    Rs = [0]
+    assert rs.convention(Rs) == [(1, 0, 0)]
+    Rs = [0, (2, 0)]
+    assert rs.convention(Rs) == [(1, 0, 0), (2, 0, 0)]
 
-    def test_conventionRs(self):
-        Rs = [(1, 0)]
-        Rs_out = rs.convention(Rs)
-        self.assertSequenceEqual(Rs_out, [(1, 0, 0)])
-        Rs = [(1, 0), (2, 0)]
-        Rs_out = rs.convention(Rs)
-        self.assertSequenceEqual(Rs_out, [(1, 0, 0), (2, 0, 0)])
 
-    def test_simplifyRs(self):
-        Rs = [(1, 0), (2, 0)]
-        Rs_out = rs.simplify(Rs)
-        self.assertSequenceEqual(Rs_out, [(3, 0, 0)])
+def test_simplify():
+    Rs = [(1, 0), 0, (1, 0)]
+    assert rs.simplify(Rs) == [(3, 0, 0)]
 
-    def test_irrep_dimRs(self):
-        Rs = [(1, 0), (3, 1), (2, 2)]
-        self.assertTrue(rs.irrep_dim(Rs) == 1 + 3 + 5)
-        Rs = [(1, 0), (3, 0), (2, 0)]
-        self.assertTrue(rs.irrep_dim(Rs) == 1 + 1 + 1)
 
-    def test_mul_dimRs(self):
-        Rs = [(1, 0), (3, 1), (2, 2)]
-        self.assertTrue(rs.mul_dim(Rs) == 6)
-        Rs = [(1, 0), (3, 0), (2, 0)]
-        self.assertTrue(rs.mul_dim(Rs) == 6)
+def test_irrep_dim():
+    Rs = [(1, 0), (3, 1), (2, 2)]
+    assert rs.irrep_dim(Rs) == 1 + 3 + 5
+    Rs = [(1, 0), (3, 0), (2, 0)]
+    assert rs.irrep_dim(Rs) == 1 + 1 + 1
 
-    def test_dimRs(self):
-        Rs = [(1, 0), (3, 1), (2, 2)]
-        self.assertTrue(rs.dim(Rs) == 1 * 1 + 3 * 3 + 2 * 5)
-        Rs = [(1, 0), (3, 0), (2, 0)]
-        self.assertTrue(rs.dim(Rs) == 1 * 1 + 3 * 1 + 2 * 1)
 
-    def test_map_irrep_to_Rs(self):
+def test_mul_dim():
+    Rs = [(1, 0), (3, 1), (2, 2)]
+    assert rs.mul_dim(Rs) == 6
+    Rs = [(1, 0), (3, 0), (2, 0)]
+    assert rs.mul_dim(Rs) == 6
+
+
+def test_dim():
+    Rs = [(1, 0), (3, 1), (2, 2)]
+    assert rs.dim(Rs) == 1 * 1 + 3 * 3 + 2 * 5
+    Rs = [(1, 0), (3, 0), (2, 0)]
+    assert rs.dim(Rs) == 1 * 1 + 3 * 1 + 2 * 1
+
+
+def test_map_irrep_to_Rs():
+    with o3.torch_default_dtype(torch.float64):
+        Rs = [(3, 0)]
+        mapping_matrix = rs.map_irrep_to_Rs(Rs)
+        assert torch.allclose(mapping_matrix, torch.ones(3, 1))
+
+        Rs = [(1, 0), (1, 1), (1, 2)]
+        mapping_matrix = rs.map_irrep_to_Rs(Rs)
+        assert torch.allclose(mapping_matrix, torch.eye(1 + 3 + 5))
+
+
+def test_map_mul_to_Rs():
+    with o3.torch_default_dtype(torch.float64):
+        Rs = [(3, 0)]
+        mapping_matrix = rs.map_mul_to_Rs(Rs)
+        assert torch.allclose(mapping_matrix, torch.eye(3))
+
+        Rs = [(1, 0), (1, 1), (1, 2)]
+        mapping_matrix = rs.map_mul_to_Rs(Rs)
+        check_matrix = torch.zeros(1 + 3 + 5, 3)
+        check_matrix[0, 0] = 1.
+        check_matrix[1:4, 1] = 1.
+        check_matrix[4:, 2] = 1.
+        assert torch.allclose(mapping_matrix, check_matrix)
+
+
+############################################################################
+
+
+def test_elementwise_tensor_product():
+    torch.set_default_dtype(torch.float64)
+
+    Rs_1 = [(3, 0), (2, 1), (5, 2)]
+    Rs_2 = [(1, 0), (2, 1), (2, 2), (2, 0), (2, 1), (1, 2)]
+
+    Rs_out, m = rs.elementwise_tensor_product(Rs_1, Rs_2)
+    mul = rs.ElementwiseTensorProduct(Rs_1, Rs_2)
+
+    x1 = torch.randn(1, rs.dim(Rs_1))
+    x2 = torch.randn(1, rs.dim(Rs_2))
+
+    y1 = mul(x1, x2)
+    y2 = torch.einsum('zi,zj->ijz', x1, x2)
+    y2 = m @ y2.reshape(-1, y2.shape[2])
+    y2 = y2.T
+
+    assert rs.dim(Rs_out) == y1.shape[1]
+    assert (y1 - y2).abs().max() < 1e-10
+
+
+############################################################################
+
+
+def test_tensor_square_equivariance():
+    with o3.torch_default_dtype(torch.float64):
+        Rs_in = [(3, 0), (2, 1), (5, 2)]
+
+        sq = rs.TensorSquare(Rs_in, o3.selection_rule)
+
+        x = rs.randn(Rs_in)
+
+        abc = o3.rand_angles()
+        D_in = rs.rep(Rs_in, *abc)
+        D_out = rs.rep(sq.Rs_out, *abc)
+
+        y1 = sq(D_in @ x)
+        y2 = D_out @ sq(x)
+
+        assert (y1 - y2).abs().max() < 1e-10
+
+
+def test_tensor_square_norm():
+    for Rs_in in [[(1, 0), (1, 1)]]:
         with o3.torch_default_dtype(torch.float64):
-            Rs = [(3, 0)]
-            mapping_matrix = rs.map_irrep_to_Rs(Rs)
-            self.assertTrue(torch.allclose(mapping_matrix, torch.ones(3, 1)))
+            Rs_out, Q = rs.tensor_square(Rs_in, o3.selection_rule, normalization='component', sorted=True)
 
-            Rs = [(1, 0), (1, 1), (1, 2)]
-            mapping_matrix = rs.map_irrep_to_Rs(Rs)
-            self.assertTrue(torch.allclose(mapping_matrix, torch.eye(1 + 3 + 5)))
+            I1 = (Q @ Q.t()).to_dense()
+            I2 = torch.eye(rs.dim(Rs_out))
 
-    def test_map_mul_to_Rs(self):
-        with o3.torch_default_dtype(torch.float64):
-            Rs = [(3, 0)]
-            mapping_matrix = rs.map_mul_to_Rs(Rs)
-            self.assertTrue(torch.allclose(mapping_matrix, torch.eye(3)))
+            d = (I1 - I2).pow(2).mean().sqrt()
+            assert d < 1e-10
 
-            Rs = [(1, 0), (1, 1), (1, 2)]
-            mapping_matrix = rs.map_mul_to_Rs(Rs)
-            check_matrix = torch.zeros(1 + 3 + 5, 3)
-            check_matrix[0, 0] = 1.
-            check_matrix[1:4, 1] = 1.
-            check_matrix[4:, 2] = 1.
-            self.assertTrue(torch.allclose(mapping_matrix, check_matrix))
-
-    def test_elementwise_tensor_product(self):
-        torch.set_default_dtype(torch.float64)
-
-        Rs_1 = [(3, 0), (2, 1), (5, 2)]
-        Rs_2 = [(1, 0), (2, 1), (2, 2), (2, 0), (2, 1), (1, 2)]
-
-        Rs_out, m = rs.elementwise_tensor_product(Rs_1, Rs_2)
-        mul = rs.ElementwiseTensorProduct(Rs_1, Rs_2)
-
-        x1 = torch.randn(1, rs.dim(Rs_1))
-        x2 = torch.randn(1, rs.dim(Rs_2))
-
-        y1 = mul(x1, x2)
-        y2 = torch.einsum('zi,zj->ijz', x1, x2)
-        y2 = m @ y2.reshape(-1, y2.shape[2])
-        y2 = y2.T
-
-        self.assertEqual(rs.dim(Rs_out), y1.shape[1])
-        self.assertLess((y1 - y2).abs().max(), 1e-7 * y1.abs().max())
-
-    def test_tensor_square_equivariance(self):
-        with o3.torch_default_dtype(torch.float64):
-            Rs_in = [(3, 0), (2, 1), (5, 2)]
-
-            sq = rs.TensorSquare(Rs_in, o3.selection_rule)
-
-            x = rs.randn(Rs_in)
-
-            abc = o3.rand_angles()
-            D_in = rs.rep(Rs_in, *abc)
-            D_out = rs.rep(sq.Rs_out, *abc)
-
-            y1 = sq(D_in @ x)
-            y2 = D_out @ sq(x)
-
-            self.assertLess((y1 - y2).abs().max(), 1e-7 * y1.abs().max())
-
-    def test_tensor_square_norm(self):
-        for Rs_in in [[(1, 0), (1, 1)]]:
-            with o3.torch_default_dtype(torch.float64):
-                Rs_out, Q = rs.tensor_square(Rs_in, o3.selection_rule, normalization='component', sorted=True)
-
-                I1 = (Q @ Q.t()).to_dense()
-                I2 = torch.eye(rs.dim(Rs_out))
-
-                d = (I1 - I2).pow(2).mean().sqrt()
-                self.assertLess(d, 1e-10)
 
 ############################################################################
 
@@ -123,7 +133,7 @@ def test_format():
 ############################################################################
 
 
-def test_tensor_product():
+def test_tensor_product_equal_TensorProduct():
     with o3.torch_default_dtype(torch.float64):
         Rs_1 = [(3, 0), (2, 1), (5, 2)]
         Rs_2 = [(1, 0), (2, 1), (2, 2), (2, 0), (2, 1), (1, 2)]
@@ -142,7 +152,7 @@ def test_tensor_product():
         assert (y1 - y2).abs().max() < 1e-10 * y1.abs().max()
 
 
-def test_tensor_to_dense():
+def test_tensor_product_to_dense():
     with o3.torch_default_dtype(torch.float64):
         Rs_1 = [(3, 0), (2, 1), (5, 2)]
         Rs_2 = [(1, 0), (2, 1), (2, 2), (2, 0), (2, 1), (1, 2)]
@@ -193,7 +203,7 @@ def test_tensor_product_left_right():
 
 
 @pytest.mark.parametrize('Rs_in1, Rs_in2', [([(1, 0)], [(2, 0)]), ([(3, 1), (2, 2)], [(2, 0), (1, 1), (1, 3)])])
-def test_tensor_product_norm(Rs_in1, Rs_in2):
+def test_tensor_product_in_in_normalization(Rs_in1, Rs_in2):
     with o3.torch_default_dtype(torch.float64):
         Rs_out, Q = rs.tensor_product(Rs_in1, Rs_in2, o3.selection_rule)
 
@@ -207,8 +217,21 @@ def test_tensor_product_norm(Rs_in1, Rs_in2):
         assert d < 1e-10
 
 
+@pytest.mark.parametrize('Rs_in1, Rs_in2', [([0], [0]), ([4, 2], [3, 4])])
+def test_tensor_product_in_in_normalization_norm(Rs_in1, Rs_in2):
+    with o3.torch_default_dtype(torch.float64):
+        tp = rs.TensorProduct(Rs_in1, Rs_in2, o3.selection_rule, normalization='norm')
+
+        x1 = rs.randn(10, Rs_in1, normalization='norm')
+        x2 = rs.randn(10, Rs_in2, normalization='norm')
+
+        n = Norm(tp.Rs_out, normalization='norm')
+        x = n(tp(x1, x2)).mean(0)
+        assert (x.log10().abs() < 1).all()
+
+
 @pytest.mark.parametrize('Rs_in1, Rs_out', [([(1, 0)], [(2, 0)]), ([(3, 1), (2, 2)], [(2, 0), (1, 1), (1, 3)])])
-def test_tensor_product_in_out_norm(Rs_in1, Rs_out):
+def test_tensor_product_in_out_normalization(Rs_in1, Rs_out):
     with o3.torch_default_dtype(torch.float64):
         n = rs.dim(Rs_out)
         I = torch.eye(n)
@@ -255,7 +278,3 @@ def test_reduce_tensor_antisymmetric_L2():
 def test_reduce_tensor_elasticity_tensor():
     Rs, _Q = rs.reduce_tensor('ijkl=jikl=klij', i=[(1, 1)])
     assert rs.dim(Rs) == 21
-
-
-if __name__ == '__main__':
-    unittest.main()
