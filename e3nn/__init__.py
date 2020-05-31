393d3b299efae7fb030d40d14d30011e72c3491c
Mario Geiger <geiger.mario@gmail.com>
2020-05-31 21:08:58 +0200

    example

--- e3nn/__init__.py
+++ e3nn/__init__.py
@@ -1,1 +1,6 @@
+# pylint: disable=missing-docstring
+from .kernel import Kernel
+from .rs import TensorProduct, TensorSquare
+
 __version__ = "0.0.0"
+__all__ = ['Kernel', 'TensorProduct', 'TensorSquare']
--- examples/example.py
+++ examples/example.py
@@ -3,12 +3,11 @@ from functools import partial
 
 import torch
 
-from e3nn.non_linearities.rescaled_act import swish
-from e3nn.radial import GaussianRadialModel
-from e3nn.kernel import Kernel
-from e3nn.point.operations import Convolution
+from e3nn import Kernel, rs
 from e3nn.non_linearities.norm import Norm
-from e3nn import rs
+from e3nn.non_linearities.rescaled_act import swish
+from e3nn.point.operations import Convolution
+from e3nn.radial import GaussianRadialModel
 
 # Define the input and output representations
 Rs_in = [(1, 0), (2, 1)]  # Input = One scalar plus two vectors
@@ -19,20 +18,17 @@ RadialModel = partial(GaussianRadialModel, max_radius=3.0, number_of_basis=3, h=
 
 # kernel: composed on a radial part that contains the learned parameters
 #  and an angular part given by the spherical hamonics and the Clebsch-Gordan coefficients
-K = partial(Kernel, RadialModel=RadialModel, normalization='norm')
-
-# Use the kernel to define a convolution operation
-C = partial(Convolution, K)
+K = partial(Kernel, RadialModel=RadialModel)
 
 # Create the convolution module
-conv = C(Rs_in, Rs_out)
+conv = Convolution(K(Rs_in, Rs_out))
 
 # Module to compute the norm of each irreducible component
-norm = Norm(Rs_out, normalization='norm')
+norm = Norm(Rs_out)
 
 
 n = 5  # number of input points
-features = rs.randn(1, n, Rs_in, normalization='norm', requires_grad=True)
+features = rs.randn(1, n, Rs_in, requires_grad=True)
 in_geometry = torch.randn(1, n, 3)
 out_geometry = torch.zeros(1, 1, 3)  # One point at the origin
 
