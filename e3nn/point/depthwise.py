fecbca6ee533804783e17c70a88ac9f9169da82a
Tess <blondegeek@gmail.com>
2020-07-23 11:11:04 -0700

    add optional final layer

--- e3nn/point/depthwise.py
+++ e3nn/point/depthwise.py
@@ -8,10 +8,11 @@ from e3nn.linear import Linear
 
 
 class DepthwiseConvolution(torch.nn.Module):
-    def __init__(self, Rs_in, Rs_out, Rs_mid1, Rs_mid2, groups, convolution, linear=Linear, scalar_activation=swish, gate_activation=sigmoid):
+    def __init__(self, Rs_in, Rs_out, Rs_mid1, Rs_mid2, groups, convolution, linear=Linear, scalar_activation=swish, gate_activation=sigmoid, final_nonlinearity=True):
         super().__init__()
 
-        act_in = GatedBlock(groups * Rs_mid1, scalar_activation, gate_activation)
+        act_in = GatedBlock(
+            groups * Rs_mid1, scalar_activation, gate_activation)
         self.lin_in = linear(Rs_in, act_in.Rs_in)
         self.act_in = act_in
 
@@ -19,9 +20,13 @@ class DepthwiseConvolution(torch.nn.Module):
         self.conv = convolution(Rs_mid1, act_mid.Rs_in)
         self.act_mid = act_mid
 
-        act_out = GatedBlock(Rs_out, scalar_activation, gate_activation)
-        self.lin_out = linear(groups * Rs_mid2, act_out.Rs_in)
-        self.act_out = act_out
+        if final_nonlinearity:
+            act_out = GatedBlock(Rs_out, scalar_activation, gate_activation)
+            self.lin_out = linear(groups * Rs_mid2, act_out.Rs_in)
+            self.act_out = act_out
+        else:
+            self.lin_out = linear(groups * Rs_mid2, Rs_out)
+            self.act_out = None
 
         self.groups = groups
 
@@ -34,9 +39,11 @@ class DepthwiseConvolution(torch.nn.Module):
         features = self.act_in(features)
 
         features = self.conv(features, *args, **kwargs, groups=self.groups)
-        features = self.act_mid(features.reshape(-1, rs.dim(self.act_mid.Rs_in))).reshape(*features.shape[:-1], -1)
+        features = self.act_mid(
+            features.reshape(-1, rs.dim(self.act_mid.Rs_in))).reshape(*features.shape[:-1], -1)
 
         features = self.lin_out(features)
-        features = self.act_out(features)
+        if self.act_out is not None:
+            features = self.act_out(features)
 
         return features
