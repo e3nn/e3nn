df036d5c8587a3e7ff3ff7aaafd241651e1f5b28
Mario Geiger <geiger.mario@gmail.com>
2020-11-18 23:14:03 +0100

    WTPConv2

--- e3nn/point/message_passing.py
+++ e3nn/point/message_passing.py
@@ -1,8 +1,11 @@
 # pylint: disable=arguments-differ, redefined-builtin, missing-docstring, no-member, invalid-name, line-too-long, not-callable, abstract-method
+import math
 import torch
 import torch_geometric as tg
+
 from e3nn import rsh, rs
-from e3nn.tensor_product import WeightedTensorProduct
+from e3nn.tensor_product import WeightedTensorProduct, GroupedWeightedTensorProduct
+from e3nn.linear import Linear
 
 
 class Convolution(tg.nn.MessagePassing):
@@ -83,4 +86,43 @@ class WTPConv(tg.nn.MessagePassing):
         :param sh:  [num_messages, dim(Rs_sh)]
         :param w:   [num_messages, nweight]
         """
+        return self.tp(x_j, sh, w)
+
+
+class WTPConv2(tg.nn.MessagePassing):
+    r"""
+    WTPConv with self interaction and grouping
+
+    This class assumes that the input and output atom positions are the same
+    """
+    def __init__(self, Rs_in, Rs_out, Rs_sh, RadialModel, groups=math.inf, normalization='component'):
+        super().__init__(aggr='add', flow='target_to_source')
+        self.Rs_in = rs.simplify(Rs_in)
+        self.Rs_out = rs.simplify(Rs_out)
+
+        self.lin1 = Linear(Rs_in, Rs_out, allow_unused_inputs=True, allow_zero_outputs=True)
+        self.tp = GroupedWeightedTensorProduct(Rs_in, Rs_sh, Rs_out, groups=groups, normalization=normalization, own_weight=False)
+        self.rm = RadialModel(self.tp.nweight)
+        self.lin2 = Linear(Rs_out, Rs_out)
+        self.Rs_sh = Rs_sh
+        self.normalization = normalization
+
+    def forward(self, features, edge_index, edge_r, sh=None, size=None, n_norm=1):
+        # features = [num_atoms, dim(Rs_in)]
+        if sh is None:
+            sh = rsh.spherical_harmonics_xyz(self.Rs_sh, edge_r, self.normalization)  # [num_messages, dim(Rs_sh)]
+        sh = sh / n_norm**0.5
+
+        w = self.rm(edge_r.norm(dim=1))  # [num_messages, nweight]
+
+        self_interation = self.lin1(features)
+        features = self.propagate(edge_index, size=size, x=features, sh=sh, w=w)
+        features = self.lin2(features)
+        has_self_interaction = torch.cat([
+            features.new_ones(mul * (2 * l + 1)) if any(l_in == l and p_in == p for _, l_in, p_in in self.Rs_in) else features.new_zeros(mul * (2 * l + 1))
+            for mul, l, p in self.Rs_out
+        ])
+        return 0.5**0.5 * self_interation + (1 + (0.5**0.5 - 1) * has_self_interaction) * features
+
+    def message(self, x_j, sh, w):
         return self.tp(x_j, sh, w)
--- examples/point/tetris_torch_geo.py
+++ examples/point/tetris_torch_geo.py
@@ -3,17 +3,15 @@ import time
 from functools import partial
 
 import torch
-import torch_geometric as tg
 from torch_geometric.data import Batch
 from torch_scatter import scatter_add
 
 from e3nn.point.data_helpers import DataNeighbors
-from e3nn import o3, rsh, rs
+from e3nn import o3, rsh
 from e3nn.networks import MLNetwork, make_gated_block
 from e3nn.non_linearities.rescaled_act import swish
-from e3nn.tensor_product import GroupedWeightedTensorProduct
+from e3nn.point.message_passing import WTPConv2
 from e3nn.radial import GaussianRadialModel
-from e3nn.linear import Linear
 
 
 def get_dataset():
@@ -43,58 +41,10 @@ def get_dataset():
     return tetris, labels
 
 
-class Convolution(tg.nn.MessagePassing):
-    """
-    Convolution with self interaction
-    """
-    def __init__(self, Rs_in, Rs_out, lmax=3):
-        super().__init__(aggr='add', flow='target_to_source')
-        RadialModel = partial(
-            GaussianRadialModel,
-            max_radius=1.2,
-            min_radius=0.0,
-            number_of_basis=3,
-            h=100,
-            L=2,
-            act=swish
-        )
-
-        Rs_sh = [(1, l, (-1)**l) for l in range(0, lmax + 1)]
-
-        self.Rs_in = rs.simplify(Rs_in)
-        self.Rs_out = rs.simplify(Rs_out)
-
-        self.lin1 = Linear(Rs_in, Rs_out, allow_unused_inputs=True, allow_zero_outputs=True)
-        self.tp = GroupedWeightedTensorProduct(Rs_in, Rs_sh, Rs_out, own_weight=False)
-        self.rm = RadialModel(self.tp.nweight)
-        self.lin2 = Linear(Rs_out, Rs_out)
-        self.Rs_sh = Rs_sh
-
-    def forward(self, features, edge_index, edge_r, sh=None, size=None, n_norm=1):
-        # features = [num_atoms, dim(Rs_in)]
-        if sh is None:
-            sh = rsh.spherical_harmonics_xyz(self.Rs_sh, edge_r, "component")  # [num_messages, dim(Rs_sh)]
-        sh = sh / n_norm**0.5
-
-        w = self.rm(edge_r.norm(dim=1))  # [num_messages, nweight]
-
-        self_interation = self.lin1(features)
-        features = self.propagate(edge_index, size=size, x=features, sh=sh, w=w)
-        features = self.lin2(features)
-        has_self_interaction = torch.cat([
-            torch.ones(mul * (2 * l + 1)) if any(l_in == l and p_in == p for _, l_in, p_in in self.Rs_in) else torch.zeros(mul * (2 * l + 1))
-            for mul, l, p in self.Rs_out
-        ])
-        return 0.5**0.5 * self_interation + (1 + (0.5**0.5 - 1) * has_self_interaction) * features
-
-    def message(self, x_j, sh, w):
-        return self.tp(x_j, sh, w)
-
-
-def forward(f, shapes, labels, lmax, device):
+def forward(f, shapes, lmax, device):
     r_max = 1.1
     x = torch.ones(4, 1)
-    batch = Batch.from_data_list([DataNeighbors(x, shape, r_max, y=label, self_interaction=False) for shape, label in zip(shapes, labels)])
+    batch = Batch.from_data_list([DataNeighbors(x, shape, r_max, self_interaction=False) for shape in shapes])
     batch = batch.to(device)
     sh = rsh.spherical_harmonics_xyz(list(range(lmax + 1)), batch.edge_attr, 'component')
     out = f(batch.x, batch.edge_index, batch.edge_attr, sh=sh, n_norm=3)
@@ -112,8 +62,18 @@ def main():
     Rs_in = [(1, 0, 1)]
     Rs_out = [(1, 0, -1), (6, 0, 1)]
     lmax = 2
+    Rs_sh = [(1, l, (-1)**l) for l in range(lmax+1)]
+    RadialModel = partial(
+        GaussianRadialModel,
+        max_radius=1.2,
+        min_radius=0.0,
+        number_of_basis=3,
+        h=100,
+        L=2,
+        act=swish
+    )
 
-    f = MLNetwork(Rs_in, Rs_out, partial(Convolution, lmax=lmax), partial(make_gated_block, mul=16, lmax=lmax), layers=4)
+    f = MLNetwork(Rs_in, Rs_out, partial(WTPConv2, Rs_sh=Rs_sh, RadialModel=RadialModel), partial(make_gated_block, mul=16, lmax=lmax), layers=4)
     f = f.to(device)
 
     # Train the network on a tetris dataset
@@ -122,14 +82,15 @@ def main():
 
     wall = time.perf_counter()
     for step in range(100):
-        out = forward(f, tetris, labels, lmax, device)
+        out = forward(f, tetris, lmax, device)
 
         acc = out.cpu().round().eq(labels).double().mean().item()
 
         with torch.no_grad():
-            r_out = forward(f, *get_dataset(), lmax, device)
+            shapes, _ = get_dataset()
+            r_out = forward(f, shapes, lmax, device)
 
-        loss = (out - labels).pow(2).mean()
+        loss = (out - labels.to(device=out.device)).pow(2).mean()
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
