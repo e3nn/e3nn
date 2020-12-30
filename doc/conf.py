e548dea997f718c725e542d3e6442416d679c1e2
Mario Geiger <geiger.mario@gmail.com>
2020-12-30 15:09:46 +0100

    prepare for release

--- doc/conf.py
+++ doc/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, e3nn Developers'
 author = 'e3nn Developers'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
--- e3nn/__init__.py
+++ e3nn/__init__.py
@@ -2,5 +2,5 @@
 from . import rs, o3
 from . import networks
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ['rs', 'o3', 'networks']
