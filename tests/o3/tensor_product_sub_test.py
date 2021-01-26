fd400bce5d088887e7ea3858e2a0f59dde443600
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-01-26 16:43:13 -0700

    Organize tests in folders

--- tests/o3/tensor_product_sub_test.py
+++ tests/o3/tensor_product_sub_test.py
@@ -1,7 +1,7 @@
 import torch
 from e3nn import o3
 from e3nn.nn import Identity
-from e3nn.o3 import FullyConnectedTensorProduct, Linear, FullTensorProduct
+from e3nn.o3 import FullyConnectedTensorProduct, Linear, FullTensorProduct, Norm
 
 
 def test_fully_connected():
@@ -45,3 +45,19 @@ def test_full():
     b = m(irreps_in1.D_from_quaternion(q) @ x1, irreps_in2.D_from_quaternion(q) @ x2)
 
     assert (a - b).abs().max() < 1e-10
+
+
+def test_norm():
+    irreps_in = o3.Irreps("3x0e + 5x1o")
+    scalars = torch.randn(3)
+    vecs = torch.randn(5, 3)
+    norm = Norm(irreps_in=irreps_in)
+    out_norms = norm(
+        torch.cat((
+            scalars.reshape(1, -1), vecs.reshape(1, -1)
+        ), dim=-1)
+    )
+    true_scalar_norms = torch.abs(scalars)
+    true_vec_norms = torch.linalg.norm(vecs, dim=-1)
+    assert torch.allclose(out_norms[0, :3], true_scalar_norms)
+    assert torch.allclose(out_norms[0, 3:], true_vec_norms)
