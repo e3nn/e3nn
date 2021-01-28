7034facb9c2ca5d3f20c8a4797c806bddbfe739c
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-01-28 08:45:17 -0700

    Equivariance Tests

--- tests/o3/reduce_tensor_test.py
+++ tests/o3/reduce_tensor_test.py
@@ -3,7 +3,7 @@ import torch
 from e3nn import o3
 
 
-def test_reduce_tensor_Levi_Civita_symbol(float_tolerance):
+def test_reduce_tensor_Levi_Civita_symbol(float_tolerance, assert_equivariant):
     tp = o3.ReducedTensorProducts('ijk=-ikj=-jik', i='1e')
     irreps = tp.irreps_out
     Q = tp.change_of_basis
@@ -15,8 +15,10 @@ def test_reduce_tensor_Levi_Civita_symbol(float_tolerance):
     Q1 = torch.einsum('li,mj,nk,ijk', D, D, D, Q)
     assert (Q1 - Q).abs().max() < 10*float_tolerance
 
+    assert_equivariant(tp, irreps_in=tp.irreps_in, irreps_out=tp.irreps_out)
 
-def test_reduce_tensor_antisymmetric_L2(float_tolerance):
+
+def test_reduce_tensor_antisymmetric_L2(float_tolerance, assert_equivariant):
     tp = o3.ReducedTensorProducts('ijk=-ikj=-jik', i='2e')
     irreps = tp.irreps_out
     Q = tp.change_of_basis
@@ -31,18 +33,22 @@ def test_reduce_tensor_antisymmetric_L2(float_tolerance):
 
     assert (Q1 - Q2).abs().max() < 10*float_tolerance
 
+    assert_equivariant(tp, irreps_in=tp.irreps_in, irreps_out=tp.irreps_out)
 
-def test_reduce_tensor_elasticity_tensor():
+
+def test_reduce_tensor_elasticity_tensor(assert_equivariant):
     tp = o3.ReducedTensorProducts('ijkl=jikl=klij', i='1e')
     irreps = tp.irreps_out
     assert irreps.dim == 21
+    assert_equivariant(tp, irreps_in=tp.irreps_in, irreps_out=tp.irreps_out)
 
 
-def test_reduce_tensor_elasticity_tensor_parity():
+def test_reduce_tensor_elasticity_tensor_parity(assert_equivariant):
     tp = o3.ReducedTensorProducts('ijkl=jikl=klij', i='1o')
     irreps = tp.irreps_out
     assert all(p == 1 for _, (_, p) in irreps)
     assert irreps.dim == 21
+    assert_equivariant(tp, irreps_in=tp.irreps_in, irreps_out=tp.irreps_out)
 
 
 # def test_reduce_tensor_rot():
@@ -51,7 +57,7 @@ def test_reduce_tensor_elasticity_tensor_parity():
 #     assert irreps.dim == 21
 
 
-def test_reduce_tensor_equivariance(float_tolerance):
+def test_reduce_tensor_equivariance(float_tolerance, assert_equivariant):
     ir = o3.Irreps('1e')
     tp = o3.ReducedTensorProducts('ijkl=jikl=klij', i=ir)
     irreps = tp.irreps_out
@@ -65,3 +71,5 @@ def test_reduce_tensor_equivariance(float_tolerance):
     q2 = torch.einsum('qa,aijkl->qijkl', D, Q)
 
     assert (q1 - q2).abs().max() < 10*float_tolerance
+
+    assert_equivariant(tp, irreps_in=tp.irreps_in, irreps_out=tp.irreps_out)
