b75a0cac4e848d9fa30f130da335a7c4d2504917
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-02-20 11:46:25 -0700

    Flat weights

--- e3nn/o3/tensor_product.py
+++ e3nn/o3/tensor_product.py
@@ -212,7 +212,7 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
         from e3nn.util import broadcast_tensors
 
         @torch.jit.script
-        def main(x1: torch.Tensor, x2: torch.Tensor, ws: List[torch.Tensor], w3j: List[torch.Tensor]) -> torch.Tensor:
+        def main(x1: torch.Tensor, x2: torch.Tensor, ws: torch.Tensor, w3j: List[torch.Tensor]) -> torch.Tensor:
             x1, x2 = broadcast_tensors(x1, x2)
             size = x1.shape[:-1]
             outsize = size + ({self.irreps_out.dim},)
@@ -237,7 +237,7 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
         from e3nn.util import broadcast_tensors
 
         @torch.jit.script
-        def main(x2: torch.Tensor, ws: List[torch.Tensor], w3j: List[torch.Tensor]) -> torch.Tensor:
+        def main(x2: torch.Tensor, ws: torch.Tensor, w3j: List[torch.Tensor]) -> torch.Tensor:
             size = x2.shape[:-1]
             outsize = size + ({self.irreps_in1.dim}, {self.irreps_out.dim},)
             assert x2.shape[-1] == {self.irreps_in2.dim}, "Incorrect feature dimension for x2"
@@ -294,6 +294,7 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
         ]
 
         index_w = -1
+        flat_weight_i = 0
 
         for ins in self.instructions:
             mul_1, (l_1, p_1) = self.irreps_in1[ins.i_in1]
@@ -346,6 +347,11 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
 
             if ins.has_weight:
                 index_w += 1
+                # Extract the weight from the flattened weight tensor
+                line = f"{s}ws_{index_w} = ws[..., {flat_weight_i}:{flat_weight_i + _prod(ins.weight_shape)}].reshape({'' if self.shared_weights else '-1, '}{', '.join(str(d) for d in ins.weight_shape)})\n"
+                code_out += line
+                code_right += line
+                flat_weight_i += _prod(ins.weight_shape)
 
             line_out = f"{s}out[:, {index_out}:{index_out+dim_out}] += {alpha} * {{}}.reshape(batch, {dim_out})\n\n"
             line_right = f"{s}out[:, {index_1}:{index_1+dim_1}, {index_out}:{index_out+dim_out}] += {alpha} * {{}}.reshape(batch, {dim_1}, {dim_out})\n\n"
@@ -365,11 +371,11 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
                     code_right += line
 
                     if ins.connection_mode == 'uvw':
-                        code_out += line_out.format(f"ein('{z}uvw,zu,zv->zw', ws[{index_w}], s1, s2)")
-                        code_right += line_right.format(f"ein('{z}uvw,zv->zuw', ws[{index_w}], s2)")
+                        code_out += line_out.format(f"ein('{z}uvw,zu,zv->zw', ws_{index_w}, s1, s2)")
+                        code_right += line_right.format(f"ein('{z}uvw,zv->zuw', ws_{index_w}, s2)")
                     if ins.connection_mode == 'uvu':
-                        code_out += line_out.format(f"ein('{z}uv,zu,zv->zu', ws[{index_w}], s1, s2)")
-                        code_right += line_right.format(f"ein('{z}uv,uw,zv->zuw', ws[{index_w}], e1, s2)")
+                        code_out += line_out.format(f"ein('{z}uv,zu,zv->zu', ws_{index_w}, s1, s2)")
+                        code_right += line_right.format(f"ein('{z}uv,uw,zv->zuw', ws_{index_w}, e1, s2)")
 
                     continue
 
@@ -377,11 +383,11 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
                     code_out += f"{s}s1 = s1.reshape(batch, {mul_1})\n"
 
                     if ins.connection_mode == 'uvw':
-                        code_out += line_out.format(f"ein('{z}uvw,zu,zvi->zwi', ws[{index_w}], s1, s2)")
-                        code_right += line_right.format(f"ein('{z}uvw,zvi->zuwi', ws[{index_w}], s2)")
+                        code_out += line_out.format(f"ein('{z}uvw,zu,zvi->zwi', ws_{index_w}, s1, s2)")
+                        code_right += line_right.format(f"ein('{z}uvw,zvi->zuwi', ws_{index_w}, s2)")
                     if ins.connection_mode == 'uvu':
-                        code_out += line_out.format(f"ein('{z}uv,zu,zvi->zui', ws[{index_w}], s1, s2)")
-                        code_right += line_right.format(f"ein('{z}uv,uw,zvi->zuwi', ws[{index_w}], e1, s2)")
+                        code_out += line_out.format(f"ein('{z}uv,zu,zvi->zui', ws_{index_w}, s1, s2)")
+                        code_right += line_right.format(f"ein('{z}uv,uw,zvi->zuwi', ws_{index_w}, e1, s2)")
 
                     continue
 
@@ -391,30 +397,30 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
                     code_right += f"{s}wig = torch.eye({2 * l_1 + 1}, dtype=x2.dtype, device=x2.device)\n"
 
                     if ins.connection_mode == 'uvw':
-                        code_out += line_out.format(f"ein('{z}uvw,zui,zv->zwi', ws[{index_w}], s1, s2)")
-                        code_right += line_right.format(f"ein('{z}uvw,ij,zv->zuiwj', ws[{index_w}], wig, s2)")
+                        code_out += line_out.format(f"ein('{z}uvw,zui,zv->zwi', ws_{index_w}, s1, s2)")
+                        code_right += line_right.format(f"ein('{z}uvw,ij,zv->zuiwj', ws_{index_w}, wig, s2)")
                     if ins.connection_mode == 'uvu':
-                        code_out += line_out.format(f"ein('{z}uv,zui,zv->zui', ws[{index_w}], s1, s2)")
-                        code_right += line_right.format(f"ein('{z}uv,ij,uw,zv->zuiwj', ws[{index_w}], wig, e1, s2)")
+                        code_out += line_out.format(f"ein('{z}uv,zui,zv->zui', ws_{index_w}, s1, s2)")
+                        code_right += line_right.format(f"ein('{z}uv,ij,uw,zv->zuiwj', ws_{index_w}, wig, e1, s2)")
 
                     continue
 
                 if l_1 == l_2 and l_out == 0 and ins.connection_mode == 'uvw' and normalization == 'component' and ins.has_weight:
                     # Cl_l_0 = eye / sqrt(2L+1)
-                    code_out += line_out.format(f"ein('{z}uvw,zui,zvi->zw', ws[{index_w}] / {sqrt(2 * l_1 + 1)}, s1, s2)")
-                    code_right += line_right.format(f"ein('{z}uvw,zvi->zuiw', ws[{index_w}] / {sqrt(2 * l_1 + 1)}, s2)")
+                    code_out += line_out.format(f"ein('{z}uvw,zui,zvi->zw', ws_{index_w} / {sqrt(2 * l_1 + 1)}, s1, s2)")
+                    code_right += line_right.format(f"ein('{z}uvw,zvi->zuiw', ws_{index_w} / {sqrt(2 * l_1 + 1)}, s2)")
                     continue
 
                 if l_1 == l_2 and l_out == 0 and ins.connection_mode == 'uvu' and normalization == 'component' and ins.has_weight:
                     # Cl_l_0 = eye / sqrt(2L+1)
-                    code_out += line_out.format(f"ein('{z}uv,zui,zvi->zu', ws[{index_w}] / {sqrt(2 * l_1 + 1)}, s1, s2)")
-                    code_right += line_right.format(f"ein('{z}uv,uw,zvi->zuiw', ws[{index_w}] / {sqrt(2 * l_1 + 1)}, e1, s2)")
+                    code_out += line_out.format(f"ein('{z}uv,zui,zvi->zu', ws_{index_w} / {sqrt(2 * l_1 + 1)}, s1, s2)")
+                    code_right += line_right.format(f"ein('{z}uv,uw,zvi->zuiw', ws_{index_w} / {sqrt(2 * l_1 + 1)}, e1, s2)")
                     continue
 
                 if l_1 == l_2 and l_out == 0 and ins.connection_mode == 'uuu' and normalization == 'component' and ins.has_weight:
                     # Cl_l_0 = eye / sqrt(2L+1)
-                    code_out += line_out.format(f"ein('{z}u,zui,zui->zu', ws[{index_w}] / {sqrt(2 * l_1 + 1)}, s1, s2)")
-                    code_right += line_right.format(f"ein('{z}u,uw,zui->zuiw', ws[{index_w}] / {sqrt(2 * l_1 + 1)}, e1, s2)")
+                    code_out += line_out.format(f"ein('{z}u,zui,zui->zu', ws_{index_w} / {sqrt(2 * l_1 + 1)}, s1, s2)")
+                    code_right += line_right.format(f"ein('{z}u,uw,zui->zuiw', ws_{index_w} / {sqrt(2 * l_1 + 1)}, e1, s2)")
                     continue
 
                 if l_1 == l_2 and l_out == 0 and ins.connection_mode == 'uuu' and normalization == 'component' and not ins.has_weight:
@@ -428,7 +434,7 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
                     code_out += f"{s}s1 = s1.reshape(batch, {mul_1}, 1, {2 * l_1 + 1})\n"
                     code_out += f"{s}s2 = s2.reshape(batch, 1, {mul_2}, {2 * l_2 + 1})\n"
                     code_out += f"{s}s1, s2 = torch.broadcast_tensors(s1, s2)\n"
-                    code_out += line_out.format(f"ein('{z}uvw,zuvi->zwi', ws[{index_w}] / {sqrt(2)}, torch.cross(s1, s2, dim=3))")
+                    code_out += line_out.format(f"ein('{z}uvw,zuvi->zwi', ws_{index_w} / {sqrt(2)}, torch.cross(s1, s2, dim=3))")
 
                     if (l_1, l_2, l_out) in wigners:
                         index_w3j = wigners.index((l_1, l_2, l_out))
@@ -436,7 +442,7 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
                         index_w3j = len(wigners)
                         wigners += [(l_1, l_2, l_out)]
 
-                    code_right += line_right.format(f"ein('{z}uvw,ijk,zvj->zuiwk', ws[{index_w}], w3j[{index_w3j}], s2)")
+                    code_right += line_right.format(f"ein('{z}uvw,ijk,zvj->zuiwk', ws_{index_w}, w3j[{index_w3j}], s2)")
                     continue
 
                 if (l_1, l_2, l_out) == (1, 1, 1) and ins.connection_mode == 'uvu' and normalization == 'component' and ins.has_weight:
@@ -444,7 +450,7 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
                     code_out += f"{s}s1 = s1.reshape(batch, {mul_1}, 1, {2 * l_1 + 1})\n"
                     code_out += f"{s}s2 = s2.reshape(batch, 1, {mul_2}, {2 * l_2 + 1})\n"
                     code_out += f"{s}s1, s2 = torch.broadcast_tensors(s1, s2)\n"
-                    code_out += line_out.format(f"ein('{z}uv,zuvi->zui', ws[{index_w}] / {sqrt(2)}, torch.cross(s1, s2, dim=3))")
+                    code_out += line_out.format(f"ein('{z}uv,zuvi->zui', ws_{index_w} / {sqrt(2)}, torch.cross(s1, s2, dim=3))")
 
                     if (l_1, l_2, l_out) in wigners:
                         index_w3j = wigners.index((l_1, l_2, l_out))
@@ -452,7 +458,7 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
                         index_w3j = len(wigners)
                         wigners += [(l_1, l_2, l_out)]
 
-                    code_right += line_right.format(f"ein('{z}uv,ijk,uw,zvj->zuiwk', ws[{index_w}], w3j[{index_w3j}], e1, s2)")
+                    code_right += line_right.format(f"ein('{z}uv,ijk,uw,zvj->zuiwk', ws_{index_w}, w3j[{index_w3j}], e1, s2)")
                     continue
 
             if last_ss != (ins.i_in1, ins.i_in2, ins.connection_mode[:2]):
@@ -470,29 +476,29 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
 
             if ins.connection_mode == 'uvw':
                 assert ins.has_weight
-                code_out += line_out.format(f"ein('{z}uvw,ijk,zuvij->zwk', ws[{index_w}], w3j[{index_w3j}], ss)")
-                code_right += line_right.format(f"ein('{z}uvw,ijk,zvj->zuiwk', ws[{index_w}], w3j[{index_w3j}], s2)")
+                code_out += line_out.format(f"ein('{z}uvw,ijk,zuvij->zwk', ws_{index_w}, w3j[{index_w3j}], ss)")
+                code_right += line_right.format(f"ein('{z}uvw,ijk,zvj->zuiwk', ws_{index_w}, w3j[{index_w3j}], s2)")
             if ins.connection_mode == 'uvu':
                 assert mul_1 == mul_out
                 if ins.has_weight:
-                    code_out += line_out.format(f"ein('{z}uv,ijk,zuvij->zuk', ws[{index_w}], w3j[{index_w3j}], ss)")
-                    code_right += line_right.format(f"ein('{z}uv,ijk,uw,zvj->zuiwk', ws[{index_w}], w3j[{index_w3j}], e1, s2)")
+                    code_out += line_out.format(f"ein('{z}uv,ijk,zuvij->zuk', ws_{index_w}, w3j[{index_w3j}], ss)")
+                    code_right += line_right.format(f"ein('{z}uv,ijk,uw,zvj->zuiwk', ws_{index_w}, w3j[{index_w3j}], e1, s2)")
                 else:
                     code_out += line_out.format(f"ein('ijk,zuvij->zuk', w3j[{index_w3j}], ss)")
                     code_right += line_right.format(f"ein('ijk,uw,zvj->zuiwk', w3j[{index_w3j}], e1, s2)")
             if ins.connection_mode == 'uvv':
                 assert mul_2 == mul_out
                 if ins.has_weight:
-                    code_out += line_out.format(f"ein('{z}uv,ijk,zuvij->zvk', ws[{index_w}], w3j[{index_w3j}], ss)")
-                    code_right += line_right.format(f"ein('{z}uv,ijk,zvj->zuivk', ws[{index_w}], w3j[{index_w3j}], s2)")
+                    code_out += line_out.format(f"ein('{z}uv,ijk,zuvij->zvk', ws_{index_w}, w3j[{index_w3j}], ss)")
+                    code_right += line_right.format(f"ein('{z}uv,ijk,zvj->zuivk', ws_{index_w}, w3j[{index_w3j}], s2)")
                 else:
                     code_out += line_out.format(f"ein('ijk,zuvij->zvk', w3j[{index_w3j}], ss)")
                     code_right += line_right.format(f"ein('u,ijk,zvj->zuivk', s2.new_zeros({mul_1}).fill_(1.0), w3j[{index_w3j}], s2)")
             if ins.connection_mode == 'uuw':
                 assert mul_1 == mul_2
                 if ins.has_weight:
-                    code_out += line_out.format(f"ein('{z}uw,ijk,zuij->zwk', ws[{index_w}], w3j[{index_w3j}], ss)")
-                    code_right += line_right.format(f"ein('{z}uw,ijk,zuj->zuiwk', ws[{index_w}], w3j[{index_w3j}], s2)")
+                    code_out += line_out.format(f"ein('{z}uw,ijk,zuij->zwk', ws_{index_w}, w3j[{index_w3j}], ss)")
+                    code_right += line_right.format(f"ein('{z}uw,ijk,zuj->zuiwk', ws_{index_w}, w3j[{index_w3j}], s2)")
                 else:
                     assert mul_out == 1
                     code_out += line_out.format(f"ein('ijk,zuij->zk', w3j[{index_w3j}], ss)")
@@ -500,16 +506,16 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
             if ins.connection_mode == 'uuu':
                 assert mul_1 == mul_2 == mul_out
                 if ins.has_weight:
-                    code_out += line_out.format(f"ein('{z}u,ijk,zuij->zuk', ws[{index_w}], w3j[{index_w3j}], ss)")
-                    code_right += line_right.format(f"ein('{z}u,ijk,uw,zuj->zuiwk', ws[{index_w}], w3j[{index_w3j}], e1, s2)")
+                    code_out += line_out.format(f"ein('{z}u,ijk,zuij->zuk', ws_{index_w}, w3j[{index_w3j}], ss)")
+                    code_right += line_right.format(f"ein('{z}u,ijk,uw,zuj->zuiwk', ws_{index_w}, w3j[{index_w3j}], e1, s2)")
                 else:
                     code_out += line_out.format(f"ein('ijk,zuij->zuk', w3j[{index_w3j}], ss)")
                     code_right += line_right.format(f"ein('ijk,uw,zuj->zuiwk', w3j[{index_w3j}], e1, s2)")
             if ins.connection_mode == 'uvuv':
                 assert mul_1 * mul_2 == mul_out
                 if ins.has_weight:
-                    code_out += line_out.format(f"ein('{z}uv,ijk,zuvij->zuvk', ws[{index_w}], w3j[{index_w3j}], ss)")
-                    code_right += line_right.format(f"ein('{z}uv,ijk,uw,zvj->zuiwvk', ws[{index_w}], w3j[{index_w3j}], e1, s2)")
+                    code_out += line_out.format(f"ein('{z}uv,ijk,zuvij->zuvk', ws_{index_w}, w3j[{index_w3j}], ss)")
+                    code_right += line_right.format(f"ein('{z}uv,ijk,uw,zvj->zuiwvk', ws_{index_w}, w3j[{index_w3j}], e1, s2)")
                 else:
                     code_out += line_out.format(f"ein('ijk,zuvij->zuvk', w3j[{index_w3j}], ss)")
                     code_right += line_right.format(f"ein('ijk,uw,zvj->zuiwvk', w3j[{index_w3j}], e1, s2)")
@@ -576,11 +582,12 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
         self.internal_weights = internal_weights
         if internal_weights:
             assert self.shared_weights, "Having internal weights impose shared weights"
-            self.weight = torch.nn.ParameterDict()
-            for ins in self.instructions:
-                if ins.has_weight:
-                    name = f'[{ins.i_in1}:{self.irreps_in1[ins.i_in1]}] x [{ins.i_in2}:{self.irreps_in2[ins.i_in2]}] -> [{ins.i_out}:{self.irreps_out[ins.i_out]}]'
-                    self.weight[name] = torch.nn.Parameter(torch.randn(ins.weight_shape))
+            # self.weight = torch.nn.ParameterDict()
+            # for ins in self.instructions:
+            #     if ins.has_weight:
+            #         name = f'[{ins.i_in1}:{self.irreps_in1[ins.i_in1]}] x [{ins.i_in2}:{self.irreps_in2[ins.i_in2]}] -> [{ins.i_out}:{self.irreps_out[ins.i_out]}]'
+            #         self.weight[name] = torch.nn.Parameter(torch.randn(ins.weight_shape))
+            self.weight = torch.nn.Parameter(torch.randn(self.weight_numel))
 
         if self.irreps_out.dim > 0:
             output_mask = torch.cat([
@@ -611,32 +618,26 @@ class TensorProduct(CodeGenMixin, torch.nn.Module):
             f"-> {self.irreps_out.simplify()} | {npath} paths | {self.weight_numel} weights)"
         )
 
+    @torch.jit.unused
     def prepare_weight_list(self, weight):
         if self.weight_numel:
-            weight_shapes = [ins.weight_shape for ins in self.instructions if ins.has_weight]
-
             if weight is None:
+                # Use internal weights
                 if not self.internal_weights:
                     raise RuntimeError("Weights must be provided when the TensorProduct does not have `internal_weights`")
-                weight = list(self.weight.values())
-            if torch.is_tensor(weight):
-                ws = []
-                i = 0
-                for shape in weight_shapes:
-                    d = _prod(shape)
-                    if not self.shared_weights:
-                        ws += [weight[..., i:i+d].reshape((-1,) + shape)]
-                    else:
-                        ws += [weight[i:i+d].reshape(shape)]
-                    i += d
-                weight = ws
-            if isinstance(weight, list):
+                weight = self.weight
+            elif torch.is_tensor(weight):
+                # It should already be a flat weight tensor
+                assert weight.shape[-1] == self.weight_numel, "Invalid weight shape"
+            elif isinstance(weight, list):
+                weight_shapes = [ins.weight_shape for ins in self.instructions if ins.has_weight]
                 if not self.shared_weights:
-                    weight = [w.reshape(-1, *shape) for w, shape in zip(weight, weight_shapes)]
+                    weight = [w.reshape(-1, _prod(shape)) for w, shape in zip(weight, weight_shapes)]
                 else:
-                    weight = [w.reshape(*shape) for w, shape in zip(weight, weight_shapes)]
+                    weight = [w.reshape(_prod(shape)) for w, shape in zip(weight, weight_shapes)]
+                weight = torch.cat(weight, dim=-1)
         else:
-            weight = []
+            weight = torch.Tensor()
         return weight
 
     def _make_tracing_inputs(self, n: int):
