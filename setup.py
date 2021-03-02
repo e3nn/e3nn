4807e2df0f797dfea260509d38e8f8a632e5f3f0
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-03-01 18:46:42 -0700

    contextvars dependency

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'scipy',   # only for SphericalTensor.find_peaks
         'torch>=1.4.0',
         'torch_geometric',
+        'contextvars;python_version<"3.7"'  # a backport for python 3.6
     ],
     include_package_data=True,
     classifiers=[
