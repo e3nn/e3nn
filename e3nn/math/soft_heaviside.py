0e2ed56d7347e8647e8020493d19d993a84d5ede
Mario Geiger <geiger.mario@gmail.com>
2021-03-16 10:24:33 +0100

    fix soft_unit_step

--- e3nn/math/soft_heaviside.py
+++ e3nn/math/soft_heaviside.py
@@ -1,3 +1,24 @@
+import torch
+
+
+class _SoftUnitStep(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x):
+        ctx.save_for_backward(x)
+        y = torch.zeros_like(x)
+        m = (x > 0.0)
+        y[m] = (-1/x[m]).exp()
+        return y
+
+    @staticmethod
+    def backward(ctx, dy):
+        x, = ctx.saved_tensors
+        dx = torch.zeros_like(x)
+        m = (x > 0.0)
+        xm = x[m]
+        dx[m] = (-1/xm).exp() / xm.pow(2)
+        return dx * dy
+
 
 def soft_unit_step(x):
     r"""smooth :math:`C^\infty` version of the unit step function
@@ -32,4 +53,4 @@ def soft_unit_step(x):
         x = torch.linspace(-1.0, 10.0, 1000)
         plt.plot(x, soft_unit_step(x));
     """
-    return (-1.0 / x).exp() * (x > 0.0)
+    return _SoftUnitStep.apply(x)
