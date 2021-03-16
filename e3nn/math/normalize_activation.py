896e4ad01c205c8b0d11994a315956a4c18e7a1c
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-03-16 13:08:17 -0600

    Infer device for normalization

--- e3nn/math/normalize_activation.py
+++ e3nn/math/normalize_activation.py
@@ -20,6 +20,13 @@ def moment(f, n, dtype=None, device=None):
 class normalize2mom(torch.nn.Module):
     def __init__(self, f, dtype=None, device=None):
         super().__init__()
+
+        # Try to infer a device:
+        if device is None and isinstance(f, torch.nn.Module):
+            # Avoid circular import
+            from e3nn.util._argtools import _get_device
+            device = _get_device(f)
+
         cst = 1 / moment(f, 2, dtype=dtype, device=device) ** 0.5
 
         if abs(cst - 1) < 1e-4:
