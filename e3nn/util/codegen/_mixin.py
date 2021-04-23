657103b8e92785cae6b5f5d7bda5e6aa6a1c5d20
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-04-22 20:49:16 -0600

    Remove ugly ScriptModule pickling hack

--- e3nn/util/codegen/_mixin.py
+++ e3nn/util/codegen/_mixin.py
@@ -5,26 +5,27 @@ import torch
 from torch import fx
 
 
-def _dummy_getstate():
-    return None
-
-
 class CodeGenMixin:
     """Mixin for classes that dynamically generate TorchScript code using FX.
 
-    This class manages evaluating and compiling generated code for subclasses while remaining pickle/deepcopy compatible. If subclasses need to override ``__getstate__``/``__setstate__``, they should be sure to call CodeGenMixin's first and use its output.
+    This class manages evaluating and compiling generated code for subclasses
+    while remaining pickle/deepcopy compatible. If subclasses need to override
+    ``__getstate__``/``__setstate__``, they should be sure to call CodeGenMixin's
+    implimentation first and use its output.
     """
     def _codegen_register(
         self,
         funcs: Dict[str, fx.Graph],
         compile: bool = True
     ) -> None:
-        """Register dynamically generated methods.
+        """Register ``fx.Graph``s as TorchScript submodules.
+
+        ``fx.GraphModule``s will be built with the current module as their ``root``.
 
         Parameters
         ----------
             funcs : Dict[str, fx.Graph]
-                Dictionary mapping method names to their code.
+                Dictionary mapping submodule names to graphs.
         """
         if not hasattr(self, "__codegen__"):
             # list of submodule names that are managed by this object
@@ -39,37 +40,51 @@ class CodeGenMixin:
                 class_name=fname
             ))
             assert isinstance(scriptmod, torch.jit.ScriptModule)
-            # To prevent pickle from erroring, even though we don't actually try
-            # to serialize the scriptmodule.
-            scriptmod.__getstate__ = _dummy_getstate
             # Add the ScriptModule as a submodule so it can be called
             setattr(self, fname, scriptmod)
 
     # In order to support copy.deepcopy and pickling, we need to not save the compiled TorchScript functions:
     # See pickle docs: https://docs.python.org/3/library/pickle.html#pickling-class-instances
-    # torch.nn.Module does not currently impliment __get/setstate__ but may in the future, which is why we have these hasattr checks for other superclasses.
     def __getstate__(self):
         # - Get a state to work with -
         # We need to check if other parent classes of self define __getstate__
+        # torch.nn.Module does not currently impliment __get/setstate__ but
+        # may in the future, which is why we have these hasattr checks for
+        # other superclasses.
         if hasattr(super(CodeGenMixin, self), "__getstate__"):
-            out = super(CodeGenMixin, self).__getstate__().copy()
+            out = super(CodeGenMixin, self).__getstate__()
         else:
-            out = self.__dict__.copy()
-        # - Remove compiled methods -
+            out = self.__dict__
+
+        out = out.copy()
+        # We need a copy of the _modules OrderedDict
+        # Otherwise, modifying the returned state will modify the current module itself
+        out["_modules"] = out["_modules"].copy()
+
+        # - Add saved versions of the ScriptModules to the state -
+        codegen_state = {}
         if hasattr(self, "__codegen__"):
-            # We cant save compiled functions
             for fname in self.__codegen__:
+                # Get the module
                 smod = getattr(self, fname)
+                assert isinstance(smod, torch.jit.ScriptModule)
                 # Save the compiled code as TorchScript IR
                 buffer = io.BytesIO()
                 torch.jit.save(smod, buffer)
                 # Serialize that IR (just some `bytes`) instead of
                 # the ScriptModule
-                out[fname] = buffer.getvalue()
+                codegen_state[fname] = buffer.getvalue()
+                # Remove the compiled submodule from being a submodule
+                # of the saved module
+                del out["_modules"][fname]
+
+            out["__codegen__"] = codegen_state
         return out
 
     def __setstate__(self, d):
         d = d.copy()
+        # We don't want to add this to the object when we call super's __setstate__
+        codegen_state = d.pop("__codegen__", None)
 
         # We need to initialize self first so that we can add submodules
         # We need to check if other parent classes of self define __getstate__
@@ -78,17 +93,15 @@ class CodeGenMixin:
         else:
             self.__dict__.update(d)
 
-        if "__codegen__" in d:
-            codegen_state = d.pop("__codegen__")
-            for fname in codegen_state:
+        if codegen_state is not None:
+            for fname, buffer in codegen_state.items():
+                assert isinstance(fname, str)
                 # Make sure bytes, not ScriptModules, got made
-                assert isinstance(getattr(self, fname), bytes)
-                # Load the TorchScript IR buffer
-                buffer = d[fname]
                 assert isinstance(buffer, bytes)
+                # Load the TorchScript IR buffer
                 buffer = io.BytesIO(buffer)
                 smod = torch.jit.load(buffer)
                 assert isinstance(smod, torch.jit.ScriptModule)
                 # Add the ScriptModule as a submodule
                 setattr(self, fname, smod)
-            self.__codegen__ = codegen_state
+            self.__codegen__ = list(codegen_state.keys())
