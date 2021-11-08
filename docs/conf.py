784a21ee4802e18308e7ae8a09da27169868e10c
Mario Geiger <geiger.mario@gmail.com>
2021-11-08 10:05:13 +0100

    update version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, e3nn Developers'
 author = 'e3nn Developers'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.1'
+release = '0.4.2'
 
 # -- General configuration ---------------------------------------------------
 
