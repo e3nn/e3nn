15b70011f74e7f8ecfef3316e8a936df9fe6e199
Mario Geiger <geiger.mario@gmail.com>
2021-12-06 08:52:22 +0100

    _sub.py

--- e3nn/o3/_tensor_product/__init__.py
+++ e3nn/o3/_tensor_product/__init__.py
@@ -1,12 +1,7 @@
 from ._instruction import Instruction
-from ._tensor_product import (
-    TensorProduct,
-    FullyConnectedTensorProduct,
-    ElementwiseTensorProduct,
-    FullTensorProduct,
-    TensorSquare,
-)
-
+from ._tensor_product import TensorProduct
+from ._sub import (ElementwiseTensorProduct, FullTensorProduct,
+                   FullyConnectedTensorProduct, TensorSquare)
 
 __all__ = [
     "Instruction",
