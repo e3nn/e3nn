67523b58f34a8f01a671ace0497e4d31ed3aeecf
Mit Kotak <mitkotak0305@gmail.com>
2024-06-13 01:33:19 -0400

    name change

--- tests/o3/experimental/test_fulltp.py
+++ tests/o3/experimental/test_fulltp.py
@@ -5,7 +5,7 @@ import pytest
 
 @pytest.mark.parametrize("irreps_in1", ["0e", "0e + 1e"])
 @pytest.mark.parametrize("irreps_in2", ["2x0e", "2x0e + 3x1e"])
-def test_full(irreps_in1, irreps_in2):
+def test_fulltp(irreps_in1, irreps_in2):
     x = o3.Irreps(irreps_in1).randn(10, -1)
     y = o3.Irreps(irreps_in2).randn(10, -1)
 
