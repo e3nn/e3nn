842156a5d99b7d10bfff88f60ef382f47fef3b11
Mit Kotak <mitkotak0305@gmail.com>
2024-07-10 03:17:34 -0400

    updated benchmark setup to hit cudagraphs

--- tests/experimental/benchmarks/linear_pt2.py
+++ tests/experimental/benchmarks/linear_pt2.py
@@ -14,7 +14,7 @@ def run_benchmark(irreps_type):
     torch._inductor.config.triton.unique_kernel_names = True
 
     device = "cuda"
-    compile_mode = "max-autotune-no-cudagraphs"  # Use reduce-overhead for quick compile times
+    compile_mode = "max-autotune"  # Use reduce-overhead for quick compile times
 
     from e3nn import o3, experimental
     import numpy as np
@@ -27,28 +27,29 @@ def run_benchmark(irreps_type):
 
     module = o3
 
-    for lmax in range(1, LMAX + 1):
-        irreps = module.Irreps.spherical_harmonics(lmax)
+    with torch.no_grad():
+        for lmax in range(1, LMAX + 1):
+            irreps = module.Irreps.spherical_harmonics(lmax)
 
-        irreps_in = (CHANNEL * irreps).regroup()
-        x = torch.randn(irreps_in.dim).to(device=device)
-        irreps_out = (int(CHANNEL / 2) * irreps).regroup()
-        print(f"{irreps_type.upper()}: {irreps_in} -> {irreps_out}")
+            irreps_in = (CHANNEL * irreps).regroup()
+            x = torch.randn(irreps_in.dim).to(device=device)
+            irreps_out = (int(CHANNEL / 2) * irreps).regroup()
+            print(f"{irreps_type.upper()}: {irreps_in} -> {irreps_out}")
 
-        linear = module.Linear(irreps_in, irreps_out)
-        linear_compile = torch.compile(linear, backend=compile_mode).to(device=device)
-        print(
-            f"{irreps_type.upper()} Linear lmax {lmax} channel {CHANNEL} batch {BATCH}: {print_performance(lambda: linear_compile(x), times=100, repeat=10)*1000:.3f}ms"
-        )
+            linear = module.Linear(irreps_in, irreps_out)
+            linear_compile = torch.compile(linear, mode=compile_mode).to(device=device)
+            print(
+                f"{irreps_type.upper()} Linear lmax {lmax} channel {CHANNEL} batch {BATCH}: {print_performance(lambda: linear_compile(x), times=100, repeat=10)*1000:.3f}ms"
+            )
 
-        linear_experimental = experimental.Linearv2(irreps_in, irreps_out)
+            linear_experimental = experimental.Linearv2(irreps_in, irreps_out)
 
-        linear_experimental_compile = torch.compile(linear_experimental, backend=compile_mode, fullgraph=True).to(
-            device=device
-        )
-        print(
-            f"{irreps_type.upper()} Linear Experimental lmax {lmax} channel {CHANNEL} batch {BATCH}: {print_performance(lambda: linear_experimental_compile(x, linear.weight), times=100, repeat=10)*1000:.3f}ms"
-        )
+            linear_experimental_compile = torch.compile(linear_experimental, mode=compile_mode, fullgraph=True).to(
+                device=device
+            )
+            print(
+                f"{irreps_type.upper()} Linear Experimental lmax {lmax} channel {CHANNEL} batch {BATCH}: {print_performance(lambda: linear_experimental_compile(x, linear.weight), times=100, repeat=10)*1000:.3f}ms"
+            )
 
 
 def main():
