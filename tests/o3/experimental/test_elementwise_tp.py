bfc79b04c1d5a8025f3bbbfdc1935d1c3ba2acb4
Mit Kotak <mitkotak0305@gmail.com>
2024-07-24 19:51:50 -0400

    added parity test

--- tests/o3/experimental/test_elementwise_tp.py
+++ tests/o3/experimental/test_elementwise_tp.py
@@ -3,7 +3,7 @@ from e3nn import o3
 import pytest
 
 
-@pytest.mark.parametrize("irreps_in1, irreps_in2", [("10x0e", "5x0e + 5x1o"), ("2x0e + 1x1e", "2x0o + 1x1e")])
+@pytest.mark.parametrize("irreps_in1, irreps_in2", [("15x0e", "5x0e + 5x1o + 5x1e"), ("2x0e + 1x1e", "2x0o + 1x1e")])
 def test_elementwise_tp(irreps_in1, irreps_in2):
 
     irreps_in1 = o3.Irreps(irreps_in1)
@@ -13,7 +13,7 @@ def test_elementwise_tp(irreps_in1, irreps_in2):
     x2 = irreps_in2.randn(5, -1)
 
     tp = o3.ElementwiseTensorProduct(irreps_in1, irreps_in2)
-    tp_pt2 = torch.compile(o3.experimental.ElementwiseTensorProductv2(irreps_in1, irreps_in2), fullgraph=True)
+    tp_pt2 = torch.compile(o3.experimental.ElementwiseTensorProductv2(irreps_in1, irreps_in2), disable=True)
     result_tp = tp(x1, x2)
     result_tp2 = tp_pt2(x1, x2)
 
