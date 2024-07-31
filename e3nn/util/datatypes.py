4f01277c015a092c98a99c1c165bfaf9949dd67c
TaufeqRazakh <taufeqmdr@gmail.com>
2024-07-31 04:27:38 -0700

    Introduce new Enum for TP mode

--- e3nn/util/datatypes.py
+++ e3nn/util/datatypes.py
@@ -1,13 +1,18 @@
 from typing import NamedTuple, Optional
-
+from enum import Enum, auto
 
 class Chunk(NamedTuple):
     mul: int
     dim: int
     slice: Optional[slice] = None
 
+class TensorProductMode(Enum):
+    UUU = auto()
+    UVUV = auto()
+    UVW = auto()
 
 class Path(NamedTuple):
     input_1_slice: Chunk
     input_2_slice: Optional[Chunk] = None
     output_slice: Optional[Chunk] = None
+    # tensor_product_mode: TensorProductMode = TensorProductMode.UUU
